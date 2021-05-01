fc2e1ba8b859c863db6b0d6441a863a0e577b21a
ShubhamBhardwaj <shubham.11901502@lpu.in>
2021-05-01 19:08:05 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-const App = () => {
+function App() {
   return <div></div>;
-};
+}
 
 export default App;
