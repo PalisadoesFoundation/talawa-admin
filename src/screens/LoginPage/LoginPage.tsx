b639307b1cafde34337dec8544894181ab934186
Yasharth dubey <yashdubeywinner@gmail.com>
2021-05-23 02:28:11 +0530

    changes in position

--- src/screens/LoginPage/LoginPage.tsx
+++ src/screens/LoginPage/LoginPage.tsx
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Navbar from 'components/Navbar/Navbar';
 import web from 'assets/fourth_image.png';
-import { Form, Input } from 'antd';
 import 'css/Login.css';
-import FormItem from 'antd/lib/form/FormItem';
 
 function LoginPage() {
+  const [formState, setFormState] = useState({
+    email: '',
+    password: '',
+  });
+
   return (
     <>
       <section className="background">
@@ -18,26 +21,36 @@ function LoginPage() {
             <h5>
               <strong className="Color_Green">Login to your account</strong>
             </h5>
-            <Form>
-              <FormItem>
-                <Input
-                  type="text"
-                  id="email"
-                  className="input_box"
-                  placeholder="Username"
-                  required
-                />
-              </FormItem>
-              <FormItem>
-                <Input
-                  type="password"
-                  id="password"
-                  className="input_box"
-                  placeholder="password"
-                  required
-                />
-              </FormItem>
-            </Form>
+            <form className="display_block">
+              <input
+                type="text"
+                id="email"
+                className="input_box"
+                placeholder="Email"
+                value={formState.email}
+                onChange={(e) => {
+                  setFormState({
+                    ...formState,
+                    email: e.target.value,
+                  });
+                }}
+                required
+              />
+              <input
+                type="password"
+                id="password"
+                className="input_box_second"
+                placeholder="Password"
+                value={formState.password}
+                onChange={(e) => {
+                  setFormState({
+                    ...formState,
+                    password: e.target.value,
+                  });
+                }}
+                required
+              />
+            </form>
             <input type="submit" className="btn" value="Login" />
           </div>
           <div className="animated">
--- src/screens/WelcomePage/WelcomePage.tsx
+++ src/screens/WelcomePage/WelcomePage.tsx
@@ -8,23 +8,26 @@ function WelcomePage() {
     <>
       <section className="background">
         <Navbar />
-        <div className="display">
-          <div className="margin">
-            <h3>
-              Welcome to <strong className="Color_Yellow">Talawa Admin</strong>
-            </h3>
-            <h5>
-              The online portal to manage{' '}
-              <strong className="Color_Green">Talawa</strong>
-            </h5>
-            <a href="/login" className="btn">
-              Get Started
-            </a>
+        <section className="flex-wrapper">
+          <div className="welcome-container">
+            <div className="margin">
+              <h3>
+                Welcome to{' '}
+                <strong className="Color_Yellow">Talawa Admin</strong>
+              </h3>
+              <h5>
+                The online portal to manage{' '}
+                <strong className="Color_Green">Talawa</strong>
+              </h5>
+              <a href="/login" className="btn">
+                Get Started
+              </a>
+            </div>
+            <div className="animated">
+              <img src={web} className="image" />
+            </div>
           </div>
-          <div className="animated">
-            <img src={web} className="image" />
-          </div>
-        </div>
+        </section>
       </section>
     </>
   );
