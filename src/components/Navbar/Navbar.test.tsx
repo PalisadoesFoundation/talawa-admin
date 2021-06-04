2d320c484ae50bd41874a33fdbf3e3f37340a7ea
Yasharth dubey <yashdubeywinner@gmail.com>
2021-06-05 04:15:50 +0530

    Update Navbar.test.tsx

--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -3,19 +3,6 @@ import { render, screen, within } from '@testing-library/react';
 import Navbar from './Navbar';
 
 describe('This is the test for Navbar component', () => {
-  test('should render 3 item of the list', () => {
-    render(<Navbar />);
-
-    // Retrieve all the list items
-    const list = screen.getByRole('list');
-    const { getAllByRole } = within(list);
-    const listItems = getAllByRole('listitem');
-
-    //Assertions
-    expect(listItems.length).toBe(3);
-    expect(screen.getByRole('list')).toBeInTheDocument();
-  });
-
   test('should render 5 text elements test for the Navbar componet', () => {
     render(<Navbar />);
     expect(screen.getByText('Talawa')).toBeInTheDocument();
