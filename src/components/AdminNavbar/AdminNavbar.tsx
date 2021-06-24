1e9244c950c6f57d3cc0a03a157a814d3f4fed04
ssaumyaa7 <saumya4799@gmail.com>
2021-06-24 21:53:52 +0530

    main-navbar sidebar

--- src/components/AdminNavbar/AdminNavbar.tsx
+++ src/components/AdminNavbar/AdminNavbar.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './AdminNavbar.module.css';
+import { Nav } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
 import Logo from 'assets/talawa-logo-lite-200x200.png';
-
 interface NavbarProps {
   targets: { url: string; name: string }[];
 }
@@ -9,17 +10,32 @@ interface NavbarProps {
 function AdminNavbar({ targets }: NavbarProps): JSX.Element {
   return (
     <>
-      <div className={styles.header_block}>
-        <img src={Logo} className={styles.logo} />
-        <strong className={styles.heading}>TalawaAdmin</strong>
-      </div>
-      <div className={styles.left_block}>
-        {targets.map(({ name, url }) => (
-          <li key={name}>
-            <a href={url}>{name}</a>
-          </li>
-        ))}
-      </div>
+      <Navbar className={styles.navbarbg}>
+        <Navbar.Brand>
+          <a className={styles.logo} href="/">
+            <img src={Logo} />
+            <strong>Talawa</strong>
+            <strong>Admin</strong>
+          </a>
+        </Navbar.Brand>
+        <Navbar.Collapse>
+          <img
+            src="https://via.placeholder.com/58"
+            className={styles.roundedcircle}
+          />
+        </Navbar.Collapse>
+      </Navbar>
+      <Nav className={styles.sidebar} activeKey="/home">
+        <div className={styles.sidebarsticky}>
+          {targets.map(({ name, url }) => (
+            <Nav.Item key={name} className={styles.navitem}>
+              <Nav.Link href={url} className={styles.naviteminside}>
+                {name}
+              </Nav.Link>
+            </Nav.Item>
+          ))}
+        </div>
+      </Nav>
     </>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import styles from './Navbar.module.css';
+import Navbar from 'react-bootstrap/Navbar';
 import Logo from 'assets/talawa-logo-lite-200x200.png';
 function NavBar(): JSX.Element {
   return (
     <>
-      <nav className={styles.nav}>
-        <div className={styles.main}>
+      <Navbar bg="">
+        <Navbar.Brand>
           <a className={styles.logo} href="/">
             <img src={Logo} />
             <strong>Talawa</strong>
             <strong>Admin</strong>
           </a>
-        </div>
-      </nav>
+        </Navbar.Brand>
+      </Navbar>
     </>
   );
 }
