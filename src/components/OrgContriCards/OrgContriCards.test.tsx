72eaa6a2d0d36f82fd7ba1b4b72bdb09dd273b58
asmitsirohi <asmitsirohi9761@gmail.com>
2022-03-06 22:05:18 +0530

    test for Organization contribution card component added

--- src/components/OrgContriCards/OrgContriCards.test.tsx
+++ src/components/OrgContriCards/OrgContriCards.test.tsx
@@ -3,23 +3,23 @@ import { render, screen } from '@testing-library/react';
 import OrgContriCards from './OrgContriCards';
 
 describe('Testing the Organization Contributions Cards', () => {
-  test('should render props and text elements test for the page component', () => {
+  it('should render props and text elements test for the page component', () => {
     render(
       <OrgContriCards
-        id="624135625"
-        key=""
-        userName="Yasharth Dubey"
-        contriDate="06/07/2008"
-        contriAmount="100"
-        contriTransactionId="ER23DV90"
-        userEmail="saumya4799@gmail.com"
+        id="784513962"
+        key="unique"
+        userName="John Doe"
+        contriDate="06/03/2022"
+        contriAmount="500"
+        contriTransactionId="QW56DA88"
+        userEmail="johndoe@gmail.com"
       />
     );
     expect(screen.getByText('Date:')).toBeInTheDocument();
-    expect(screen.getByText('Yasharth Dubey')).toBeInTheDocument();
-    expect(screen.getByText('06/07/2008')).toBeInTheDocument();
-    expect(screen.getByText('100')).toBeInTheDocument();
-    expect(screen.getByText('ER23DV90')).toBeInTheDocument();
-    expect(screen.getByText('saumya4799@gmail.com')).toBeInTheDocument();
+    expect(screen.getByText('John Doe')).toBeInTheDocument();
+    expect(screen.getByText('06/03/2022')).toBeInTheDocument();
+    expect(screen.getByText('500')).toBeInTheDocument();
+    expect(screen.getByText('QW56DA88')).toBeInTheDocument();
+    expect(screen.getByText('johndoe@gmail.com')).toBeInTheDocument();
   });
 });
