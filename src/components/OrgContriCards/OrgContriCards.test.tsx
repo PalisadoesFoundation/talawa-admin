dcf7ae648709a359731cf57ab657de12049932d9
asmitsirohi <asmitsirohi9761@gmail.com>
2022-03-08 05:17:32 +0530

    test configured with graphql server.

--- src/components/OrgContriCards/OrgContriCards.test.tsx
+++ src/components/OrgContriCards/OrgContriCards.test.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import OrgContriCards from './OrgContriCards';
+import {
+  ApolloClient,
+  NormalizedCacheObject,
+  ApolloProvider,
+  InMemoryCache,
+} from '@apollo/client';
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'http://localhost:4000/graphql',
+});
 
 describe('Testing the Organization Contributions Cards', () => {
   it('should render props and text elements test for the page component', () => {
     render(
-      <OrgContriCards
-        id="784513962"
-        key="unique"
-        userName="John Doe"
-        contriDate="06/03/2022"
-        contriAmount="500"
-        contriTransactionId="QW56DA88"
-        userEmail="johndoe@gmail.com"
-      />
+      <ApolloProvider client={client}>
+        <OrgContriCards
+          id="784513962"
+          key="unique"
+          userName="John Doe"
+          contriDate="06/03/2022"
+          contriAmount="500"
+          contriTransactionId="QW56DA88"
+          userEmail="johndoe@gmail.com"
+        />
+      </ApolloProvider>
     );
     expect(screen.getByText('Date:')).toBeInTheDocument();
     expect(screen.getByText('John Doe')).toBeInTheDocument();
