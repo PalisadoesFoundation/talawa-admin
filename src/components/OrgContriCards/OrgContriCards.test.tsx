ff7f21990a8c964e99bd3b2ae386a1e6867a86e9
Asmit Kumar Sirohi <63240102+asmitsirohi@users.noreply.github.com>
2022-03-14 13:44:48 +0530

    [Fixes] Code Coverage: Create tests for OrgContriCards.tsx (#285)
    * test for Organization contribution card component added
    
    * test configured with graphql server.
    
    * tests for OrgContriCards Updated

--- src/components/OrgContriCards/OrgContriCards.test.tsx
+++ src/components/OrgContriCards/OrgContriCards.test.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import OrgContriCards from './OrgContriCards';
+import {
+  ApolloClient,
+  NormalizedCacheObject,
+  ApolloProvider,
+  InMemoryCache,
+} from '@apollo/client';
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: process.env.REACT_APP_BACKEND_ENDPOINT,
+});
 
 describe('Testing the Organization Contributions Cards', () => {
-  test('should render props and text elements test for the page component', () => {
+  const props = {
+    key: '123',
+    id: '123',
+    userName: 'John Doe',
+    contriDate: '06/03/2022',
+    contriAmount: '500',
+    contriTransactionId: 'QW56DA88',
+    userEmail: 'johndoe@gmail.com',
+  };
+
+  it('should render props and text elements test for the page component', () => {
     render(
-      <OrgContriCards
-        id="624135625"
-        key=""
-        userName="Yasharth Dubey"
-        contriDate="06/07/2008"
-        contriAmount="100"
-        contriTransactionId="ER23DV90"
-        userEmail="saumya4799@gmail.com"
-      />
+      <ApolloProvider client={client}>
+        <OrgContriCards
+          id={props.key}
+          key={props.id}
+          userName={props.userName}
+          contriDate={props.contriDate}
+          contriAmount={props.contriAmount}
+          contriTransactionId={props.contriTransactionId}
+          userEmail={props.userEmail}
+        />
+      </ApolloProvider>
     );
     expect(screen.getByText('Date:')).toBeInTheDocument();
-    expect(screen.getByText('Yasharth Dubey')).toBeInTheDocument();
-    expect(screen.getByText('06/07/2008')).toBeInTheDocument();
-    expect(screen.getByText('100')).toBeInTheDocument();
-    expect(screen.getByText('ER23DV90')).toBeInTheDocument();
-    expect(screen.getByText('saumya4799@gmail.com')).toBeInTheDocument();
+    expect(screen.getByText('John Doe')).toBeInTheDocument();
+    expect(screen.getByText('06/03/2022')).toBeInTheDocument();
+    expect(screen.getByText('500')).toBeInTheDocument();
+    expect(screen.getByText('QW56DA88')).toBeInTheDocument();
+    expect(screen.getByText('johndoe@gmail.com')).toBeInTheDocument();
   });
 });
