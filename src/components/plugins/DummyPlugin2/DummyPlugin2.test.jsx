d80b68b2ac98016c12cda347ef7eea34e6a854b6
Asmit Kumar Sirohi <63240102+asmitsirohi@users.noreply.github.com>
2022-03-18 18:02:35 +0530

    [Fixes] Code Coverage: Create tests for DummyPlugin2.tsx (#294)
    * tests for DummyPlugin.tsx is written
    
    * tests for DummyPlugin2.tsx is written
    
    * minor bugs fixes

---
+++ src/components/plugins/DummyPlugin2/DummyPlugin2.test.jsx
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from 'state/store';
+import DummyPlugin2 from './DummyPlugin2';
+
+describe('Testing dummy plugin 2', () => {
+  test('should render props and text elements test for the page component', () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <DummyPlugin2 />
+        </Provider>
+      </BrowserRouter>
+    );
+  });
+});
