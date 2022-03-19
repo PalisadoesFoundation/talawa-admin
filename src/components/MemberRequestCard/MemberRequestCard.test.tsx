b9f956c79c2aa36c863136d6e4a4c24944993367
asmitsirohi <asmitsirohi9761@gmail.com>
2022-03-19 18:13:40 +0530

    minor bugs fixes

--- src/components/MemberRequestCard/MemberRequestCard.test.tsx
+++ src/components/MemberRequestCard/MemberRequestCard.test.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { act, render, screen } from '@testing-library/react';
-import MemberRequestCard from './MemberRequestCard';
+import { MockedProvider } from '@apollo/react-testing';
+import userEvent from '@testing-library/user-event';
+
 import {
   ACCEPT_ORGANIZATION_REQUEST_MUTATION,
   REJECT_ORGANIZATION_REQUEST_MUTATION,
 } from 'GraphQl/Mutations/mutations';
-import { MockedProvider } from '@apollo/react-testing';
-import userEvent from '@testing-library/user-event';
+import MemberRequestCard from './MemberRequestCard';
 
 const MOCKS = [
   {
@@ -56,28 +57,57 @@ describe('Testing Member Request Card', () => {
     memberName: 'John Doe',
     memberLocation: 'India',
     joinDate: '18/03/2022',
-    memberImage: '',
+    memberImage: 'https://via.placeholder.com/200x100',
     email: 'johndoe@gmail.com',
   };
 
-  global.confirm = () => true;
   global.alert = jest.fn();
 
   it('should render props and text elements test for the page component', async () => {
+    global.confirm = () => true;
+
     render(
       <MockedProvider addTypename={false} mocks={MOCKS}>
         <MemberRequestCard {...props} />
       </MockedProvider>
     );
 
+    await wait();
     userEvent.click(screen.getByText(/Accept/i));
     userEvent.click(screen.getByText(/Reject/i));
-
-    await wait();
 
     expect(screen.getByAltText(/userImage/i)).toBeInTheDocument();
     expect(screen.getByText(/Joined:/i)).toBeInTheDocument();
     expect(screen.getByText(props.memberName)).toBeInTheDocument();
+    expect(screen.getByText(props.memberLocation)).toBeInTheDocument();
+    expect(screen.getByText(props.joinDate)).toBeInTheDocument();
+    expect(screen.getByText(props.email)).toBeInTheDocument();
+  });
+
+  it('Should render text elements when props value is not passed', async () => {
+    global.confirm = () => false;
+
+    render(
+      <MockedProvider addTypename={false} mocks={MOCKS}>
+        <MemberRequestCard
+          key="123"
+          id="1"
+          memberName=""
+          memberLocation="India"
+          joinDate="18/03/2022"
+          memberImage=""
+          email="johndoe@gmail.com"
+        />
+      </MockedProvider>
+    );
+
+    await wait();
+    userEvent.click(screen.getByText(/Accept/i));
+    userEvent.click(screen.getByText(/Reject/i));
+
+    expect(screen.getByAltText(/userImage/i)).toBeInTheDocument();
+    expect(screen.getByText(/Joined:/i)).toBeInTheDocument();
+    expect(screen.queryByText(props.memberName)).not.toBeInTheDocument();
     expect(screen.getByText(props.memberLocation)).toBeInTheDocument();
     expect(screen.getByText(props.joinDate)).toBeInTheDocument();
     expect(screen.getByText(props.email)).toBeInTheDocument();
