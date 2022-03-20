ffe1a20496231c02c9a3c3eb8115bbcc8479fad5
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2022-03-20 04:37:51 +0000

    🐛 FIX: remove extra file

--- src/state/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { actionCreators } from './index';
-
-test('Testing Index File of "src/state/"', () => {
-  expect(actionCreators.installPlugin).toBeTruthy();
-  expect(actionCreators.removePlugin).toBeTruthy();
-  expect(actionCreators.updateInstalled).toBeTruthy();
-  expect(actionCreators.updatePluginLinks).toBeTruthy();
-});
