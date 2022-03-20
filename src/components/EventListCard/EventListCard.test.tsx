783bc13ad80aa66edfae6a25c9d666ddc154e968
Asmit Kumar Sirohi <63240102+asmitsirohi@users.noreply.github.com>
2022-03-20 20:31:12 +0530

    [Fixes] Code Coverage: Create tests for EventListCard.tsx (#300)
    * minor fixes
    
    * tests for event list card added
    
    * minor bugs fixes

--- src/components/EventListCard/EventListCard.test.tsx
+++ src/components/EventListCard/EventListCard.test.tsx
@@ -1,43 +1,107 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import EventListCard from './EventListCard';
-import {
-  ApolloClient,
-  NormalizedCacheObject,
-  ApolloProvider,
-  InMemoryCache,
-} from '@apollo/client';
+import { act, render, screen } from '@testing-library/react';
+import { MockedProvider } from '@apollo/react-testing';
+import userEvent from '@testing-library/user-event';
 
-const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  cache: new InMemoryCache(),
-  uri: 'https://talawa-graphql-api.herokuapp.com/graphql',
-});
+import EventListCard from './EventListCard';
+import { DELETE_EVENT_MUTATION } from 'GraphQl/Mutations/mutations';
+
+const MOCKS = [
+  {
+    request: {
+      query: DELETE_EVENT_MUTATION,
+      variable: { id: '123' },
+    },
+    result: {
+      data: {
+        organizations: [
+          {
+            _id: '1',
+          },
+        ],
+      },
+    },
+  },
+];
+
+async function wait(ms = 0) {
+  await act(() => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  });
+}
 
 describe('Testing Event List Card', () => {
-  test('should render props and text elements test for the page component', () => {
+  const props = {
+    key: '123',
+    id: '1',
+    eventLocation: 'India',
+    eventName: 'Shelter for Dogs',
+    totalAdmin: '5',
+    totalMember: '107',
+    eventImage: 'https://via.placeholder.com/200x100',
+    regDate: '19/03/2022',
+    regDays: '3',
+  };
+
+  global.alert = jest.fn();
+
+  test('should render props and text elements test for the page component', async () => {
+    global.confirm = () => true;
+
     render(
-      <ApolloProvider client={client}>
-        <EventListCard
-          key="123"
-          id=""
-          eventLocation="Gujarat"
-          eventName="Shelter for Dogs"
-          totalAdmin="5"
-          totalMember="107"
-          eventImage=""
-          regDate="07/04/2020"
-          regDays="3"
-        />
-      </ApolloProvider>
+      <MockedProvider addTypename={false} mocks={MOCKS}>
+        <EventListCard {...props} />
+      </MockedProvider>
     );
+
+    await wait();
+
+    userEvent.click(screen.getByText(/Delete/i));
+
     expect(screen.getByText('Admin:')).toBeInTheDocument();
     expect(screen.getByText('Member:')).toBeInTheDocument();
     expect(screen.getByText('Days:')).toBeInTheDocument();
-    expect(screen.getByText('Gujarat')).toBeInTheDocument();
-    expect(screen.getByText('Shelter for Dogs')).toBeInTheDocument();
-    expect(screen.getByText('5')).toBeInTheDocument();
-    expect(screen.getByText('107')).toBeInTheDocument();
-    expect(screen.getByText('07/04/2020')).toBeInTheDocument();
-    expect(screen.getByText('3')).toBeInTheDocument();
+    expect(screen.getByText(props.eventLocation)).toBeInTheDocument();
+    expect(screen.getByText(props.eventName)).toBeInTheDocument();
+    expect(screen.getByText(props.totalAdmin)).toBeInTheDocument();
+    expect(screen.getByText(props.totalMember)).toBeInTheDocument();
+    expect(screen.getByText(props.regDate)).toBeInTheDocument();
+    expect(screen.getByText(props.regDays)).toBeInTheDocument();
+  });
+
+  test('Should render text elements when props value is not passed', async () => {
+    global.confirm = () => false;
+
+    render(
+      <MockedProvider addTypename={false} mocks={MOCKS}>
+        <EventListCard
+          key="123"
+          id="1"
+          eventName=""
+          eventLocation=""
+          totalAdmin="5"
+          eventImage=""
+          totalMember="107"
+          regDate="19/03/2022"
+          regDays="3"
+        />
+      </MockedProvider>
+    );
+
+    await wait();
+
+    userEvent.click(screen.getByText(/Delete/i));
+
+    expect(screen.getByText('Admin:')).toBeInTheDocument();
+    expect(screen.getByText('Member:')).toBeInTheDocument();
+    expect(screen.getByText('Days:')).toBeInTheDocument();
+    expect(screen.queryByText(props.eventLocation)).toBeInTheDocument();
+    expect(screen.queryByText(props.eventName)).not.toBeInTheDocument();
+    expect(screen.getByText(props.totalAdmin)).toBeInTheDocument();
+    expect(screen.getByText(props.totalMember)).toBeInTheDocument();
+    expect(screen.getByText(props.regDate)).toBeInTheDocument();
+    expect(screen.getByText(props.regDays)).toBeInTheDocument();
   });
 });
