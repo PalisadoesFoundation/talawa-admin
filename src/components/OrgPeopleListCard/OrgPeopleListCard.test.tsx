45393be1b4f5335c3ff2ab3f97fce4c729011fe3
asmitsirohi <asmitsirohi9761@gmail.com>
2022-03-21 05:18:14 +0530

    tests for organization people list card added

--- src/components/OrgPeopleListCard/OrgPeopleListCard.test.tsx
+++ src/components/OrgPeopleListCard/OrgPeopleListCard.test.tsx
@@ -1,35 +1,91 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import OrgPeopleListCard from './OrgPeopleListCard';
-import {
-  ApolloClient,
-  NormalizedCacheObject,
-  ApolloProvider,
-  InMemoryCache,
-} from '@apollo/client';
+import { act, render, screen } from '@testing-library/react';
+import { MockedProvider } from '@apollo/react-testing';
+import userEvent from '@testing-library/user-event';
 
-const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  cache: new InMemoryCache(),
-  uri: 'https://talawa-graphql-api.herokuapp.com/graphql',
-});
+import OrgPeopleListCard from './OrgPeopleListCard';
+import { REMOVE_MEMBER_MUTATION } from 'GraphQl/Mutations/mutations';
+
+const MOCKS = [
+  {
+    request: {
+      query: REMOVE_MEMBER_MUTATION,
+      variable: { userid: '123', orgid: '456' },
+    },
+    result: {
+      data: {
+        organizations: [
+          {
+            _id: '1',
+          },
+        ],
+      },
+    },
+  },
+];
+
+async function wait(ms = 0) {
+  await act(() => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  });
+}
 
 describe('Testing Organization People List Card', () => {
-  test('should render props and text elements test for the page component', () => {
+  const props = {
+    key: '123',
+    id: '1',
+    memberName: 'John Doe',
+    memberLocation: 'India',
+    joinDate: '20/03/2022',
+    memberImage: 'https://via.placeholder.com/200x100',
+  };
+
+  global.alert = jest.fn();
+
+  test('should render props and text elements test for the page component', async () => {
+    global.confirm = () => true;
+
     render(
-      <ApolloProvider client={client}>
+      <MockedProvider addTypename={false} mocks={MOCKS}>
+        <OrgPeopleListCard {...props} />
+      </MockedProvider>
+    );
+
+    await wait();
+
+    userEvent.click(screen.getByText(/Remove/i));
+
+    expect(screen.getByText(/Joined:/i)).toBeInTheDocument();
+    expect(screen.getByText(props.memberName)).toBeInTheDocument();
+    expect(screen.getByText(props.memberLocation)).toBeInTheDocument();
+    expect(screen.getByText(props.joinDate)).toBeInTheDocument();
+  });
+
+  test('Should render text elements when props value is not passed', async () => {
+    global.confirm = () => false;
+
+    render(
+      <MockedProvider>
         <OrgPeopleListCard
           key="123"
-          id=""
-          memberName="John Doe"
-          memberLocation="USA"
-          joinDate="04/07/2005"
+          id="1"
+          memberName=""
+          memberLocation="India"
+          joinDate="20/03/2022"
           memberImage=""
         />
-      </ApolloProvider>
+      </MockedProvider>
     );
-    expect(screen.getByText('Joined:')).toBeInTheDocument();
-    expect(screen.getByText('John Doe')).toBeInTheDocument();
-    expect(screen.getByText('USA')).toBeInTheDocument();
-    expect(screen.getByText('04/07/2005')).toBeInTheDocument();
+
+    await wait();
+
+    userEvent.click(screen.getByText(/Remove/i));
+
+    expect(screen.getByText(/Joined:/i)).toBeInTheDocument();
+    expect(screen.queryByText(props.memberName)).not.toBeInTheDocument();
+    expect(screen.getByText(props.memberLocation)).toBeInTheDocument();
+    expect(screen.getByText(props.joinDate)).toBeInTheDocument();
   });
 });
