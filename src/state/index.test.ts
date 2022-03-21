bc8fefae5ec86dfa64e4c094aee360bdf1ea1b9b
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2022-03-21 12:53:20 +0000

    refractoring index.test.ts

--- src/state/index.test.ts
+++ src/state/index.test.ts
@@ -1,8 +1,10 @@
 import { actionCreators } from './index';
 
-test('Testing Index File of "src/state/"', () => {
-  expect(actionCreators.installPlugin).toBeTruthy();
-  expect(actionCreators.removePlugin).toBeTruthy();
-  expect(actionCreators.updateInstalled).toBeTruthy();
-  expect(actionCreators.updatePluginLinks).toBeTruthy();
-});
+describe('Testing Index File of "src/state/', () => {
+  test('Testing Index File of "src/state/"', () => {
+    expect(actionCreators.installPlugin).toBeTruthy();
+    expect(actionCreators.removePlugin).toBeTruthy();
+    expect(actionCreators.updateInstalled).toBeTruthy();
+    expect(actionCreators.updatePluginLinks).toBeTruthy();
+  });
+})
