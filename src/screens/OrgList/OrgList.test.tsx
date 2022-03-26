31de560710e8e82dcb451e6ac6487d7ec5e935a3
Asmit Kumar Sirohi <63240102+asmitsirohi@users.noreply.github.com>
2022-03-26 22:55:37 +0530

    [Fixes] Code Coverage: Create tests for OrgList.tsx (#312)
    * jest-localstorage-mock package added
    
    * minor changes
    
    * tests for organization list screen updated

--- src/screens/OrgList/OrgList.test.tsx
+++ src/screens/OrgList/OrgList.test.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { MockedProvider } from '@apollo/react-testing';
-import { act, render } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import 'jest-localstorage-mock';
+
 import OrgList from './OrgList';
 import {
   ORGANIZATION_LIST,
   USER_ORGANIZATION_LIST,
 } from 'GraphQl/Queries/Queries';
-import { Provider } from 'react-redux';
 import { store } from 'state/store';
+import userEvent from '@testing-library/user-event';
 
 const MOCKS = [
   {
@@ -41,7 +44,7 @@ const MOCKS = [
   {
     request: {
       query: USER_ORGANIZATION_LIST,
-      variables: { id: localStorage.getItem('id') },
+      variables: { id: '123' },
     },
     result: {
       data: {
@@ -51,11 +54,11 @@ const MOCKS = [
             lastName: 'Doe',
             image: '',
             email: 'John_Does_Palasidoes@gmail.com',
-            userType: '',
+            userType: 'SUPERADMIN',
             adminFor: {
               _id: 1,
-              image: '',
               name: 'Akatsuki',
+              image: '',
             },
           },
         ],
@@ -72,11 +75,23 @@ async function wait(ms = 0) {
   });
 }
 
+afterEach(() => {
+  localStorage.clear();
+});
+
 describe('Organisation List Page', () => {
-  test('correct mock data should be queried', async () => {
+  const formData = {
+    name: 'Dummy Organization',
+    description: 'This is a dummy organization',
+    location: 'Delhi, India',
+    image: new File(['hello'], 'hello.png', { type: 'image/png' }),
+  };
+
+  global.alert = jest.fn();
+
+  test('Correct mock data should be queried', async () => {
     const dataQuery1 = MOCKS[0]?.result?.data?.organizations;
 
-    console.log(`Data is ${dataQuery1}`);
     expect(dataQuery1).toEqual([
       {
         _id: 1,
@@ -94,7 +109,8 @@ describe('Organisation List Page', () => {
       },
     ]);
   });
-  test('should render props and text elements test for the screen', async () => {
+
+  test('Should render props and text elements test for the screen', async () => {
     const { container } = render(
       <MockedProvider addTypename={false} mocks={MOCKS}>
         <Provider store={store}>
@@ -104,11 +120,106 @@ describe('Organisation List Page', () => {
     );
 
     expect(container.textContent).not.toBe('Loading data...');
+
     await wait();
-    console.log(container);
+
+    userEvent.click(screen.getByTestId(/logoutBtn/i));
+
     expect(container.textContent).toMatch('Name:');
     expect(container.textContent).toMatch('Designation:');
     expect(container.textContent).toMatch('Email:');
     expect(container.textContent).toMatch('Contact:');
+  });
+
+  test('Testing UserType from local storage', async () => {
+    render(
+      <MockedProvider addTypename={false} mocks={MOCKS}>
+        <Provider store={store}>
+          <OrgList />
+        </Provider>
+      </MockedProvider>
+    );
+
+    await wait();
+
+    expect(screen.getByTestId(/createOrganizationBtnDisable/i)).toBeTruthy();
+  });
+
+  test('Testing Organization data is not present', async () => {
+    render(
+      <MockedProvider addTypename={false}>
+        <Provider store={store}>
+          <OrgList />
+        </Provider>
+      </MockedProvider>
+    );
+
+    await wait();
+  });
+
+  test('Testing create organization modal', async () => {
+    localStorage.setItem('UserType', 'SUPERADMIN');
+
+    render(
+      <MockedProvider addTypename={false} mocks={MOCKS}>
+        <Provider store={store}>
+          <OrgList />
+        </Provider>
+      </MockedProvider>
+    );
+
+    await wait();
+
+    userEvent.click(screen.getByTestId(/createOrganizationBtnEnable/i));
+    userEvent.click(screen.getByTestId(/closeOrganizationModal/i));
+  });
+
+  test('Create organization model should work properly', async () => {
+    localStorage.setItem('UserType', 'SUPERADMIN');
+
+    render(
+      <MockedProvider addTypename={false} mocks={MOCKS}>
+        <Provider store={store}>
+          <OrgList />
+        </Provider>
+      </MockedProvider>
+    );
+
+    await wait();
+
+    expect(localStorage.setItem).toHaveBeenLastCalledWith(
+      'UserType',
+      'SUPERADMIN'
+    );
+
+    userEvent.click(screen.getByTestId(/createOrganizationBtnEnable/i));
+
+    userEvent.type(screen.getByPlaceholderText(/Enter Name/i), formData.name);
+    userEvent.type(
+      screen.getByPlaceholderText(/Enter Description/i),
+      formData.description
+    );
+    userEvent.type(
+      screen.getByPlaceholderText(/Enter Location/i),
+      formData.location
+    );
+    userEvent.click(screen.getByLabelText(/Is Public:/i));
+    userEvent.click(screen.getByLabelText(/Visible:/i));
+    userEvent.upload(screen.getByLabelText(/Display Image:/i), formData.image);
+
+    expect(screen.getByPlaceholderText(/Enter Name/i)).toHaveValue(
+      formData.name
+    );
+    expect(screen.getByPlaceholderText(/Enter Description/i)).toHaveValue(
+      formData.description
+    );
+    expect(screen.getByPlaceholderText(/Enter Location/i)).toHaveValue(
+      formData.location
+    );
+    expect(screen.getByLabelText(/Is Public/i)).not.toBeChecked();
+    expect(screen.getByLabelText(/Visible:/i)).toBeChecked();
+    expect(screen.getByLabelText(/Display Image:/i)).toBeTruthy();
+
+    userEvent.click(screen.getByTestId(/submitOrganizationForm/i));
   });
 });
