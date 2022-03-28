6485abf0a87192251c306d4003e80ed68f11c081
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2022-03-28 10:04:59 +0000

    Added Test Cases : Initial State & Install Plugins

--- src/state/reducers/pluginReducer.test.ts
+++ src/state/reducers/pluginReducer.test.ts
@@ -2,10 +2,32 @@ import reducer from './pluginReducer';
 import * as types from '../action-creators/index';
 import expect from 'expect';
 
-describe('team reducer', () => {
-  it('should return the initial state');
-  it('should handle GET_POST_SUCCESS');
-  it('should handle UPDATE_POST_SUCCESS');
-  it('should handle GET_POST_FAIL');
-  it('should handle GET_POST_START');
-}
+describe('Testing Plugin Reducer', () => {
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {
+            type: '',
+            payload: undefined
+        })).toEqual({
+            installed: [],
+            addonStore: [],
+            extras: [],
+        });
+    });
+    it('should handle INSTALL_PLUGIN',  () => {
+            expect(reducer({ installed: [],
+                addonStore: [],
+                extras: []}, 
+                {
+                type: 'INSTALL_PLUGIN',
+                payload: {name: "testplug"}, }
+              )).toEqual({
+                installed: [{name: "testplug"}],
+                addonStore: [],
+                extras: [],
+              })
+            });
+    test.todo('should handle REMOVE_PLUGIN');
+    test.todo('should handle UPDATE_P_TARGETS');
+    test.todo('should handle UPDATE_INSTALLED');
+});
+
