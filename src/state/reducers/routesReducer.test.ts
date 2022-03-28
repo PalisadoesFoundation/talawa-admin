8cab98385305834e8a217e4295bc52f836f5b084
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2022-03-28 09:08:20 +0000

    created file: routesReducer.test.ts

---
+++ src/state/reducers/routesReducer.test.ts
@@ -0,0 +1,11 @@
+import reducer from './getPostsReducer';
+import * as types from '../actions/posts/getPostsReduxAction';
+import expect from 'expect';
+
+describe('team reducer', () => {
+  it('should return the initial state');
+  it('should handle GET_POST_SUCCESS');
+  it('should handle UPDATE_POST_SUCCESS');
+  it('should handle GET_POST_FAIL');
+  it('should handle GET_POST_START');
+});
