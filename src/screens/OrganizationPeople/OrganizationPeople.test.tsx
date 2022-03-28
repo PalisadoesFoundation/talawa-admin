daaf9b8d6be8675c9f11d9d325bb7d7b22c0ffb6
Asmit Kumar Sirohi <63240102+asmitsirohi@users.noreply.github.com>
2022-03-28 22:12:24 +0530

    [Fixes] Code Coverage: Create tests for OrganizationPeople.tsx (#313)
    * minor changes
    
    * Tests for organization people screen updated

--- src/screens/OrganizationPeople/OrganizationPeople.test.tsx
+++ src/screens/OrganizationPeople/OrganizationPeople.test.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { MockedProvider } from '@apollo/react-testing';
-import { act, render } from '@testing-library/react';
-import OrganizationPeople from './OrganizationPeople';
-import { ADMIN_LIST, MEMBERS_LIST } from 'GraphQl/Queries/Queries';
+import { act, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from 'state/store';
 import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+
+import OrganizationPeople from './OrganizationPeople';
+import { store } from 'state/store';
+import { ADMIN_LIST, MEMBERS_LIST, USER_LIST } from 'GraphQl/Queries/Queries';
 
 const MOCKS = [
   {
@@ -16,7 +18,7 @@ const MOCKS = [
       data: {
         organizations: [
           {
-            _id: 1,
+            _id: 10,
             members: [
               {
                 _id: 100,
@@ -38,7 +40,7 @@ const MOCKS = [
       data: {
         organizations: [
           {
-            _id: 1,
+            _id: 20,
             admins: [
               {
                 _id: 500,
@@ -47,6 +49,23 @@ const MOCKS = [
                 image: '',
               },
             ],
+          },
+        ],
+      },
+    },
+  },
+  {
+    request: {
+      query: USER_LIST,
+    },
+    result: {
+      data: {
+        users: [
+          {
+            _id: 800,
+            firstName: 'Peter',
+            lastName: 'Parker',
+            image: '',
           },
         ],
       },
@@ -63,15 +82,55 @@ async function wait(ms = 0) {
 }
 
 describe('Organisation People Page', () => {
-  test('correct mock data should be queried', async () => {
+  const searchData = {
+    name: 'John Doe',
+    location: 'Delhi, India',
+    event: 'Dummy Event',
+  };
+
+  test('Correct mock data should be queried', async () => {
     const dataQuery1 = MOCKS[0]?.result?.data?.organizations;
-
-    console.log(`Data is ${dataQuery1}`);
-    expect(dataQuery1[0]?._id).toEqual(1);
-
     const dataQuery2 = MOCKS[1]?.result?.data?.organizations;
-    expect(dataQuery2[0]?._id).toEqual(1);
+    const dataQuery3 = MOCKS[2]?.result?.data?.users;
+
+    expect(dataQuery1).toEqual([
+      {
+        _id: 10,
+        members: [
+          {
+            _id: 100,
+            firstName: 'John',
+            lastName: 'Doe',
+            image: '',
+          },
+        ],
+      },
+    ]);
+
+    expect(dataQuery2).toEqual([
+      {
+        _id: 20,
+        admins: [
+          {
+            _id: 500,
+            firstName: 'Sam',
+            lastName: 'Francisco',
+            image: '',
+          },
+        ],
+      },
+    ]);
+
+    expect(dataQuery3).toEqual([
+      {
+        _id: 800,
+        firstName: 'Peter',
+        lastName: 'Parker',
+        image: '',
+      },
+    ]);
   });
+
   test('It is necessary to query the correct mock data.', async () => {
     const { container } = render(
       <MockedProvider addTypename={false} mocks={MOCKS}>
@@ -84,10 +143,112 @@ describe('Organisation People Page', () => {
     );
 
     expect(container.textContent).not.toBe('Loading data...');
+
     await wait();
+
     expect(container.textContent).toMatch('Members');
     expect(container.textContent).toMatch('Filter by Name');
     expect(container.textContent).toMatch('Filter by Location');
     expect(container.textContent).toMatch('Filter by Event');
+  });
+
+  test('Testing filters and MEMBER LIST', async () => {
+    render(
+      <MockedProvider mocks={MOCKS} addTypename={false}>
+        <BrowserRouter>
+          <Provider store={store}>
+            <OrganizationPeople />
+          </Provider>
+        </BrowserRouter>
+      </MockedProvider>
+    );
+
+    await wait();
+
+    userEvent.type(screen.getByPlaceholderText(/Enter Name/i), searchData.name);
+    userEvent.type(
+      screen.getByPlaceholderText(/Enter Location/i),
+      searchData.location
+    );
+    userEvent.type(
+      screen.getByPlaceholderText(/Enter Event/i),
+      searchData.event
+    );
+
+    expect(screen.getByPlaceholderText(/Enter Name/i)).toHaveValue(
+      searchData.name
+    );
+    expect(screen.getByPlaceholderText(/Enter Location/i)).toHaveValue(
+      searchData.location
+    );
+    expect(screen.getByPlaceholderText(/Enter Event/i)).toHaveValue(
+      searchData.event
+    );
+
+    userEvent.click(screen.getByLabelText(/Members/i));
+
+    expect(screen.getByLabelText(/Members/i)).toBeChecked();
+  });
+
+  test('Testing ADMIN LIST', async () => {
+    render(
+      <MockedProvider addTypename={false} mocks={MOCKS}>
+        <BrowserRouter>
+          <Provider store={store}>
+            <OrganizationPeople />
+          </Provider>
+        </BrowserRouter>
+      </MockedProvider>
+    );
+
+    await wait();
+
+    userEvent.click(screen.getByLabelText(/Admins/i));
+
+    await wait();
+
+    expect(screen.getByLabelText(/Admins/i)).toBeChecked();
+  });
+
+  test('Testing USER LIST', async () => {
+    render(
+      <MockedProvider addTypename={false} mocks={MOCKS}>
+        <BrowserRouter>
+          <Provider store={store}>
+            <OrganizationPeople />
+          </Provider>
+        </BrowserRouter>
+      </MockedProvider>
+    );
+
+    await wait();
+
+    userEvent.click(screen.getByLabelText(/Users/i));
+
+    await wait();
+
+    expect(screen.getByLabelText(/Users/i)).toBeChecked();
+  });
+
+  test('No Mock Data', async () => {
+    render(
+      <MockedProvider addTypename={false}>
+        <BrowserRouter>
+          <Provider store={store}>
+            <OrganizationPeople />
+          </Provider>
+        </BrowserRouter>
+      </MockedProvider>
+    );
+
+    await wait();
+
+    userEvent.click(screen.getByLabelText(/Admins/i));
+
+    await wait();
+
+    userEvent.click(screen.getByLabelText(/Users/i));
+
+    await wait();
   });
 });
