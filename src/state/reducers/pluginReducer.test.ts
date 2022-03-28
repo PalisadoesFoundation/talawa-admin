462614ed599b2b5f2901f8b83a06bc34c0b4dffb
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2022-03-28 09:25:27 +0000

    create file : pluginReducer.test.ts

---
+++ src/state/reducers/pluginReducer.test.ts
@@ -0,0 +1,11 @@
+import reducer from './pluginReducer';
+import * as types from '../action-creators/index';
+import expect from 'expect';
+
+describe('team reducer', () => {
+  it('should return the initial state');
+  it('should handle GET_POST_SUCCESS');
+  it('should handle UPDATE_POST_SUCCESS');
+  it('should handle GET_POST_FAIL');
+  it('should handle GET_POST_START');
+}
