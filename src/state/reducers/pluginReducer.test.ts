03a337147e07ed0d30ec84b00c067323b429e5a3
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-03-29 21:59:24 +0530

    [Fixes] Code Coverage: Test plugin reducer (#318)
    * create file : pluginReducer.test.ts
    
    * Added Test Cases : Initial State & Install Plugins
    
    * Add test : REMOVE_PLUGIN
    
    * Add test : UPDATE_INSTALLED
    
    * Add test : UPDATE_STORE
    
    * Add test: UPDATE_EXTRAS
    
    * FIX : Increased Code Coverage and Some fixes

---
+++ src/state/reducers/pluginReducer.test.ts
@@ -0,0 +1,103 @@
+import reducer from './pluginReducer';
+import * as types from '../action-creators/index';
+import expect from 'expect';
+
+describe('Testing Plugin Reducer', () => {
+  it('should return the initial state', () => {
+    expect(
+      reducer(undefined, {
+        type: '',
+        payload: undefined,
+      })
+    ).toEqual({
+      installed: [],
+      addonStore: [],
+      extras: [],
+    });
+  });
+  it('should handle INSTALL_PLUGIN', () => {
+    expect(
+      reducer(
+        { installed: [], addonStore: [], extras: [] },
+        {
+          type: 'INSTALL_PLUGIN',
+          payload: { name: 'testplug' },
+        }
+      )
+    ).toEqual({
+      installed: [{ name: 'testplug' }],
+      addonStore: [],
+      extras: [],
+    });
+  });
+  it('should handle REMOVE_PLUGIN', () => {
+    expect(
+      reducer(
+        {
+          installed: [
+            { name: 'testplug2', id: 3 },
+            { name: 'testplug3', id: 5 },
+          ],
+          addonStore: [],
+          extras: [],
+        },
+        {
+          type: 'REMOVE_PLUGIN',
+          payload: { id: 3 },
+        }
+      )
+    ).toEqual({
+      installed: [{ name: 'testplug3', id: 5 }],
+      addonStore: [],
+      extras: [],
+    });
+  });
+  it('should handle UPDATE_INSTALLED', () => {
+    expect(
+      reducer(
+        { installed: [], addonStore: [], extras: [] },
+        {
+          type: 'UPDATE_INSTALLED',
+          //Here payload is expected to be as array
+          payload: [{ name: 'testplug-updated' }],
+        }
+      )
+    ).toEqual({
+      installed: [{ name: 'testplug-updated' }],
+      addonStore: [],
+      extras: [],
+    });
+  });
+  it('should handle UPDATE_STORE', () => {
+    expect(
+      reducer(
+        { installed: [], addonStore: [], extras: [] },
+        {
+          type: 'UPDATE_STORE',
+          //Here payload is expected to be as array
+          payload: [{ name: 'sample-addon' }],
+        }
+      )
+    ).toEqual({
+      installed: [],
+      addonStore: [{ name: 'sample-addon' }],
+      extras: [],
+    });
+  });
+  it('should handle UPDATE_EXTRAS', () => {
+    expect(
+      reducer(
+        { installed: [], addonStore: [], extras: [] },
+        {
+          type: 'UPDATE_EXTRAS',
+          //Here payload is expected to be as array
+          payload: [{ name: 'sample-addon-extra' }],
+        }
+      )
+    ).toEqual({
+      installed: [],
+      addonStore: [{ name: 'sample-addon-extra' }],
+      extras: [],
+    });
+  });
+});
