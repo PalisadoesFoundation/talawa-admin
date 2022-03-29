e4a6afd8202f6092e96ceea999908de5ed226aa6
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2022-03-29 08:43:23 +0000

    Add test : REMOVE_PLUGIN

--- src/state/reducers/pluginReducer.test.ts
+++ src/state/reducers/pluginReducer.test.ts
@@ -3,31 +3,46 @@ import * as types from '../action-creators/index';
 import expect from 'expect';
 
 describe('Testing Plugin Reducer', () => {
-    it('should return the initial state', () => {
-        expect(reducer(undefined, {
-            type: '',
-            payload: undefined
-        })).toEqual({
-            installed: [],
-            addonStore: [],
-            extras: [],
-        });
+  it('should return the initial state', () => {
+    expect(
+      reducer(undefined, {
+        type: '',
+        payload: undefined,
+      })
+    ).toEqual({
+      installed: [],
+      addonStore: [],
+      extras: [],
     });
-    it('should handle INSTALL_PLUGIN',  () => {
-            expect(reducer({ installed: [],
-                addonStore: [],
-                extras: []}, 
-                {
-                type: 'INSTALL_PLUGIN',
-                payload: {name: "testplug"}, }
-              )).toEqual({
-                installed: [{name: "testplug"}],
-                addonStore: [],
-                extras: [],
-              })
-            });
-    test.todo('should handle REMOVE_PLUGIN');
-    test.todo('should handle UPDATE_P_TARGETS');
-    test.todo('should handle UPDATE_INSTALLED');
+  });
+  it('should handle INSTALL_PLUGIN', () => {
+    expect(
+      reducer(
+        { installed: [], addonStore: [], extras: [] },
+        {
+          type: 'INSTALL_PLUGIN',
+          payload: { name: 'testplug' },
+        }
+      )
+    ).toEqual({
+      installed: [{ name: 'testplug' }],
+      addonStore: [],
+      extras: [],
+    });
+  });
+  it('should handle REMOVE_PLUGIN', () => {
+    expect(
+      reducer(
+        { installed: [], addonStore: [], extras: [] },
+        {
+          type: 'REMOVE_PLUGIN',
+          payload: { name: 'testplug' },
+        }
+      )
+    ).toEqual({
+      installed: [],
+      addonStore: [],
+      extras: [],
+    });
+  });
 });
-
