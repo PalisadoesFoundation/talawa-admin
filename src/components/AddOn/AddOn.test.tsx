edf995e395bf7508165d90b5f2a6422981922c98
asmitsirohi <asmitsirohi9761@gmail.com>
2022-03-29 20:45:58 +0530

    tests for AddOn component added

---
+++ src/components/AddOn/AddOn.test.tsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import { store } from 'state/store';
+import AddOn from './AddOn';
+
+describe('Testing Addon', () => {
+  test('should render props and text elements test for the page component', () => {
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <AddOn />
+        </Provider>
+      </BrowserRouter>
+    );
+
+    expect(getByTestId('pluginContainer')).toBeInTheDocument();
+  });
+});
