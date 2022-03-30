d62434fff1e1c140b5216f3f5e9560c78041a3a5
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2022-03-30 09:30:24 +0000

    Linter Improvements

--- src/state/reducers/routesReducer.test.ts
+++ src/state/reducers/routesReducer.test.ts
@@ -69,9 +69,9 @@ describe('Testing Routes reducer', () => {
     });
   });
 
-  it('should handle UPDATE_TARGETS',()=>{
+  it('should handle UPDATE_TARGETS', () => {
     expect(
-      reducer( undefined, {
+      reducer(undefined, {
         type: 'UPDATE_TARGETS',
         payload: { test: 'testupdate' },
       })
@@ -133,10 +133,75 @@ describe('Testing Routes reducer', () => {
         { name: '', comp_id: 'orglist', component: 'OrgList' },
         { name: '', comp_id: 'orgsetting', component: 'OrgSettings' },
       ],
-    })
+    });
   });
 
-  test.todo('should handle UPDATE_P_TARGETS');
-  test.todo('should handle GET_POST_FAIL');
-  test.todo('should handle GET_POST_START');
+  it('should handle UPDATE_P_TARGETS', () => {
+    expect(
+      reducer(undefined, {
+        type: 'UPDATE_TARGETS',
+        payload: { test: 'test-target' },
+      })
+    ).toEqual({
+      targets: [
+        { name: 'Dashboard', url: '/orgdash/id=undefined' },
+        { name: 'People', url: '/orgpeople/id=undefined' },
+        { name: 'Events', url: '/orgevents/id=undefined' },
+        { name: 'Contributions', url: '/orgcontribution/id=undefined' },
+        { name: 'Posts', url: '/orgpost/id=undefined' },
+        {
+          name: 'Plugins',
+          subTargets: [
+            {
+              icon: 'fa-store',
+              name: 'Plugin Store',
+              url: '/orgstore/id=undefined',
+            },
+          ],
+        },
+        {
+          test: 'test-target',
+        },
+      ],
+      configUrl: '/orgsetting/id=undefined',
+      components: [
+        {
+          name: 'Dashboard',
+          comp_id: 'orgdash',
+          component: 'OrganizationDashboard',
+        },
+        {
+          name: 'People',
+          comp_id: 'orgpeople',
+          component: 'OrganizationPeople',
+        },
+        {
+          name: 'Events',
+          comp_id: 'orgevents',
+          component: 'OrganizationEvents',
+        },
+        {
+          name: 'Contributions',
+          comp_id: 'orgcontribution',
+          component: 'OrgContribution',
+        },
+        { name: 'Posts', comp_id: 'orgpost', component: 'OrgPost' },
+        {
+          name: 'Plugins',
+          comp_id: null,
+          component: 'AddOnStore',
+          subTargets: [
+            {
+              comp_id: 'orgstore',
+              component: 'AddOnStore',
+              icon: 'fa-store',
+              name: 'Plugin Store',
+            },
+          ],
+        },
+        { name: '', comp_id: 'orglist', component: 'OrgList' },
+        { name: '', comp_id: 'orgsetting', component: 'OrgSettings' },
+      ],
+    });
+  });
 });
