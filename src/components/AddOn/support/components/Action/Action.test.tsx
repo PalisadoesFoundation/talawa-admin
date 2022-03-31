a7f5c035f213640b8d34d42092e8268b56271211
asmitsirohi <asmitsirohi9761@gmail.com>
2022-03-31 16:18:28 +0530

    tests for action component added

---
+++ src/components/AddOn/support/components/Action/Action.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+
+import { store } from 'state/store';
+import Action from './Action';
+
+describe('Testing Action Component', () => {
+  const props = {
+    children: 'dummy children',
+    label: 'dummy label',
+  };
+
+  test('should render props and text elements test for the page component', () => {
+    const { debug, getByText } = render(
+      <Provider store={store}>
+        <Action {...props} />
+      </Provider>
+    );
+
+    debug();
+
+    expect(getByText(props.label)).toBeInTheDocument();
+    expect(getByText(props.children)).toBeInTheDocument();
+  });
+});
