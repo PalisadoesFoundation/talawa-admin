28b530dae922a743089fa8f369bf551b5ca4e375
Siddhesh Bhupendra Kuakde <littlelord124@gmail.com>
2022-05-28 14:51:14 +0000

    Add test:  for promises

--- src/components/AddOn/support/services/Plugin.helper.test.ts
+++ src/components/AddOn/support/services/Plugin.helper.test.ts
@@ -7,4 +7,19 @@ describe('Testing src/components/AddOn/support/services/Plugin.helper.ts', () =>
     expect(pluginHelper).toHaveProperty('generateLinks');
     expect(pluginHelper).toHaveProperty('generateLinks');
   });
+  test('Fetchstore function should return a promise', async () => {
+    const pluginHelper = new PluginHelper();
+    await expect(pluginHelper.fetchStore()).rejects.toThrow();
+  });
+  test('FetchInstalled Should Return a promise', async () => {
+    const pluginHelper = new PluginHelper();
+    await expect(pluginHelper.fetchInstalled()).rejects.toThrow();
+  });
+  test('generateLinks should return proper objects', () => {
+    const obj = { enabled: true, name: 'demo', component: 'samplecomponent' };
+    const objToMatch = { name: 'demo', url: '/plugin/samplecomponent' };
+    const pluginHelper = new PluginHelper();
+    const val = pluginHelper.generateLinks([obj]);
+    expect(val).toMatchObject([objToMatch]);
+  });
 });
