dc95eda64c76d0fe814c91561c229d58a673ba5e
SiddheshKukade <littlelord124@gmail.com>
2022-07-25 00:26:10 +0530

    test/addOnEntry

--- src/components/AddOn/core/AddOnEntry/AddOnEntry.test.tsx
+++ src/components/AddOn/core/AddOnEntry/AddOnEntry.test.tsx
@@ -3,29 +3,50 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import AddOnEntry from './AddOnEntry';
+import {
+  ApolloClient,
+  NormalizedCacheObject,
+  ApolloProvider,
+  InMemoryCache,
+  ApolloLink,
+  HttpLink,
+} from '@apollo/client';
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_BACKEND_ENDPOINT,
+  headers: {
+    authorization: 'Bearer ' + localStorage.getItem('token') || '',
+  },
+});
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: ApolloLink.from([httpLink]),
+});
 
 describe('Testing AddOnEntry', () => {
-  // const props = {
-  //   id: 'string',
-  //   enabled: true,
-  //   title: 'string',
-  //   description: 'string',
-  //   createdBy: 'string',
-  //   component: 'string',
-  //   installed: true,
-  //   configurable: true,
-  //   modified: true,
-  //   isInstalled: true,
-  //   getInstalledPlugins: () => {
-  //     return { sample: 'sample' };
-  //   },
-  // };
+  const props = {
+    id: 'string',
+    enabled: true,
+    title: 'string',
+    description: 'string',
+    createdBy: 'string',
+    component: 'string',
+    installed: true,
+    configurable: true,
+    modified: true,
+    isInstalled: true,
+    getInstalledPlugins: () => {
+      return { sample: 'sample' };
+    },
+  };
 
   test('should render modal and take info to add plugin for registered organization', () => {
-    // const { getByTestId } = render(
-    //   <BrowserRouter>{ <AddOnEntry {...props} /> }</BrowserRouter>
-    // );
+    const { getByTestId } = render(
+      <ApolloProvider client={client}>
+        <BrowserRouter>{<AddOnEntry {...props} />}</BrowserRouter>
+      </ApolloProvider>
+    );
     expect(true).toBe(true);
-    // expect(getByTestId('AddOnEntry')).toBeInTheDocument();
+    expect(getByTestId('AddOnEntry')).toBeInTheDocument();
   });
 });
