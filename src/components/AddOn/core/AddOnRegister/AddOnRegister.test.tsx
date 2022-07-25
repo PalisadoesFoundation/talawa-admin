711aa50261b7e555a6093d90391ad0f73e98fb71
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-07-25 16:33:40 +0000

    fix test 2

--- src/components/AddOn/core/AddOnRegister/AddOnRegister.test.tsx
+++ src/components/AddOn/core/AddOnRegister/AddOnRegister.test.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from "@testing-library/user-event";
+import userEvent from '@testing-library/user-event';
 import AddOnRegister from './AddOnRegister';
 import {
   ApolloClient,
@@ -23,29 +24,40 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache: new InMemoryCache(),
   link: ApolloLink.from([httpLink]),
 });
-describe("Testing AddOnRegister", () => {
+describe('Testing AddOnRegister', () => {
   const props = {
-    id: '6234d8bf6ud937ddk70ecc5c9'
+    id: '6234d8bf6ud937ddk70ecc5c9',
   };
 
-  test("should render modal and take info to add plugin for registered organization", () => {
+  test('should render modal and take info to add plugin for registered organization', () => {
     render(
-        <ApolloProvider client={client}>
+      <ApolloProvider client={client}>
         <Provider store={store}>
-          <BrowserRouter>{  <AddOnRegister {...props}/>}</BrowserRouter>
+          <BrowserRouter>{<AddOnRegister {...props} />}</BrowserRouter>
         </Provider>
       </ApolloProvider>
-    
-    ) 
-    userEvent.click(screen.getByRole('button', { name: /add new/i}));
-    userEvent.type(screen.getByPlaceholderText("Ex. Donations"), "myplugin");
-    userEvent.type(screen.getByPlaceholderText("Ex. This plugin provides UI for ...."), "test description");
-    userEvent.type(screen.getByPlaceholderText("Ex. Mr John Doe"), "test creator");
+    );
+    userEvent.click(screen.getByRole('button', { name: /add new/i }));
+    userEvent.type(screen.getByPlaceholderText('Ex. Donations'), 'myplugin');
+    userEvent.type(
+      screen.getByPlaceholderText('Ex. This plugin provides UI for ....'),
+      'test description'
+    );
+    userEvent.type(
+      screen.getByPlaceholderText('Ex. Mr John Doe'),
+      'test creator'
+    );
     userEvent.click(screen.getByTestId('addonregister'));
     userEvent.click(screen.getByTestId('addonclose'));
-    
-    expect(screen.getByPlaceholderText("Ex. Donations")).toHaveValue("myplugin");
-    expect(screen.getByPlaceholderText("Ex. This plugin provides UI for ....")).toHaveValue("test description");
-    expect(screen.getByPlaceholderText("Ex. Mr John Doe")).toHaveValue("test creator");
-  })
-})
+
+    expect(screen.getByPlaceholderText('Ex. Donations')).toHaveValue(
+      'myplugin'
+    );
+    expect(
+      screen.getByPlaceholderText('Ex. This plugin provides UI for ....')
+    ).toHaveValue('test description');
+    expect(screen.getByPlaceholderText('Ex. Mr John Doe')).toHaveValue(
+      'test creator'
+    );
+  });
+});
