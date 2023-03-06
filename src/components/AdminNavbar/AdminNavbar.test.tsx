b8c23161f26b3787fdaba9e858af9e7c14ca2050
Rishav Jha <76212518+rishav-jha-mech@users.noreply.github.com>
2023-03-06 17:52:07 +0530

    Fixed navbar added tests (#534)
    * Fixed navbar added tests
    
    * Fixed the translation of All orgnaizations btn in AdminNavbar

--- src/components/AdminNavbar/AdminNavbar.test.tsx
+++ src/components/AdminNavbar/AdminNavbar.test.tsx
@@ -206,8 +206,11 @@ describe('Testing Admin Navbar', () => {
 
     expect(container.textContent).not.toBe('Loading data...');
     await wait();
-    const image = screen.getByTestId(/navbarOrgImagePresent/i);
-    expect(image).toBeInTheDocument();
+
+    const imageOptions = screen.getByTestId(/navbarOrgImagePresent/i);
+    const imageLogo = screen.getByTestId(/orgLogoPresent/i);
+    expect(imageLogo).toBeInTheDocument();
+    expect(imageOptions).toBeInTheDocument();
   });
 
   test('Should check if organisation image is not present', async () => {
@@ -225,7 +228,9 @@ describe('Testing Admin Navbar', () => {
 
     expect(container.textContent).not.toBe('Loading data...');
     await wait();
-    const image = screen.getByTestId(/navbarOrgImageAbsent/i);
-    expect(image).toBeInTheDocument();
+    const imageOptions = screen.getByTestId(/navbarOrgImageAbsent/i);
+    const imageLogo = screen.getByTestId(/orgLogoAbsent/i);
+    expect(imageLogo).toBeInTheDocument();
+    expect(imageOptions).toBeInTheDocument();
   });
 });
