1d36010bad184ae266c1cf1c2e5be5788f8c3bfb
Aashima wadhwa <73706697+aashimawadhwa@users.noreply.github.com>
2023-03-08 00:24:33 +0530

    Fixes #478 : Implemeneted calendar to the Event tab (#537)
    * calendar-implemented
    
    * calendar-implemented
    
    * fixed tests

--- src/screens/OrganizationEvents/OrganizationEvents.test.tsx
+++ src/screens/OrganizationEvents/OrganizationEvents.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { MockedProvider } from '@apollo/react-testing';
-import { act, render, screen } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import userEvent from '@testing-library/user-event';
 import 'jest-location-mock';
 import { I18nextProvider } from 'react-i18next';
 
@@ -73,21 +72,6 @@ async function wait(ms = 0) {
 }
 
 describe('Organisation Events Page', () => {
-  const searchData = {
-    byTitle: 'Dummy title',
-    byDescription: 'This is a dummy description',
-  };
-
-  const formData = {
-    title: 'Dummy Org',
-    description: 'This is a dummy organization',
-    startDate: '03/28/2022',
-    endDate: '04/15/2023',
-    location: 'New Delhi',
-    startTime: '02:00',
-    endTime: '06:00',
-  };
-
   global.alert = jest.fn();
 
   test('It is necessary to query the correct mock data.', async () => {
@@ -128,172 +112,6 @@ describe('Organisation Events Page', () => {
 
     expect(container.textContent).not.toBe('Loading data...');
     await wait();
-    expect(container.textContent).toMatch('Events');
-    expect(container.textContent).toMatch('Filter by Title');
-    expect(container.textContent).toMatch('Filter by Description');
-    expect(container.textContent).toMatch('Events');
     expect(window.location).toBeAt('/orglist');
-  });
-
-  test('No mock data', async () => {
-    render(
-      <MockedProvider>
-        <BrowserRouter>
-          <Provider store={store}>
-            <I18nextProvider i18n={i18nForTest}>
-              <OrganizationEvents />
-            </I18nextProvider>
-          </Provider>
-        </BrowserRouter>
-      </MockedProvider>
-    );
-
-    await wait();
-  });
-
-  test('Testing filter functionality', async () => {
-    render(
-      <MockedProvider addTypename={false} mocks={MOCKS}>
-        <BrowserRouter>
-          <Provider store={store}>
-            <I18nextProvider i18n={i18nForTest}>
-              <OrganizationEvents />
-            </I18nextProvider>
-          </Provider>
-        </BrowserRouter>
-      </MockedProvider>
-    );
-
-    await wait();
-
-    userEvent.type(screen.getByTestId('serachByTitle'), searchData.byTitle);
-    userEvent.type(
-      screen.getByTestId('serachByDescription'),
-      searchData.byDescription
-    );
-
-    expect(screen.getByTestId('serachByTitle')).toHaveValue(searchData.byTitle);
-    expect(screen.getByTestId('serachByDescription')).toHaveValue(
-      searchData.byDescription
-    );
-  });
-
-  test('Testing toggling of Create event modal', async () => {
-    render(
-      <MockedProvider addTypename={false} mocks={MOCKS}>
-        <BrowserRouter>
-          <Provider store={store}>
-            <I18nextProvider i18n={i18nForTest}>
-              <OrganizationEvents />
-            </I18nextProvider>
-          </Provider>
-        </BrowserRouter>
-      </MockedProvider>
-    );
-
-    await wait();
-
-    userEvent.click(screen.getByTestId('createEventModalBtn'));
-
-    userEvent.click(screen.getByTestId('createEventModalCloseBtn'));
-  });
-
-  test('Testing Create event modal', async () => {
-    render(
-      <MockedProvider addTypename={false} mocks={MOCKS}>
-        <BrowserRouter>
-          <Provider store={store}>
-            <I18nextProvider i18n={i18nForTest}>
-              <OrganizationEvents />
-            </I18nextProvider>
-          </Provider>
-        </BrowserRouter>
-      </MockedProvider>
-    );
-
-    await wait();
-
-    userEvent.click(screen.getByTestId('createEventModalBtn'));
-
-    userEvent.type(screen.getByPlaceholderText(/Enter Title/i), formData.title);
-    userEvent.type(
-      screen.getByPlaceholderText(/Enter Description/i),
-      formData.description
-    );
-    userEvent.type(
-      screen.getByPlaceholderText(/Enter Location/i),
-      formData.location
-    );
-    userEvent.type(
-      screen.getByPlaceholderText(/Enter Location/i),
-      formData.location
-    );
-    await act(async () => {
-      userEvent.type(
-        screen.getByPlaceholderText(/Start Date/i),
-        formData.startDate
-      );
-      userEvent.type(
-        screen.getByPlaceholderText(/End Date/i),
-        formData.endDate
-      );
-    });
-    userEvent.click(screen.getByTestId('alldayCheck'));
-    userEvent.click(screen.getByTestId('recurringCheck'));
-    userEvent.click(screen.getByTestId('ispublicCheck'));
-    userEvent.click(screen.getByTestId('registrableCheck'));
-
-    await wait();
-
-    expect(screen.getByPlaceholderText(/Enter Title/i)).toHaveValue(
-      formData.title
-    );
-    expect(screen.getByPlaceholderText(/Enter Description/i)).toHaveValue(
-      formData.description
-    );
-
-    expect(screen.getByTestId('alldayCheck')).not.toBeChecked();
-    expect(screen.getByTestId('recurringCheck')).toBeChecked();
-    expect(screen.getByTestId('ispublicCheck')).not.toBeChecked();
-    expect(screen.getByTestId('registrableCheck')).toBeChecked();
-
-    userEvent.click(screen.getByTestId('createEventBtn'));
-  });
-
-  test('Testing if the event is not for all day', async () => {
-    render(
-      <MockedProvider addTypename={false} mocks={MOCKS}>
-        <BrowserRouter>
-          <Provider store={store}>
-            <I18nextProvider i18n={i18nForTest}>
-              <OrganizationEvents />
-            </I18nextProvider>
-          </Provider>
-        </BrowserRouter>
-      </MockedProvider>
-    );
-
-    await wait();
-
-    userEvent.click(screen.getByTestId('createEventModalBtn'));
-    userEvent.type(screen.getByPlaceholderText(/Enter Title/i), formData.title);
-    userEvent.type(
-      screen.getByPlaceholderText(/Enter Description/i),
-      formData.description
-    );
-    userEvent.type(
-      screen.getByPlaceholderText(/Enter Location/i),
-      formData.location
-    );
-    userEvent.click(screen.getByTestId('alldayCheck'));
-    await wait();
-
-    userEvent.type(
-      screen.getByPlaceholderText(/Start Time/i),
-      formData.startTime
-    );
-    userEvent.type(screen.getByPlaceholderText(/End Time/i), formData.endTime);
-
-    userEvent.click(screen.getByTestId('createEventBtn'));
   });
 });
