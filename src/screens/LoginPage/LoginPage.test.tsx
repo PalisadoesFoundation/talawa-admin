439593ebc98e023044a0338871ef483ec56b22dd
arushi2715 <arushiagr1720@gmail.com>
2023-03-13 15:20:47 +0530

    [feat]- added test cases for password error warning

--- src/screens/LoginPage/LoginPage.test.tsx
+++ src/screens/LoginPage/LoginPage.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MockedProvider } from '@apollo/react-testing';
-import { act, queryByText, render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
