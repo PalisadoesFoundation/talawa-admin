eee4168201345da44b7b63669e73931bb0b86c2d
Sheguun <Oluwasegunmichael8@gmail.com>
2023-03-17 12:16:28 +0100

    Test init for pagination

--- src/screens/OrgList/OrgList.test.tsx
+++ src/screens/OrgList/OrgList.test.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { MockedProvider } from '@apollo/react-testing';
-import { act, render, screen } from '@testing-library/react';
+import {
+  act,
+  getByTestId,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react';
 import { Provider } from 'react-redux';
 import 'jest-localstorage-mock';
 import userEvent from '@testing-library/user-event';
@@ -15,6 +21,31 @@ import {
 import { store } from 'state/store';
 import i18nForTest from 'utils/i18nForTest';
 import { I18nextProvider } from 'react-i18next';
+import { faker } from '@faker-js/faker';
+
+const organizations = [];
+
+for (let x = 0; x < 10; x++) {
+  organizations.push({
+    _id: faker.datatype.uuid(),
+    image: '',
+    name: faker.name.fullName(),
+    creator: {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+    },
+    admins: [
+      {
+        _id: faker.datatype.uuid(),
+      },
+    ],
+    members: {
+      _id: faker.datatype.uuid(),
+    },
+    createdAt: faker.date.birthdate().toString().split('T')[0],
+    location: faker.address.city(),
+  });
+}
 
 const MOCKS = [
   {
@@ -23,27 +54,7 @@ const MOCKS = [
     },
     result: {
       data: {
-        organizationsConnection: [
-          {
-            _id: 1,
-            image: '',
-            name: 'Akatsuki',
-            creator: {
-              firstName: 'John',
-              lastName: 'Doe',
-            },
-            admins: [
-              {
-                _id: '123',
-              },
-            ],
-            members: {
-              _id: '234',
-            },
-            createdAt: '02/02/2022',
-            location: 'Washington DC',
-          },
-        ],
+        organizationsConnection: organizations,
       },
     },
   },
@@ -94,6 +105,29 @@ describe('Organisation List Page', () => {
   };
 
   global.alert = jest.fn();
+
+  // eslint-disable-next-line jest/no-focused-tests
+  test.only('The number of organizations rendered on the dom should be equal to the rowsPerPage', async () => {
+    const rowsPerPage = 5; //The default set in the OrgList component
+
+    const { getByTestId } = render(
+      <MockedProvider addTypename={false} mocks={MOCKS}>
+        <BrowserRouter>
+          <Provider store={store}>
+            <I18nextProvider i18n={i18nForTest}>
+              <OrgList />
+            </I18nextProvider>
+          </Provider>
+        </BrowserRouter>
+      </MockedProvider>
+    );
+
+    await waitFor(() => {
+      expect(getByTestId('organizations-list').childNodes.length).toBe(
+        rowsPerPage
+      );
+    });
+  });
 
   test('Correct mock data should be queried', async () => {
     const dataQuery1 = MOCKS[0]?.result?.data?.organizationsConnection;
