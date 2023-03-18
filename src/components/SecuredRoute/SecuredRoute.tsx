ac2c0bc69b8e62c900973cf3508eff70f5a0ed31
Theodora <107179983+TheoCathy@users.noreply.github.com>
2023-03-18 19:50:26 +0100

    Updated session timeout notification toast with already existing one (#734)
    * Added a toast to notify users that session has expired and redirects to the login page
    
    * Updated toast notification on session timeout  with already existing one
    
    * eliminated the use of  magic numbers
    
    * minor error correction on the timeout minutes
    
    ---------
    
    Co-authored-by: TheoCathy <theocathyne@gmail.com>

--- src/components/SecuredRoute/SecuredRoute.tsx
+++ src/components/SecuredRoute/SecuredRoute.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const SecuredRoute = (props: any): JSX.Element => {
   const isLoggedIn = localStorage.getItem('IsLoggedIn');
-
   return isLoggedIn === 'TRUE' ? (
     <>
       <Route {...props} />
@@ -12,5 +12,28 @@ const SecuredRoute = (props: any): JSX.Element => {
     <Redirect to="/" />
   );
 };
+
+const timeoutMinutes = 15;
+const timeoutMilliseconds = timeoutMinutes * 60 * 1000;
+
+const inactiveIntervalMin = 1;
+const inactiveIntervalMilsec = inactiveIntervalMin * 60 * 1000;
+let lastActive: number = Date.now();
+
+document.addEventListener('mousemove', () => {
+  lastActive = Date.now();
+});
+
+setInterval(() => {
+  const currentTime = Date.now();
+  const timeSinceLastActive = currentTime - lastActive;
+
+  if (timeSinceLastActive > timeoutMilliseconds) {
+    toast.warn('Kindly relogin as sessison has expired');
+
+    window.location.href = '/';
+    localStorage.setItem('IsLoggedIn', 'FALSE');
+  }
+}, inactiveIntervalMilsec);
 
 export default SecuredRoute;
