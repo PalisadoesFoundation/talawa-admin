6e6a55f874a5d2b4781f1c44babdaaa9aad780fb
Sheguun <oluwasegunmichael8@gmail.com>
2023-03-22 13:38:39 +0000

    add data-testid attributes to their markup being referenced from tests

--- src/components/OrgAdminListCard/OrgAdminListCard.tsx
+++ src/components/OrgAdminListCard/OrgAdminListCard.tsx
@@ -56,7 +56,7 @@ function OrgAdminListCard(props: OrgPeopleListCardProps): JSX.Element {
   };
   return (
     <>
-      <div className={styles.peoplelistdiv}>
+      <div className={styles.peoplelistdiv} data-testid="peoplelistitem">
         <Row className={styles.memberlist}>
           {props.memberImage ? (
             <img src={props.memberImage} className={styles.memberimg} />
