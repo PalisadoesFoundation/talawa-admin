b81023f233137c74f455d54da784d2b52055825e
Nwobodo Leonard <nwobodoe71@gmail.com>
2023-03-25 20:29:27 +0100

    Modify event visibility for non-admins (#796)
    * Modify event visibility for non-admins
    
    Signed-off-by: Ekene Nwobodo <Nwobodoe71@gmail.com>
    
    * Add test suites for events visibilty
    
    ---------
    
    Signed-off-by: Ekene Nwobodo <Nwobodoe71@gmail.com>

--- src/screens/OrganizationEvents/OrganizationEvents.tsx
+++ src/screens/OrganizationEvents/OrganizationEvents.tsx
@@ -13,7 +13,10 @@ import Calendar from 'components/EventCalendar/Calendar';
 
 import styles from './OrganizationEvents.module.css';
 import AdminNavbar from 'components/AdminNavbar/AdminNavbar';
-import { ORGANIZATION_EVENT_CONNECTION_LIST } from 'GraphQl/Queries/Queries';
+import {
+  ORGANIZATION_EVENT_CONNECTION_LIST,
+  ORGANIZATIONS_LIST,
+} from 'GraphQl/Queries/Queries';
 import { CREATE_EVENT_MUTATION } from 'GraphQl/Mutations/mutations';
 import { RootState } from 'state/reducers';
 import debounce from 'utils/debounce';
@@ -67,6 +70,13 @@ function OrganizationEvents(): JSX.Element {
       },
     }
   );
+
+  const { data: orgData } = useQuery(ORGANIZATIONS_LIST, {
+    variables: { id: currentUrl },
+  });
+
+  const userId = localStorage.getItem('id') as string;
+  const userRole = localStorage.getItem('UserType') as string;
 
   const [create, { loading: loading_2 }] = useMutation(CREATE_EVENT_MUTATION);
 
@@ -215,7 +225,12 @@ function OrganizationEvents(): JSX.Element {
               </Button>
             </Row>
           </div>
-          <Calendar eventData={data?.eventsByOrganizationConnection} />
+          <Calendar
+            eventData={data?.eventsByOrganizationConnection}
+            orgData={orgData}
+            userRole={userRole}
+            userId={userId}
+          />
         </Col>
       </Row>
       <Modal
