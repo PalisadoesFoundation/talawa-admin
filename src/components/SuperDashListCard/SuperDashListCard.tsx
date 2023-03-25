ebe5613bb3e45bfa223ccd41d04dffc60225d6eb
Mohammed Saad Abban <115649011+saadabban76@users.noreply.github.com>
2023-03-26 00:57:59 +0530

    Increased the OrgList Code Coverage (#786)
    * increased the orgList coverage to 95$
    
    * increased the orgList Code Coverage
    
    * increased the orgList Code Coverage
    
    * increased the orgList Code Coverage
    
    * fixed image rendering in orgPost page
    
    * increased the code coverage for orgList Component
    
    * increased code coverage to 97% in orgList Component
    
    ---------
    
    Co-authored-by: saadabban76 <115649011+saadabban76@users.noreply.github.com>

--- src/components/SuperDashListCard/SuperDashListCard.tsx
+++ src/components/SuperDashListCard/SuperDashListCard.tsx
@@ -14,7 +14,7 @@ interface SuperDashListCardProps {
   createdDate: string;
   image: string;
   admins: any;
-  members: string;
+  members: any;
 }
 
 function SuperDashListCard(props: SuperDashListCardProps): JSX.Element {
@@ -75,6 +75,7 @@ function SuperDashListCard(props: SuperDashListCardProps): JSX.Element {
                 onClick={handleClick}
                 disabled={
                   userType !== 'SUPERADMIN' &&
+                  props.admins.length > 0 &&
                   !props.admins.some((admin: any) => admin._id === userId)
                 }
               >
