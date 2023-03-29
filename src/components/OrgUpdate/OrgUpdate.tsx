a759859995b4555b6e6709487594f1ce6ff80db3
Grayce Muthui <graycemuthui20@gmail.com>
2023-03-29 18:11:09 +0300

    Fix the linter errors

--- src/components/OrgUpdate/OrgUpdate.tsx
+++ src/components/OrgUpdate/OrgUpdate.tsx
@@ -61,16 +61,13 @@ function OrgUpdate(props: OrgUpdateProps): JSX.Element {
       });
       /* istanbul ignore next */
       if (data) {
-
         toast.success('Successful updated', { autoClose });
         setTimeout(() => {
           window.location.reload();
         }, 2000);
 
         window.location.assign(`/orgdash/id=${props.orgid}`);
-
         toast.success('Successfully updated');
-
       }
     } catch (error: any) {
       /* istanbul ignore next */
