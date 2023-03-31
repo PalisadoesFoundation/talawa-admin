d25bf0296dfafe2e012cfdc31b80223154096089
sarazaToussaint <tmsaraza@gmail.com>
2023-03-31 15:24:23 +0200

    Remove unused codes

--- src/components/LandingPage/LandingPage.test.tsx
+++ src/components/LandingPage/LandingPage.test.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { render, waitFor } from '@testing-library/react';
+import { render, waitFor, screen } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 
 import LandingPage from './LandingPage';
