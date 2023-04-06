d943df9327e32746e2ca5ad7e015df59cb2e872f
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-04-06 09:14:43 +0530

    Fixed Organization Update

--- src/components/OrgUpdate/OrgUpdate.tsx
+++ src/components/OrgUpdate/OrgUpdate.tsx
@@ -16,21 +16,32 @@ interface OrgUpdateProps {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function OrgUpdate(props: OrgUpdateProps): JSX.Element {
   const currentUrl = window.location.href.split('=')[1];
-  const [formState, setFormState] = React.useState({
+
+  const [formState, setFormState] = React.useState<{
+    orgName: string;
+    orgDescrip: string;
+    location: string;
+    orgImage: string | null;
+  }>({
     orgName: '',
     orgDescrip: '',
     location: '',
-    orgImage: '',
+    orgImage: null,
   });
+
   const [publicchecked, setPublicChecked] = React.useState(true);
   const [visiblechecked, setVisibleChecked] = React.useState(false);
+
   const [login] = useMutation(UPDATE_ORGANIZATION_MUTATION);
+
   const { t } = useTranslation('translation', {
     keyPrefix: 'orgUpdate',
   });
+
   const { data, loading: loadingdata } = useQuery(ORGANIZATIONS_LIST, {
     variables: { id: currentUrl },
   });
+
   React.useEffect(() => {
     if (data) {
       setFormState({
@@ -38,14 +49,15 @@ function OrgUpdate(props: OrgUpdateProps): JSX.Element {
         orgName: data.organizations[0].name,
         orgDescrip: data.organizations[0].description,
         location: data.organizations[0].location,
-        orgImage: data.organizations[0].image,
       });
     }
   }, [data]);
+
   if (loadingdata) {
     return <div className="loader"></div>;
   }
-  const login_link = async () => {
+
+  const onSaveChangesClicked = async () => {
     try {
       const { data } = await login({
         variables: {
@@ -187,7 +199,7 @@ function OrgUpdate(props: OrgUpdateProps): JSX.Element {
               type="button"
               className={styles.greenregbtn}
               value="savechanges"
-              onClick={login_link}
+              onClick={onSaveChangesClicked}
             >
               {t('saveChanges')}
             </button>
