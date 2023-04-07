6bdc1bda28034cb8b47532f0ca7bdf13ce2778ca
rishav-jha-mech <rishav.rj74@gmail.com>
2023-04-07 11:00:31 +0530

    Added comments

--- src/utils/errorHandler.tsx
+++ src/utils/errorHandler.tsx
@@ -1,6 +1,10 @@
 import { TFunction } from 'react-i18next';
 import { toast } from 'react-toastify';
-
+/* 
+  This function is used to handle api errors in the application.
+  It takes in the error object and displays the error message to the user.
+  If the error is due to the Talawa API being unavailable, it displays a custom message.
+*/
 export const errorHandler = (
   t: TFunction<'translation', string>,
   error: any
