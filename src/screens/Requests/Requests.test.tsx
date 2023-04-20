7493a7973bb342ea9844685b9d82b334beba9140
Sumit Maithani <86047367+Sumitmaithani@users.noreply.github.com>
2023-04-20 20:35:25 +0530

    fixed the Inability to set user roles for specific organizations #556

--- src/screens/Requests/Requests.test.tsx
+++ src/screens/Requests/Requests.test.tsx
@@ -45,10 +45,34 @@ const MOCKS = [
                 name: 'ABC',
               },
             ],
+            adminFor: [
+              {
+                name: 'pokemn',
+                __typename: 'Organization',
+                _id: '6411aa2a97d5631eb0765945',
+              },
+            ],
             joinedOrganizations: [
               {
                 __typename: 'Organization',
                 _id: '6401ff65ce8e8406b8f07af1',
+                name: 'WHO',
+                members: [
+                  {
+                    _id: '6411a8f197d5631eb07658',
+                    firstName: 'John',
+                    lastName: 'Doe',
+                    email: 'John@gmail.com',
+                  },
+                ],
+                admins: [
+                  {
+                    _id: '6411a8f197d5631eb07658',
+                    firstName: 'John',
+                    lastName: 'Doe',
+                    email: 'John@gmail.com',
+                  },
+                ],
               },
             ],
           },
@@ -67,10 +91,34 @@ const MOCKS = [
                 name: 'ABC',
               },
             ],
+            adminFor: [
+              {
+                name: 'pokemn',
+                __typename: 'Organization',
+                _id: '6411aa2a97d5631eb0765945',
+              },
+            ],
             joinedOrganizations: [
               {
                 __typename: 'Organization',
                 _id: '6401ff65ce8e8406b8f07af2',
+                name: 'WHO',
+                members: [
+                  {
+                    _id: '6411a8f197d5631eb07658',
+                    firstName: 'John',
+                    lastName: 'Doe',
+                    email: 'John@gmail.com',
+                  },
+                ],
+                admins: [
+                  {
+                    _id: '6411a8f197d5631eb07658',
+                    firstName: 'John',
+                    lastName: 'Doe',
+                    email: 'John@gmail.com',
+                  },
+                ],
               },
             ],
           },
@@ -89,10 +137,34 @@ const MOCKS = [
                 name: 'ABC',
               },
             ],
+            adminFor: [
+              {
+                name: 'pokemn',
+                __typename: 'Organization',
+                _id: '6411aa2a97d5631eb0765945',
+              },
+            ],
             joinedOrganizations: [
               {
                 __typename: 'Organization',
                 _id: '6401ff65ce8e8406b8f07af3',
+                name: 'WHO',
+                members: [
+                  {
+                    _id: '6411a8f197d5631eb07658',
+                    firstName: 'John',
+                    lastName: 'Doe',
+                    email: 'John@gmail.com',
+                  },
+                ],
+                admins: [
+                  {
+                    _id: '6411a8f197d5631eb07658',
+                    firstName: 'John',
+                    lastName: 'Doe',
+                    email: 'John@gmail.com',
+                  },
+                ],
               },
             ],
           },
