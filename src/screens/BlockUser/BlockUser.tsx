e22b3cbc97779c1e76ec2776f388f9e4f005e910
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-05-08 21:24:48 +0530

    Block/Unblock Page Improvements (#885)
    * Added Radio Buttons
    
    * Optimized Queries
    
    * Optimized Query and Added Last Name Filter
    
    * Radio Button Working
    
    * Added Translations
    
    * Fixed Filter Name
    
    * Fixed Debounce
    
    * Fixed Asynchronous Debounce
    
    * Added Tests
    
    * Better Error and Loading State Handline
    
    * Fixed Loader
    
    * Fixed Loader
    
    * Modulrised Mock Data
    
    * Minor Change
    
    * Fixed Tests
    
    * Increase Coverage
    
    * User query replaced with OrgMemberConnection
    
    * Fixed Tests
    
    * Code Coverage Increase
    
    * Code Cov
    
    * Fixed Translations
    
    * Fixed Failing Tests

--- src/screens/BlockUser/BlockUser.tsx
+++ src/screens/BlockUser/BlockUser.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { useMutation, useQuery } from '@apollo/client';
 import { toast } from 'react-toastify';
 import { useSelector } from 'react-redux';
 
 import styles from './BlockUser.module.css';
-import { MEMBERS_LIST, USER_LIST } from 'GraphQl/Queries/Queries';
+import { BLOCK_PAGE_MEMBER_LIST } from 'GraphQl/Queries/Queries';
 import AdminNavbar from 'components/AdminNavbar/AdminNavbar';
 import { RootState } from 'state/reducers';
 import {
@@ -15,6 +15,20 @@ import {
 import { useTranslation } from 'react-i18next';
 import PaginationList from 'components/PaginationList/PaginationList';
 import { errorHandler } from 'utils/errorHandler';
+import debounce from 'utils/debounce';
+import { CircularProgress } from '@mui/material';
+
+interface Member {
+  _id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  organizationsBlockedBy: {
+    _id: string;
+    __typename: 'Organization';
+  }[];
+  __typename: 'User';
+}
 
 const Requests = () => {
   const { t } = useTranslation('translation', {
@@ -23,26 +37,30 @@ const Requests = () => {
 
   document.title = t('title');
 
-  const [membersArray, setMembersArray] = useState([]);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
-  const [searchByName, setSearchByName] = useState('');
 
   const currentUrl = window.location.href.split('=')[1];
 
   const appRoutes = useSelector((state: RootState) => state.appRoutes);
   const { targets, configUrl } = appRoutes;
 
+  const [membersData, setMembersData] = useState<Member[]>([]);
+  const [state, setState] = useState(0);
+
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const lastNameRef = useRef<HTMLInputElement>(null);
+
   const {
     data,
-    loading: spammers_loading,
+    loading,
     error,
-    refetch,
-  } = useQuery(USER_LIST);
-
-  const { data: data_2 } = useQuery(MEMBERS_LIST, {
+    refetch: memberRefetch,
+  } = useQuery(BLOCK_PAGE_MEMBER_LIST, {
     variables: {
-      id: currentUrl,
+      orgId: currentUrl,
+      firstName_contains: '',
+      lastName_contains: '',
     },
   });
 
@@ -50,22 +68,22 @@ const Requests = () => {
   const [unBlockUser] = useMutation(UNBLOCK_USER_MUTATION);
 
   useEffect(() => {
-    if (data_2) {
-      setMembersArray(data_2.organizations[0].members);
+    if (!data) {
+      setMembersData([]);
+      return;
     }
-  }, [data, data_2]);
 
-  if (spammers_loading) {
-    return <div className={styles.loader}></div>;
-  }
+    if (state === 0) {
+      setMembersData(data?.organizationsMemberConnection.edges);
+    } else {
+      const blockUsers = data?.organizationsMemberConnection.edges.filter(
+        (user: Member) =>
+          user.organizationsBlockedBy.some((org) => org._id === currentUrl)
+      );
 
-  const memberIds = membersArray.map((user: { _id: string }) => user._id);
-
-  /* istanbul ignore next */
-  const memberUsersData =
-    data?.users.filter((user: { _id: string }) =>
-      memberIds.includes(user._id)
-    ) ?? [];
+      setMembersData(blockUsers);
+    }
+  }, [state, data]);
 
   /* istanbul ignore next */
   const handleChangePage = (
@@ -83,15 +101,6 @@ const Requests = () => {
     setPage(0);
   };
 
-  const handleSearchByName = (e: any) => {
-    const { value } = e.target;
-    setSearchByName(value);
-
-    refetch({
-      filter: searchByName,
-    });
-  };
-
   const handleBlockUser = async (userId: string) => {
     try {
       const { data } = await blockUser({
@@ -103,7 +112,7 @@ const Requests = () => {
       /* istanbul ignore next */
       if (data) {
         toast.success(t('blockedSuccessfully'));
-        refetch();
+        memberRefetch();
       }
     } catch (error: any) {
       /* istanbul ignore next */
@@ -122,7 +131,7 @@ const Requests = () => {
       /* istanbul ignore next */
       if (data) {
         toast.success(t('Un-BlockedSuccessfully'));
-        refetch();
+        memberRefetch();
       }
     } catch (error: any) {
       /* istanbul ignore next */
@@ -132,8 +141,22 @@ const Requests = () => {
 
   /* istanbul ignore next */
   if (error) {
-    window.location.replace('/orglist');
+    console.error(error);
+
+    toast.error(error.message);
   }
+
+  const handleSearch = () => {
+    const filterData = {
+      orgId: currentUrl,
+      firstName_contains: firstNameRef.current?.value ?? '',
+      lastName_contains: lastNameRef.current?.value ?? '',
+    };
+
+    memberRefetch(filterData);
+  };
+
+  const handleSearchDebounced = debounce(handleSearch);
 
   return (
     <>
@@ -145,86 +168,128 @@ const Requests = () => {
               <h6 className={styles.searchtitle}>{t('searchByName')}</h6>
               <input
                 type="name"
-                id="orgname"
-                placeholder={t('orgName')}
-                data-testid="searchByName"
+                id="firstName"
+                placeholder={t('searchFirstName')}
+                name="firstName_contains"
+                data-testid="searchByFirstName"
                 autoComplete="off"
-                required
-                onChange={handleSearchByName}
+                onChange={handleSearchDebounced}
+                ref={firstNameRef}
               />
+
+              <input
+                type="name"
+                id="lastName"
+                placeholder={t('searchLastName')}
+                name="lastName_contains"
+                data-testid="searchByLastName"
+                autoComplete="off"
+                onChange={handleSearchDebounced}
+                ref={lastNameRef}
+              />
+
+              <div className={styles.radio_buttons} data-testid="usertypelist">
+                <input
+                  id="allusers"
+                  value="allusers"
+                  name="displaylist"
+                  type="radio"
+                  data-testid="allusers"
+                  defaultChecked={state == 0}
+                  onClick={() => {
+                    setState(0);
+                  }}
+                />
+                <label htmlFor="allusers">{t('allMembers')}</label>
+
+                <input
+                  id="blockedusers"
+                  value="blockedusers"
+                  name="displaylist"
+                  data-testid="blockedusers"
+                  type="radio"
+                  defaultChecked={state == 1}
+                  onClick={() => {
+                    setState(1);
+                  }}
+                />
+                <label htmlFor="blockedusers">{t('blockedUsers')}</label>
+              </div>
             </div>
           </div>
         </Col>
+
         <Col sm={8}>
           <div className={styles.mainpageright}>
             <Row className={styles.justifysp}>
               <p className={styles.logintitle}>{t('listOfUsers')}</p>
             </Row>
-            <div className={styles.list_box}>
-              <div className="table-responsive">
-                <table className={`table table-hover ${styles.userListTable}`}>
-                  <thead>
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">{t('name')}</th>
-                      <th scope="col">{t('email')}</th>
-                      <th scope="col" className="text-center">
-                        {t('block_unblock')}
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {(rowsPerPage > 0
-                      ? memberUsersData.slice(
-                          page * rowsPerPage,
-                          page * rowsPerPage + rowsPerPage
-                        )
-                      : memberUsersData
-                    ).map(
-                      (
-                        user: {
-                          _id: string;
-                          firstName: string;
-                          lastName: string;
-                          email: string;
-                          organizationsBlockedBy: [];
-                        },
-                        index: number
-                      ) => {
-                        return (
-                          <tr key={user._id} data-testid={`row${user._id}`}>
-                            <th scope="row">{page * 10 + (index + 1)}</th>
-                            <td>{`${user.firstName} ${user.lastName}`}</td>
-                            <td>{user.email}</td>
-                            <td className="text-center">
-                              {user.organizationsBlockedBy.some(
-                                (spam: any) => spam._id === currentUrl
-                              ) ? (
-                                <button
-                                  className="btn btn-danger"
-                                  onClick={() => handleUnBlockUser(user._id)}
-                                  data-testid={`unBlockUser${user._id}`}
-                                >
-                                  {t('unblock')}
-                                </button>
-                              ) : (
-                                <button
-                                  className="btn btn-success"
-                                  onClick={() => handleBlockUser(user._id)}
-                                  data-testid={`blockUser${user._id}`}
-                                >
-                                  {t('block')}
-                                </button>
-                              )}
-                            </td>
-                          </tr>
-                        );
-                      }
-                    )}
-                  </tbody>
-                </table>
+            {loading ? (
+              <div className={styles.loader}>
+                <CircularProgress />
               </div>
-            </div>
+            ) : (
+              <div className={styles.list_box}>
+                <div className="table-responsive">
+                  <table
+                    className={`table table-hover ${styles.userListTable}`}
+                  >
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">{t('name')}</th>
+                        <th scope="col">{t('email')}</th>
+                        <th scope="col" className="text-center">
+                          {t('block_unblock')}
+                        </th>
+                      </tr>
+                    </thead>
+
+                    <tbody>
+                      {
+                        /* istanbul ignore next */
+                        (rowsPerPage > 0
+                          ? membersData.slice(
+                              page * rowsPerPage,
+                              page * rowsPerPage + rowsPerPage
+                            )
+                          : membersData
+                        ).map((user, index: number) => {
+                          return (
+                            <tr key={user._id}>
+                              <th scope="row">{page * 10 + (index + 1)}</th>
+                              <td>{`${user.firstName} ${user.lastName}`}</td>
+                              <td>{user.email}</td>
+                              <td className="text-center">
+                                {user.organizationsBlockedBy.some(
+                                  (spam: any) => spam._id === currentUrl
+                                ) ? (
+                                  <button
+                                    className="btn btn-danger"
+                                    onClick={() => handleUnBlockUser(user._id)}
+                                    data-testid={`unBlockUser${user._id}`}
+                                  >
+                                    {t('unblock')}
+                                  </button>
+                                ) : (
+                                  <button
+                                    className="btn btn-success"
+                                    onClick={() => handleBlockUser(user._id)}
+                                    data-testid={`blockUser${user._id}`}
+                                  >
+                                    {t('block')}
+                                  </button>
+                                )}
+                              </td>
+                            </tr>
+                          );
+                        })
+                      }
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            )}
             <div>
               <table
                 style={{
@@ -236,7 +301,7 @@ const Requests = () => {
                 <tbody>
                   <tr>
                     <PaginationList
-                      count={memberUsersData.length}
+                      count={membersData.length}
                       rowsPerPage={rowsPerPage}
                       page={page}
                       onPageChange={handleChangePage}
