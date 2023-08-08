6856a1325cda3bd0ef1b7a8444bf4d901f663a73
SiddheshKukade <littlelord124@gmail.com>
2023-08-08 16:49:34 +0530

    fix: warnings and solves #951  & #948

--- src/components/AddOn/core/AddOnEntry/AddOnEntry.test.tsx
+++ src/components/AddOn/core/AddOnEntry/AddOnEntry.test.tsx
@@ -1,21 +1,21 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import { BrowserRouter } from 'react-router-dom';
-// import AddOnEntry from './AddOnEntry';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import AddOnEntry from './AddOnEntry';
 import {
   ApolloClient,
-  // ApolloProvider,
+  ApolloProvider,
   InMemoryCache,
   ApolloLink,
   HttpLink,
 } from '@apollo/client';
 
 import type { NormalizedCacheObject } from '@apollo/client';
-// import { Provider } from 'react-redux';
-// import { store } from 'state/store';
+import { Provider } from 'react-redux';
+import { store } from 'state/store';
 import { BACKEND_URL } from 'Constant/constant';
-// import i18nForTest from 'utils/i18nForTest';
-// import { I18nextProvider } from 'react-i18next';
+import i18nForTest from 'utils/i18nForTest';
+import { I18nextProvider } from 'react-i18next';
 
 const httpLink = new HttpLink({
   uri: BACKEND_URL,
@@ -24,40 +24,40 @@ const httpLink = new HttpLink({
   },
 });
 
-// const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-//   cache: new InMemoryCache(),
-//   link: ApolloLink.from([httpLink]),
-// });
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: ApolloLink.from([httpLink]),
+});
 
 describe('Testing AddOnEntry', () => {
-  // const props = {
-  //   id: 'string',
-  //   enabled: true,
-  //   title: 'string',
-  //   description: 'string',
-  //   createdBy: 'string',
-  //   component: 'string',
-  //   installed: true,
-  //   configurable: true,
-  //   modified: true,
-  //   isInstalled: true,
-  //   getInstalledPlugins: (): { sample: string } => {
-  //     return { sample: 'sample' };
-  //   },
-  // };
+  const props = {
+    id: 'string',
+    enabled: true,
+    title: 'string',
+    description: 'string',
+    createdBy: 'string',
+    component: 'string',
+    installed: true,
+    configurable: true,
+    modified: true,
+    isInstalled: true,
+    getInstalledPlugins: (): { sample: string } => {
+      return { sample: 'sample' };
+    },
+  };
 
   test('should render modal and take info to add plugin for registered organization', () => {
-    // const { getByTestId } = render(
-    // <ApolloProvider client={client}>
-    //   <Provider store={store}>
-    //     <BrowserRouter>
-    //       <I18nextProvider i18n={i18nForTest}>
-    //         {<AddOnEntry {...props} />}
-    //       </I18nextProvider>
-    //     </BrowserRouter>
-    //   </Provider>
-    // </ApolloProvider>
-    // );
-    // expect(getByTestId('AddOnEntry')).toBeInTheDocument();
+    const { getByTestId } = render(
+      <ApolloProvider client={client}>
+        <Provider store={store}>
+          <BrowserRouter>
+            <I18nextProvider i18n={i18nForTest}>
+              {<AddOnEntry uninstalledOrgs={[]} {...props} />}
+            </I18nextProvider>
+          </BrowserRouter>
+        </Provider>
+      </ApolloProvider>
+    );
+    expect(getByTestId('AddOnEntry')).toBeInTheDocument();
   });
 });
