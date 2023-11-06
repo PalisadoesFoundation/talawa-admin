8234779fb6ca4110f17f3cfb3b1ff35129f4f67b
Akhilender <akhilenderb9@gmail.com>
2023-11-06 19:37:11 +0530

    test: Achieve 100% Test Coverage and Fix Uncovered Lines
    - Improved the test coverage for the Loader component, addressing the previously uncovered lines and ensuring that all tests pass successfully.
    - Added a test to ensure that the component renders correctly with a custom 'sm' size. I verified that both the spinner-wrapper and
      spinner elements are present and that the correct class is applied.
    - Added a test to ensure that the component renders correctly with a 'lg' size. I verified that the spinner element has the appropriate class.
    - Added a test to ensure that the component renders correctly with an 'xl' size. I verified that the spinner element has the correct class.
    
    With these new tests, I now have 100% test coverage, and there are no more uncovered lines. The Loader component is thoroughly tested for
    different size scenarios, and all tests pass successfully.
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>

--- src/components/Loader/Loader.test.tsx
+++ src/components/Loader/Loader.test.tsx
@@ -15,4 +15,35 @@ describe('Testing Loader component', () => {
     expect(screen.getByTestId('spinner-wrapper')).toBeInTheDocument();
     expect(screen.getByTestId('spinner')).toBeInTheDocument();
   });
+
+  test('Component should render on custom sizes', () => {
+    render(
+      <BrowserRouter>
+        <Loader size="sm" />
+      </BrowserRouter>
+    );
+
+    expect(screen.getByTestId('spinner-wrapper')).toBeInTheDocument();
+    expect(screen.getByTestId('spinner')).toBeInTheDocument();
+    expect(screen.getByTestId('spinner')).toHaveClass(
+      '_spinnerSm_1vy2z_21 spinner-border text-primary'
+    );
+  });
+
+  test('Component should render with large size', () => {
+    render(<Loader size="lg" />);
+    const spinner = screen.getByTestId('spinner');
+    expect(spinner).toHaveClass(
+      '_spinnerLg_1vy2z_15 spinner-border text-primary'
+    );
+  });
+
+  test('renders with extra-large size', () => {
+    render(<Loader size="xl" />);
+    const spinner = screen.getByTestId('spinner');
+
+    expect(spinner).toHaveClass(
+      '_spinnerXl_1vy2z_9 spinner-border text-primary'
+    );
+  });
 });
