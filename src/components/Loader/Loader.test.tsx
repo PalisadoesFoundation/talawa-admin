69ebdd1618426d42a3c3fc1ac6f6bbca0dd6cfcb
Akhilender <akhilenderb9@gmail.com>
2023-11-11 19:37:15 +0530

    test: Improving the tests on the Loader Component
    - Added a new aspect in the test-case1
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>

--- src/components/Loader/Loader.test.tsx
+++ src/components/Loader/Loader.test.tsx
@@ -6,8 +6,10 @@ describe('Testing Loader component', () => {
   test('Component should be rendered properly', () => {
     render(<Loader />);
 
-    expect(screen.getByTestId('spinner-wrapper')).toBeInTheDocument();
+    const spinnerWrapper = screen.getByTestId('spinner-wrapper');
+    expect(spinnerWrapper).toBeInTheDocument();
     expect(screen.getByTestId('spinner')).toBeInTheDocument();
+    expect(spinnerWrapper).toHaveClass('_spinner_wrapper_1vy2z_1');
   });
 
   test('Component should render on custom sizes', () => {
