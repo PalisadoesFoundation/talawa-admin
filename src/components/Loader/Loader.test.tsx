93b7572a74072cfbc87bbe7052aaf45c77cc7626
Akhilender <akhilenderb9@gmail.com>
2023-11-11 19:05:40 +0530

    fix: Linting
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>

--- src/components/Loader/Loader.test.tsx
+++ src/components/Loader/Loader.test.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Loader from './Loader';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Loader from "./Loader";
 
-describe('Testing Loader component', () => {
-  test('Component should be rendered properly', () => {
+describe("Testing Loader component", () => {
+  test("Component should be rendered properly", () => {
     render(<Loader />);
 
-    expect(screen.getByTestId('spinner-wrapper')).toBeInTheDocument();
-    expect(screen.getByTestId('spinner')).toBeInTheDocument();
+    expect(screen.getByTestId("spinner-wrapper")).toBeInTheDocument();
+    expect(screen.getByTestId("spinner")).toBeInTheDocument();
   });
 
-  test('Component should render on custom sizes', () => {
+  test("Component should render on custom sizes", () => {
     render(<Loader size="sm" />);
 
-    const spinner = screen.getByTestId('spinner');
-    expect(screen.getByTestId('spinner-wrapper')).toBeInTheDocument();
+    const spinner = screen.getByTestId("spinner");
+    expect(screen.getByTestId("spinner-wrapper")).toBeInTheDocument();
     expect(spinner).toBeInTheDocument();
     expect(spinner).toHaveClass(
-      '_spinnerSm_1vy2z_21 spinner-border text-primary'
+      "_spinnerSm_1vy2z_21 spinner-border text-primary"
     );
   });
 
-  test('Component should render with large size', () => {
+  test("Component should render with large size", () => {
     render(<Loader size="lg" />);
 
-    const spinner = screen.getByTestId('spinner');
-    expect(screen.getByTestId('spinner-wrapper')).toBeInTheDocument();
+    const spinner = screen.getByTestId("spinner");
+    expect(screen.getByTestId("spinner-wrapper")).toBeInTheDocument();
     expect(spinner).toBeInTheDocument();
     expect(spinner).toHaveClass(
-      '_spinnerLg_1vy2z_15 spinner-border text-primary'
+      "_spinnerLg_1vy2z_15 spinner-border text-primary"
     );
   });
 });
