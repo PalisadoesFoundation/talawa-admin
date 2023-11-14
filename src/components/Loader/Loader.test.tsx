09f89c89bf8cdfd2eae4e328e6f92c1552a6291b
Akhilender <akhilenderb9@gmail.com>
2023-11-14 14:35:07 +0530

    Fixed: Removed runtime-generated classes from testing
    - Eliminated all checks for classes associated with bootstrap components assigned at runtime.
    - Recognized that validating classes assigned during runtime is inappropriate.
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>

--- src/components/Loader/Loader.test.tsx
+++ src/components/Loader/Loader.test.tsx
@@ -6,25 +6,21 @@ describe('Testing Loader component', () => {
   test('Component should be rendered properly', () => {
     render(<Loader />);
 
-    const spinnerWrapper = screen.getByTestId('spinner-wrapper');
-    expect(spinnerWrapper).toBeInTheDocument();
+    expect(screen.getByTestId('spinner-wrapper')).toBeInTheDocument();
     expect(screen.getByTestId('spinner')).toBeInTheDocument();
-    expect(spinnerWrapper).toHaveClass('_spinner_wrapper_1vy2z_1');
   });
 
   test('Component should render on custom sizes', () => {
     render(<Loader size="sm" />);
 
-    expect(screen.getByTestId('spinner')).toHaveClass(
-      '_spinnerSm_1vy2z_21 spinner-border text-primary'
-    );
+    expect(screen.getByTestId('spinner-wrapper')).toBeInTheDocument();
+    expect(screen.getByTestId('spinner')).toBeInTheDocument();
   });
 
   test('Component should render with large size', () => {
     render(<Loader size="lg" />);
 
-    expect(screen.getByTestId('spinner')).toHaveClass(
-      '_spinnerLg_1vy2z_15 spinner-border text-primary'
-    );
+    expect(screen.getByTestId('spinner-wrapper')).toBeInTheDocument();
+    expect(screen.getByTestId('spinner')).toBeInTheDocument();
   });
 });
