6a67e7da4d34993bcc21a52aa508384981123249
SiddheshKukade <littlelord124@gmail.com>
2023-11-17 21:20:47 +0530

    Added test for entry file2

--- src/components/Advertisements/Advertisement.test.tsx
+++ src/components/Advertisements/Advertisement.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getByTestId, getByText, render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 import {
   ApolloClient,
