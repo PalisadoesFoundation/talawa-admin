7ee1bf287dea3dcbe0180442cd94b796a82a00fb
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2023-12-09 23:08:55 +0530

    adding test for increasing code coverage

--- src/components/Advertisements/core/AdvertisementEntry/AdvertisementEntry.test.tsx
+++ src/components/Advertisements/core/AdvertisementEntry/AdvertisementEntry.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import {
   ApolloClient,
@@ -56,5 +56,50 @@ describe('Testing Advertisement Entry Component', () => {
     expect(getByTestId('AdEntry')).toBeInTheDocument();
     expect(getAllByText('POPUP')[0]).toBeInTheDocument();
     expect(getAllByText('Advert1')[0]).toBeInTheDocument();
+  });
+  it('should open and close the dropdown when options button is clicked', () => {
+    const { getByTestId, queryByText, getAllByText } = render(
+      <ApolloProvider client={client}>
+        <Provider store={store}>
+          <BrowserRouter>
+            <I18nextProvider i18n={i18nForTest}>
+              <AdvertisementEntry
+                endDate={new Date()}
+                startDate={new Date()}
+                id="1"
+                key={1}
+                link="google.com"
+                name="Advert1"
+                orgId="1"
+                type="POPUP"
+              />
+            </I18nextProvider>
+          </BrowserRouter>
+        </Provider>
+      </ApolloProvider>
+    );
+
+    // Test initial rendering
+    expect(getByTestId('AdEntry')).toBeInTheDocument();
+    expect(getAllByText('POPUP')[0]).toBeInTheDocument();
+    expect(getAllByText('Advert1')[0]).toBeInTheDocument();
+
+    // Test dropdown functionality
+    const optionsButton = getByTestId('moreiconbtn');
+
+    // Initially, the dropdown should not be visible
+    expect(queryByText('Edit')).toBeNull();
+
+    // Click to open the dropdown
+    fireEvent.click(optionsButton);
+
+    // After clicking the button, the dropdown should be visible
+    expect(queryByText('Edit')).toBeInTheDocument();
+
+    // Click again to close the dropdown
+    fireEvent.click(optionsButton);
+
+    // After the second click, the dropdown should be hidden again
+    expect(queryByText('Edit')).toBeNull();
   });
 });
