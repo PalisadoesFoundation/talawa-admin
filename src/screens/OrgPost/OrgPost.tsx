b0e60211c70757d4b6e277fc04401f1a1dbcd5a8
Shekhar Patel <90516956+duplixx@users.noreply.github.com>
2023-12-16 02:25:07 +0530

    Update OrgPost.tsx

--- src/screens/OrgPost/OrgPost.tsx
+++ src/screens/OrgPost/OrgPost.tsx
@@ -413,9 +413,8 @@ function orgPost(): JSX.Element {
                   <img src={postformState.addMedia} alt="Post Image Preview" />
                 ) : (
                   <video controls data-testid="videoPreview">
-                    
-                    <source src={postformState.addMedia} type={file.type} />
-                    ({t('tag')})
+                    <source src={postformState.addMedia} type={file.type} />(
+                    {t('tag')})
                   </video>
                 )}
                 <button
