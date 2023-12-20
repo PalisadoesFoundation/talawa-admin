f9a452b252769ed8c09defeab91dcafcb89c5652
ShatilKhan <shatilshahriar009@gmail.com>
2023-12-20 21:53:31 +0600

    Code Coverage for DeleteOrg.tsx

--- src/components/DeleteOrg/DeleteOrg.test.tsx
+++ src/components/DeleteOrg/DeleteOrg.test.tsx
@@ -63,6 +63,61 @@ describe('Delete Organization Component', () => {
     expect(window.location).toBeAt('/orgsetting/id=123');
   });
 
+  test('should render delete button if canDelete is true', () => {
+    render(
+      <MockedProvider addTypename={false} link={link}>
+        <BrowserRouter>
+          <Provider store={store}>
+            <I18nextProvider i18n={i18nForTest}>
+              <DeleteOrg />
+            </I18nextProvider>
+          </Provider>
+        </BrowserRouter>
+      </MockedProvider>
+    );
+    const deleteButton = screen.getByTestId(/openDeleteModalBtn/i);
+    expect(deleteButton).toBeInTheDocument();
+  });
+
+  test('should open delete modal when delete button is clicked', () => {
+    render(
+      <MockedProvider addTypename={false} link={link}>
+        <BrowserRouter>
+          <Provider store={store}>
+            <I18nextProvider i18n={i18nForTest}>
+              <DeleteOrg />
+            </I18nextProvider>
+          </Provider>
+        </BrowserRouter>
+      </MockedProvider>
+    );
+    const deleteButton = screen.getByTestId(/openDeleteModalBtn/i);
+    fireEvent.click(deleteButton);
+    const deleteModal = screen.getByTestId(/orgDeleteModal/i);
+    expect(deleteModal).toBeInTheDocument();
+  });
+
+  test('should close delete modal when close button is clicked', async () => {
+    render(
+      <MockedProvider addTypename={false} link={link}>
+        <BrowserRouter>
+          <Provider store={store}>
+            <I18nextProvider i18n={i18nForTest}>
+              <DeleteOrg />
+            </I18nextProvider>
+          </Provider>
+        </BrowserRouter>
+      </MockedProvider>
+    );
+    const deleteButton = screen.getByTestId(/openDeleteModalBtn/i);
+    fireEvent.click(deleteButton);
+    const closeButton = screen.getByTestId(/closeDelOrgModalBtn/i);
+    fireEvent.click(closeButton);
+    await act(async () => {
+      expect(screen.queryByTestId(/orgDeleteModal/i)).not.toBeInTheDocument();
+    });
+  });
+
   test('Delete organization functionality should work properly', async () => {
     window.location.assign('/orgsetting/id=123');
     localStorage.setItem('UserType', 'SUPERADMIN');
