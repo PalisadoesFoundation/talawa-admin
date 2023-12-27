4e0a15dd00b5810bb620bcdada58aba0b48dde77
Shekhar Patel <90516956+duplixx@users.noreply.github.com>
2023-12-28 02:45:39 +0530

    Update OrgPost.test.tsx

--- src/screens/OrgPost/OrgPost.test.tsx
+++ src/screens/OrgPost/OrgPost.test.tsx
@@ -14,6 +14,7 @@ import { CREATE_POST_MUTATION } from 'GraphQl/Mutations/mutations';
 import i18nForTest from 'utils/i18nForTest';
 import { StaticMockLink } from 'utils/StaticMockLink';
 import { ToastContainer } from 'react-toastify';
+import { debug } from 'jest-preview';
 
 const MOCKS = [
   {
@@ -45,11 +46,49 @@ const MOCKS = [
               likeCount: 0,
               commentCount: 0,
               comments: [],
-              pinned: false,
+              pinned: true,
               likedBy: [],
             },
             {
               _id: '6411e54835d7ba2344a78e29',
+              title: 'posttwo',
+              text: 'Tis is the post two',
+              imageUrl: null,
+              videoUrl: null,
+              createdAt: '2023-08-24T09:26:56.524+00:00',
+              creator: {
+                _id: '640d98d9eb6a743d75341067',
+                firstName: 'Aditya',
+                lastName: 'Shelke',
+                email: 'adidacreator1@gmail.com',
+              },
+              likeCount: 0,
+              commentCount: 0,
+              pinned: false,
+              likedBy: [],
+              comments: [],
+            },
+            {
+              _id: '6411e54835d7ba2344a78e30',
+              title: 'posttwo',
+              text: 'Tis is the post two',
+              imageUrl: null,
+              videoUrl: null,
+              createdAt: '2023-08-24T09:26:56.524+00:00',
+              creator: {
+                _id: '640d98d9eb6a743d75341067',
+                firstName: 'Aditya',
+                lastName: 'Shelke',
+                email: 'adidacreator1@gmail.com',
+              },
+              likeCount: 0,
+              commentCount: 0,
+              pinned: true,
+              likedBy: [],
+              comments: [],
+            },
+            {
+              _id: '6411e54835d7ba2344a78e31',
               title: 'posttwo',
               text: 'Tis is the post two',
               imageUrl: null,
@@ -144,7 +183,7 @@ describe('Organisation Post Page', () => {
       },
       likeCount: 0,
       commentCount: 0,
-      pinned: false,
+      pinned: true,
       likedBy: [],
       comments: [],
     });
@@ -170,8 +209,14 @@ describe('Organisation Post Page', () => {
     userEvent.type(screen.getByTestId('modalTitle'), formData.posttitle);
 
     userEvent.type(screen.getByTestId('modalinfo'), formData.postinfo);
-    userEvent.upload(screen.getByTestId('addMediaField'), formData.postImage);
-    userEvent.upload(screen.getByTestId('addMediaField'), formData.postVideo);
+    userEvent.upload(
+      screen.getByTestId('organisationImage'),
+      formData.postImage
+    );
+    userEvent.upload(
+      screen.getByTestId('organisationImage'),
+      formData.postVideo
+    );
 
     userEvent.click(screen.getByTestId('createPostBtn'));
 
@@ -200,7 +245,7 @@ describe('Organisation Post Page', () => {
       });
     }
     await debounceWait();
-    userEvent.type(screen.getByPlaceholderText(/Search By/i), 'postone');
+    userEvent.type(screen.getByPlaceholderText(/Search By/i), 'postone{enter}');
     await debounceWait();
     const sortDropdown = screen.getByTestId('sort');
     userEvent.click(sortDropdown);
@@ -331,7 +376,7 @@ describe('Organisation Post Page', () => {
     fireEvent.click(createPostBtn);
   }, 15000);
 
-  test('Create post, preview image, and close preview', async () => {
+  test('Create post and preview', async () => {
     render(
       <MockedProvider addTypename={false} link={link}>
         <BrowserRouter>
@@ -346,7 +391,6 @@ describe('Organisation Post Page', () => {
     );
 
     await wait();
-
     userEvent.click(screen.getByTestId('createPostModalBtn'));
 
     const postTitleInput = screen.getByTestId('modalTitle');
@@ -356,24 +400,18 @@ describe('Organisation Post Page', () => {
     fireEvent.change(postInfoTextarea, {
       target: { value: 'Test post information' },
     });
-
-    // Simulate uploading an image
-    const imageFile = new File(['image content'], 'image.png', {
+    const file = new File(['image content'], 'image.png', {
       type: 'image/png',
     });
-    const imageInput = screen.getByTestId('addMediaField');
-    userEvent.upload(imageInput, imageFile);
+    const input = screen.getByTestId('organisationImage');
+    userEvent.upload(input, file);
 
-    // Check if the image is displayed
-    const imagePreview = await screen.findByAltText('Post Image Preview');
-    expect(imagePreview).toBeInTheDocument();
+    await screen.findByAltText('Post Image Preview');
+    expect(screen.getByAltText('Post Image Preview')).toBeInTheDocument();
 
-    // Check if the close button for the image works
-    const closeButton = screen.getByTestId('mediaCloseButton');
-    fireEvent.click(closeButton);
-
-    // Check if the image is removed from the preview
-    expect(imagePreview).not.toBeInTheDocument();
+    const createPostBtn = screen.getByTestId('createPostBtn');
+    fireEvent.click(createPostBtn);
+    debug();
   }, 15000);
 
   test('Modal opens and closes', async () => {
@@ -426,8 +464,8 @@ describe('Organisation Post Page', () => {
     // Check if input fields and buttons are present
     expect(screen.getByTestId('modalTitle')).toBeInTheDocument();
     expect(screen.getByTestId('modalinfo')).toBeInTheDocument();
-    expect(screen.getByTestId('addMediaField')).toBeInTheDocument();
-    // expect(screen.getByTestId('organisationVideo')).toBeInTheDocument();
+    expect(screen.getByTestId('organisationImage')).toBeInTheDocument();
+    expect(screen.getByTestId('organisationVideo')).toBeInTheDocument();
     expect(screen.getByTestId('createPostBtn')).toBeInTheDocument();
   });
 
@@ -488,16 +526,16 @@ describe('Organisation Post Page', () => {
     const file = new File(['image content'], 'image.png', {
       type: 'image/png',
     });
-    const input = screen.getByTestId('addMediaField');
+    const input = screen.getByTestId('organisationImage');
     userEvent.upload(input, file);
 
     await screen.findByAltText('Post Image Preview');
     expect(screen.getByAltText('Post Image Preview')).toBeInTheDocument();
 
-    const closeButton = screen.getByTestId('mediaCloseButton');
+    const closeButton = screen.getByTestId('closePreview');
     fireEvent.click(closeButton);
   }, 15000);
-  test('Create post, preview video, and close preview', async () => {
+  test('Create post, preview image, and close preview', async () => {
     await act(async () => {
       render(
         <MockedProvider addTypename={false} link={link}>
@@ -528,19 +566,21 @@ describe('Organisation Post Page', () => {
         type: 'video/mp4',
       });
 
-      userEvent.upload(screen.getByTestId('addMediaField'), videoFile);
+      const videoInput = screen.getByTestId('organisationVideo');
+      fireEvent.change(videoInput, {
+        target: {
+          files: [videoFile],
+        },
+      });
 
       // Check if the video is displayed
-      const videoPreview = await screen.findByTestId('mediaPreview');
+      const videoPreview = await screen.findByTestId('videoPreview');
       expect(videoPreview).toBeInTheDocument();
 
       // Check if the close button for the video works
-      const closeVideoPreviewButton = screen.getByTestId('mediaCloseButton');
+      const closeVideoPreviewButton = screen.getByTestId('videoclosebutton');
       fireEvent.click(closeVideoPreviewButton);
-
-      // Check if the video is not present after closing
-      const videoPreviewAfterClose = screen.queryByTestId('mediaPreview');
-      expect(videoPreviewAfterClose).toBeNull();
+      expect(videoPreview).not.toBeInTheDocument();
     });
   });
 });
