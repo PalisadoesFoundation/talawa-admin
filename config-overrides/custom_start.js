4abbbff249a5f1694ca3f0ba0d509a402ded922d
Ta.run <97682967+Tarunmeena0901@users.noreply.github.com>
2023-12-30 00:12:55 +0530

    Update custom_start.js

--- config-overrides/custom_start.js
+++ config-overrides/custom_start.js
@@ -4,7 +4,7 @@ const { spawn } = require('child_process');
 const react_script_start = 'npx react-scripts start';
 const react_app_rewired_start = 'npx react-app-rewired start';
 
-if (process.env.LOGS_CONFIG_OVERRIDE === "YES") {
+if (process.env.ALLOW_LOGS === "YES") {
   // Execute the npm command
   spawn(react_app_rewired_start, { stdio: 'inherit', shell: true });
 
