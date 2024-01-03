e2d895a22a0f8cba14ad597bf6bde26b561aee8f
gauravsingh94 <gauravrajputsingh94@gmail.com>
2024-01-03 23:17:29 +0530

    Fixed-Issue-#1266 Bug Report When a new post is added, the latest post section in the organization's dashboard becomes distorted Updated

--- src/components/OrganizationDashCards/CardItem.tsx
+++ src/components/OrganizationDashCards/CardItem.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { ReactComponent as EventsIcon } from 'assets/svgs/events.svg';
 import { ReactComponent as PostsIcon } from 'assets/svgs/post.svg';
 import { ReactComponent as MarkerIcon } from 'assets/svgs/location.svg';
-import { ReactComponent as DateIcon } from 'assets/svgs/date.svg';
-import { ReactComponent as UserIcon } from 'assets/svgs/user.svg';
 import dayjs from 'dayjs';
 import styles from './CardItem.module.css';
 import { PersonAddAlt1Rounded } from '@mui/icons-material';
@@ -37,48 +35,37 @@ const cardItem = (props: InterfaceCardItem): JSX.Element => {
             )
           )}
         </div>
-        <span className={styles.title}>{`${title}`}</span>
+        <div className={styles.content}>
+          <span className={styles.title}>{`${title}`}</span>
 
-        <div className={styles.rightCard}>
-          {creator && (
-            <small className={styles.creator}>
-              <UserIcon
-                title="Post Creator"
-                fill="var(--bs-primary)"
-                width={20}
-                height={20}
-              />{' '}
-              {'  '}
-              <a>
-                {creator.firstName} {creator.lastName}
-              </a>
-            </small>
-          )}
+          <div className={styles.rightCard}>
+            {time && (
+              <span className={styles.time}>
+                <span>Posted on: </span>
+                {dayjs(time).format('D MMM YYYY')}
+              </span>
+            )}
+            {creator && (
+              <small className={styles.creator}>
+                <span>Author: </span>
+                <a>
+                  {creator.firstName} {creator.lastName}
+                </a>
+              </small>
+            )}
 
-          {location && (
-            <span className={styles.location}>
-              <MarkerIcon
-                title="Event Location"
-                fill="var(--bs-primary)"
-                width={20}
-                height={20}
-              />{' '}
-              {location}
-            </span>
-          )}
-          {time && (
-            <span className={styles.time}>
-              {type === 'Event' && (
-                <DateIcon
-                  title="Event Date"
-                  fill="#4cd964"
+            {location && (
+              <span className={styles.location}>
+                <MarkerIcon
+                  title="Event Location"
+                  fill="var(--bs-primary)"
                   width={20}
                   height={20}
-                />
-              )}{' '}
-              {dayjs(time).format('MMM D, YYYY')}
-            </span>
-          )}
+                />{' '}
+                {location}
+              </span>
+            )}
+          </div>
         </div>
       </div>
     </>
