e5f0e1e06238fd872dbf8c46773fda7c6e3610b3
Disha <dishatalreja1202@gmail.com>
2024-01-08 19:32:18 +0530

    fix: failing tests

--- src/components/Advertisements/core/AdvertisementRegister/AdvertisementRegister.test.tsx
+++ src/components/Advertisements/core/AdvertisementRegister/AdvertisementRegister.test.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  render,
-  fireEvent,
-  screen,
-  waitFor,
-  act,
-} from '@testing-library/react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 
 import {
   ApolloClient,
