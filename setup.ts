3f30d8d37cddf4bf652c975a6933516e9ae3392c
Disha <dishatalreja1202@gmail.com>
2024-01-11 17:09:06 +0530

    fix: check api is running, existing comments are not removed, if configurations exists, default Y is assumed, automatically insert missing values

--- setup.ts
+++ setup.ts
@@ -1,12 +1,46 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import dotenv from 'dotenv';
 import fs from 'fs';
 import inquirer from 'inquirer';
+import http from 'http';
 
 dotenv.config();
 
-function abort(): void {
-  console.log('\nSetup process aborted. 🫠');
-  process.exit(1);
+async function checkConnection(url: string): Promise<any> {
+  console.log('\nChecking Talawa-API connection....');
+  let isConnected = false;
+  await new Promise((resolve) => {
+    const req = http.request(url, (res) => {
+      return res;
+    });
+
+    req.on('response', (res) => {
+      resolve(res);
+      isConnected = true;
+      console.log('\nConnection to Talawa-API successful! 🎉');
+    });
+
+    req.on('error', (err) => {
+      console.log(
+        '\nTalawa-API service is unavailable. Is it running? Check your network connectivity too.'
+      );
+      resolve(err);
+      isConnected = false;
+    });
+  });
+  return isConnected;
+}
+
+async function askForTalawaApiUrl(): Promise<string> {
+  const { endpoint } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'endpoint',
+      message: 'Enter your talawa-api endpoint:',
+      default: 'http://localhost:4000/graphql/',
+    },
+  ]);
+  return endpoint;
 }
 
 // Check if all the fields in .env.example are present in .env
@@ -15,8 +49,10 @@ function checkEnvFile(): void {
   const envSample = dotenv.parse(fs.readFileSync('.env.example'));
   const misplaced = Object.keys(envSample).filter((key) => !(key in env));
   if (misplaced.length > 0) {
-    console.log('Please copy the contents of .env.example to .env file');
-    abort();
+    const config = dotenv.parse(fs.readFileSync('.env.example'));
+    misplaced.map((key) =>
+      fs.appendFileSync('.env', `${key}=${config[key]}\n`)
+    );
   }
 }
 
@@ -29,39 +65,54 @@ async function main(): Promise<void> {
   console.log('Welcome to the Talawa Admin setup! 🚀');
 
   if (!fs.existsSync('.env')) {
-    fs.copyFileSync('.env.example', '.env');
+    // fs.copyFileSync('.env.example', '.env');
+    fs.openSync('.env', 'w');
+    const config = dotenv.parse(fs.readFileSync('.env.example'));
+    for (const key in config) {
+      fs.appendFileSync('.env', `${key}=${config[key]}\n`);
+    }
   } else {
     checkEnvFile();
   }
+
+  let shouldSetTalawaApiUrl: boolean;
 
   if (process.env.REACT_APP_TALAWA_URL) {
     console.log(
       `\nEndpoint for accessing talawa-api graphql service already exists with the value:\n${process.env.REACT_APP_TALAWA_URL}`
     );
+    shouldSetTalawaApiUrl = true;
+  } else {
+    const { shouldSetTalawaApiUrlResponse } = await inquirer.prompt({
+      type: 'confirm',
+      name: 'shouldSetTalawaApiUrlResponse',
+      message: 'Would you like to set up talawa-api endpoint?',
+      default: true,
+    });
+    shouldSetTalawaApiUrl = shouldSetTalawaApiUrlResponse;
   }
 
-  const { shouldSetTalawaApiUrl } = await inquirer.prompt({
-    type: 'confirm',
-    name: 'shouldSetTalawaApiUrl',
-    message: 'Would you like to set up talawa-api endpoint?',
-    default: true,
-  });
-
   if (shouldSetTalawaApiUrl) {
-    const { endpoint } = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'endpoint',
-        message: 'Enter your talawa-api endpoint:',
-        default: 'http://localhost:4000/graphql/',
-      },
-    ]);
-    const config = dotenv.parse(fs.readFileSync('.env'));
-    config.REACT_APP_TALAWA_URL = endpoint;
-    fs.writeFileSync('.env', '');
-    for (const key in config) {
-      fs.appendFileSync('.env', `${key}=${config[key]}\n`);
+    let isConnected = false,
+      endpoint = '';
+
+    while (!isConnected) {
+      endpoint = await askForTalawaApiUrl();
+      const url = new URL(endpoint);
+      isConnected = await checkConnection(url.origin);
     }
+
+    const talawaApiUrl = dotenv.parse(
+      fs.readFileSync('.env')
+    ).REACT_APP_TALAWA_URL;
+
+    fs.readFile('.env', 'utf8', (err, data) => {
+      const result = data.replace(
+        `REACT_APP_TALAWA_URL=${talawaApiUrl}`,
+        `REACT_APP_TALAWA_URL=${endpoint}`
+      );
+      fs.writeFileSync('.env', result, 'utf8');
+    });
   }
 
   const { shouldUseRecaptcha } = await inquirer.prompt({
@@ -72,29 +123,38 @@ async function main(): Promise<void> {
   });
 
   if (shouldUseRecaptcha) {
-    const config = dotenv.parse(fs.readFileSync('.env'));
-    config.REACT_APP_USE_RECAPTCHA = 'yes';
-    fs.writeFileSync('.env', '');
-    for (const key in config) {
-      fs.appendFileSync('.env', `${key}=${config[key]}\n`);
-    }
+    const useRecaptcha = dotenv.parse(
+      fs.readFileSync('.env')
+    ).REACT_APP_USE_RECAPTCHA;
+
+    fs.readFile('.env', 'utf8', (err, data) => {
+      const result = data.replace(
+        `REACT_APP_USE_RECAPTCHA=${useRecaptcha}`,
+        `REACT_APP_USE_RECAPTCHA=yes`
+      );
+      fs.writeFileSync('.env', result, 'utf8');
+    });
+    let shouldSetRecaptchaSiteKey: boolean;
     if (process.env.REACT_APP_RECAPTCHA_SITE_KEY) {
       console.log(
         `\nreCAPTCHA site key already exists with the value ${process.env.REACT_APP_RECAPTCHA_SITE_KEY}`
       );
+      shouldSetRecaptchaSiteKey = true;
+    } else {
+      const { shouldSetRecaptchaSiteKeyResponse } = await inquirer.prompt({
+        type: 'confirm',
+        name: 'shouldSetRecaptchaSiteKeyResponse',
+        message: 'Would you like to set up a reCAPTCHA site key?',
+        default: true,
+      });
+      shouldSetRecaptchaSiteKey = shouldSetRecaptchaSiteKeyResponse;
     }
-    const { shouldSetRecaptchaSiteKey } = await inquirer.prompt({
-      type: 'confirm',
-      name: 'shouldSetRecaptchaSiteKey',
-      message: 'Would you like to set up a reCAPTCHA site key?',
-      default: true,
-    });
 
     if (shouldSetRecaptchaSiteKey) {
-      const { recaptchaSiteKey } = await inquirer.prompt([
+      const { recaptchaSiteKeyInput } = await inquirer.prompt([
         {
           type: 'input',
-          name: 'recaptchaSiteKey',
+          name: 'recaptchaSiteKeyInput',
           message: 'Enter your reCAPTCHA site key:',
           validate: async (input: string): Promise<boolean | string> => {
             if (validateRecaptcha(input)) {
@@ -105,13 +165,35 @@ async function main(): Promise<void> {
         },
       ]);
 
-      const config = dotenv.parse(fs.readFileSync('.env'));
-      config.REACT_APP_RECAPTCHA_SITE_KEY = recaptchaSiteKey;
-      fs.writeFileSync('.env', '');
-      for (const key in config) {
-        fs.appendFileSync('.env', `${key}=${config[key]}\n`);
-      }
+      const recaptchaSiteKey = dotenv.parse(
+        fs.readFileSync('.env')
+      ).REACT_APP_RECAPTCHA_SITE_KEY;
+
+      fs.readFile('.env', 'utf8', (err, data) => {
+        const result = data.replace(
+          `REACT_APP_RECAPTCHA_SITE_KEY=${recaptchaSiteKey}`,
+          `REACT_APP_RECAPTCHA_SITE_KEY=${recaptchaSiteKeyInput}`
+        );
+        fs.writeFileSync('.env', result, 'utf8');
+      });
     }
+  }
+
+  const { shouldLogErrors } = await inquirer.prompt({
+    type: 'confirm',
+    name: 'shouldLogErrors',
+    message:
+      'Would you like to log Compiletime and Runtime errors in the console?',
+    default: true,
+  });
+
+  if (shouldLogErrors) {
+    const logErrors = dotenv.parse(fs.readFileSync('.env')).ALLOW_LOGS;
+
+    fs.readFile('.env', 'utf8', (err, data) => {
+      const result = data.replace(`ALLOW_LOGS=${logErrors}`, 'ALLOW_LOGS=YES');
+      fs.writeFileSync('.env', result, 'utf8');
+    });
   }
 
   console.log(
