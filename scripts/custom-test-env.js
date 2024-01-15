19fdedbd3655a7e03b5b7e0294392c8185fbf622
Shubh152 <shubhj013@gmail.com>
2024-01-15 14:37:11 +0530

    update packages and test environment

--- scripts/custom-test-env.js
+++ scripts/custom-test-env.js
@@ -1,11 +1,11 @@
 import Environment from 'jest-environment-jsdom';
 import { TextEncoder, TextDecoder } from 'util';
-
+const testEnvironment = Environment.TestEnvironment;
 /**
  * A custom environment to set the TextEncoder and TextDecoder variables, that is required by @pdfme during testing.
  * Providing a polyfill to the environment for the same
  */
-export default class CustomTestEnvironment extends Environment {
+export default class CustomTestEnvironment extends testEnvironment {
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   async setup() {
     await super.setup();
