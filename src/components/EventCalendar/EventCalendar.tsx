103598ea6bb554021076871dcdc78fe7a4e23860
karthxk07 <karthikkalyanam07@gmail.com>
2024-01-17 15:38:42 +0530

    Implemented logic

--- src/components/EventCalendar/EventCalendar.tsx
+++ src/components/EventCalendar/EventCalendar.tsx
@@ -39,6 +39,11 @@ enum Role {
   SUPERADMIN = 'SUPERADMIN',
   ADMIN = 'ADMIN',
 }
+
+enum ViewType {
+  DAY = 'day',
+  MONTH = 'month',
+}
 interface InterfaceIEventAttendees {
   userId: string;
   user?: string;
@@ -71,13 +76,16 @@ const Calendar: React.FC<InterfaceCalendarProps> = ({
     'November',
     'December',
   ];
+  const hours = Array.from({ length: 24 }, (_, index) => index);
 
   const today = new Date();
+  const [currentDate, setCurrentDate] = useState(today.getDate());
   const [currentMonth, setCurrentMonth] = useState(today.getMonth());
   const [currentYear, setCurrentYear] = useState(today.getFullYear());
   const [events, setEvents] = useState<InterfaceEvent[] | null>(null);
   const [expanded, setExpanded] = useState<number>(-1);
   const [windowWidth, setWindowWidth] = useState<number>(window.screen.width);
+  const [viewType, setViewType] = useState<string>(ViewType.MONTH);
 
   useEffect(() => {
     function handleResize(): void {
@@ -129,6 +137,10 @@ const Calendar: React.FC<InterfaceCalendarProps> = ({
     setEvents(data);
   }, [eventData, orgData, userRole, userId]);
 
+  const handleChangeView = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+    setViewType(e.target.value);
+  };
+
   const handlePrevMonth = (): void => {
     if (currentMonth === 0) {
       setCurrentMonth(11);
@@ -146,10 +158,206 @@ const Calendar: React.FC<InterfaceCalendarProps> = ({
       setCurrentMonth(currentMonth + 1);
     }
   };
+
+  const handlePrevDate = (): void => {
+    if (currentDate > 1) {
+      setCurrentDate(currentDate - 1);
+    } else {
+      if (currentMonth > 0) {
+        const lastDayOfPrevMonth = new Date(
+          currentYear,
+          currentMonth,
+          0
+        ).getDate();
+        setCurrentDate(lastDayOfPrevMonth);
+        setCurrentMonth(currentMonth - 1);
+      } else {
+        setCurrentDate(31);
+        setCurrentMonth(11);
+        setCurrentYear(currentYear - 1);
+      }
+    }
+  };
+
+  const handleNextDate = (): void => {
+    const lastDayOfCurrentMonth = new Date(
+      currentYear,
+      currentMonth - 1,
+      0
+    ).getDate();
+    if (currentDate < lastDayOfCurrentMonth) {
+      setCurrentDate(currentDate + 1);
+    } else {
+      if (currentMonth < 12) {
+        setCurrentDate(1);
+        setCurrentMonth(currentMonth + 1);
+      } else {
+        setCurrentDate(1);
+        setCurrentMonth(1);
+        setCurrentYear(currentYear + 1);
+      }
+    }
+  };
+
   const handleTodayButton = (): void => {
     setCurrentYear(today.getFullYear());
     setCurrentMonth(today.getMonth());
+    setCurrentDate(today.getDate());
   };
+
+  const renderHours = (): JSX.Element => {
+    const allDayEventsList: any = events
+      ?.filter((datas) => {
+        const currDate = new Date(currentYear, currentMonth, currentDate);
+        if (
+          datas.startTime == undefined &&
+          datas.startDate == dayjs(currDate).format('YYYY-MM-DD')
+        ) {
+          return datas;
+        }
+      })
+      .map((datas: InterfaceEvent) => {
+        return (
+          <EventListCard
+            key={datas._id}
+            id={datas._id}
+            eventLocation={datas.location}
+            eventName={datas.title}
+            eventDescription={datas.description}
+            regDate={datas.startDate}
+            regEndDate={datas.endDate}
+            startTime={datas.startTime}
+            endTime={datas.endTime}
+            allDay={datas.allDay}
+            recurring={datas.recurring}
+            isPublic={datas.isPublic}
+            isRegisterable={datas.isRegisterable}
+          />
+        );
+      });
+
+    return (
+      <>
+        {allDayEventsList.length > 0 && (
+          <div
+            style={{
+              borderBottom: '1px solid gray',
+              borderBottomRightRadius: '5px',
+              padding: '3px',
+            }}
+          >
+            {allDayEventsList}
+          </div>
+        )}
+        {hours.map((hour, index) => {
+          const toggleExpand = (index: number): void => {
+            if (expanded === index) {
+              setExpanded(-1);
+            } else {
+              setExpanded(index);
+            }
+          };
+
+          const timeEventsList: any = events
+            ?.filter((datas) => {
+              const currDate = new Date(currentYear, currentMonth, currentDate);
+              if (
+                datas.startTime?.slice(0, 2) ==
+                  dayjs().hour(hour).format('HH') &&
+                datas.startDate == dayjs(currDate).format('YYYY-MM-DD')
+              ) {
+                return datas;
+              }
+            })
+            .map((datas: InterfaceEvent) => {
+              return (
+                <EventListCard
+                  key={datas._id}
+                  id={datas._id}
+                  eventLocation={datas.location}
+                  eventName={datas.title}
+                  eventDescription={datas.description}
+                  regDate={datas.startDate}
+                  regEndDate={datas.endDate}
+                  startTime={datas.startTime}
+                  endTime={datas.endTime}
+                  allDay={datas.allDay}
+                  recurring={datas.recurring}
+                  isPublic={datas.isPublic}
+                  isRegisterable={datas.isRegisterable}
+                />
+              );
+            });
+
+          return (
+            <div
+              key={hour}
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                borderBottom: '1px solid gray',
+                position: 'relative',
+                minHeight: '70px',
+                borderBottomRightRadius: '5px',
+              }}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'flex-end',
+                }}
+              >
+                <p
+                  style={{
+                    top: '20px',
+                    left: '-5px',
+                    position: 'relative',
+                  }}
+                >{`${hour}:00`}</p>
+              </div>
+              <div style={{ width: 1 }}></div>
+              <div style={{ position: 'relative' }}>
+                <div
+                  className={
+                    expanded === index ? styles.expand_list_container : ''
+                  }
+                  style={{
+                    margin: '10px',
+                    width: 'fit-content',
+                  }}
+                >
+                  <div
+                    className={
+                      expanded === index
+                        ? styles.expand_event_list
+                        : styles.event_list
+                    }
+                  >
+                    {expanded === index
+                      ? timeEventsList
+                      : timeEventsList?.slice(0, 1)}
+                  </div>
+                  {(timeEventsList?.length > 1 ||
+                    (windowWidth <= 700 && timeEventsList?.length > 0)) && (
+                    <button
+                      className={styles.btn__more}
+                      onClick={() => {
+                        toggleExpand(index);
+                      }}
+                    >
+                      {expanded === index ? 'View less' : 'View all'}
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </>
+    );
+  };
+
   const renderDays = (): JSX.Element[] => {
     const monthStart = new Date(currentYear, currentMonth, 1);
     const monthEnd = new Date(currentYear, currentMonth + 1, 0);
@@ -259,18 +467,23 @@ const Calendar: React.FC<InterfaceCalendarProps> = ({
       <div className={styles.calendar__header}>
         <Button
           className={styles.button}
-          onClick={handlePrevMonth}
+          onClick={viewType == ViewType.DAY ? handlePrevDate : handlePrevMonth}
           data-testid="prevmonth"
         >
           <ChevronLeft />
         </Button>
+
         <div
           className={styles.calendar__header_month}
           data-testid="current-date"
         >
+          {viewType == ViewType.DAY ? `${currentDate}` : ``}{' '}
           {months[currentMonth]} {currentYear}
         </div>
-        <Button className={styles.button} onClick={handleNextMonth}>
+        <Button
+          className={styles.button}
+          onClick={viewType == ViewType.DAY ? handleNextDate : handleNextMonth}
+        >
           <ChevronRight />
         </Button>
         <div>
@@ -282,16 +495,30 @@ const Calendar: React.FC<InterfaceCalendarProps> = ({
             Today
           </Button>
         </div>
+        <div style={{ flexGrow: 1 }}></div>
+        <div>
+          <select className={styles.selectType} onChange={handleChangeView}>
+            <option value="month">Month</option>
+            <option value="day">Day</option>
+          </select>
+        </div>
       </div>
-
-      <div className={styles.calendar__weekdays}>
-        {weekdays.map((weekday, index) => (
-          <div key={index} className={styles.weekday}>
-            {weekday}
+      <div style={{ height: '80vh', overflowY: 'scroll', padding: '10px' }}>
+        {viewType == ViewType.MONTH ? (
+          <div>
+            <div className={styles.calendar__weekdays}>
+              {weekdays.map((weekday, index) => (
+                <div key={index} className={styles.weekday}>
+                  {weekday}
+                </div>
+              ))}
+            </div>
+            <div className={styles.calendar__days}>{renderDays()}</div>
           </div>
-        ))}
+        ) : (
+          <div className={styles.clendar__hours}>{renderHours()}</div>
+        )}
       </div>
-      <div className={styles.calendar__days}>{renderDays()}</div>
     </div>
   );
 };
