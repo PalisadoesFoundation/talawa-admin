3624dc81f2f7b8dbebd86bb8bbc38795aa59a7cd
Akhilender Bongirwar <112749383+akhilender-bongirwar@users.noreply.github.com>
2024-01-18 02:55:30 +0530

    fix: Removed the unused `organizationUserBelongsTo` field (#1432)
    * fix: Removed the unused `organizationUserBelongsTo` field
    
    - Removed `organizationUserBelongsTo` field from schema to align it with the talawa-api schema.
    - Removed the field use from the memberDetail.tsx screen as it is not being modified.
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>
    
    * fix: fixed the failing test
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>
    
    ---------
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>

--- src/GraphQl/Queries/Queries.ts
+++ src/GraphQl/Queries/Queries.ts
@@ -423,9 +423,6 @@ export const USER_DETAILS = gql`
       joinedOrganizations {
         _id
       }
-      organizationUserBelongsTo {
-        _id
-      }
       organizationsBlockedBy {
         _id
       }
