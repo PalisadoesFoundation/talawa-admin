97a858c45703907fd71b3b5998a9faa97c5f7343
chandel-aman <chandel.s.aman@gmail.com>
2024-01-26 02:45:32 +0530

    Add pre-commit hook to check localStorage usage

---
+++ scripts/githooks/check-localstorage-usage.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+import { readFileSync } from 'fs';
+import path from 'path';
+
+import { execSync } from 'child_process';
+
+const getModifiedFiles = () => {
+  try {
+    const result = execSync('git diff --cached --name-only', {
+      encoding: 'utf-8',
+    });
+    return result.trim().split('\n');
+  } catch (error) {
+    console.error('Error fetching modified files:', error.message);
+    process.exit(1);
+  }
+};
+
+const files = getModifiedFiles();
+
+const filesWithLocalStorage = [];
+
+const checkLocalStorageUsage = (file) => {
+  if (!file) {
+    return;
+  }
+
+  const scriptPath = path.resolve(new URL(import.meta.url).pathname);
+
+  if (
+    file === scriptPath ||
+    path.basename(file) === 'check-localstorage-usage.js'
+  ) {
+    return;
+  }
+
+  const content = readFileSync(file, 'utf-8');
+
+  if (
+    content.includes('localStorage.getItem') ||
+    content.includes('localStorage.setItem') ||
+    content.includes('localStorage.removeItem')
+  ) {
+    filesWithLocalStorage.push(file);
+  }
+};
+
+files.forEach(checkLocalStorageUsage);
+
+if (filesWithLocalStorage.length > 0) {
+  console.error('\x1b[31m%s\x1b[0m', '\nError: Found usage of localStorage');
+  console.error('\nFiles with localStorage usage:');
+  filesWithLocalStorage.forEach((file) => console.error(file));
+
+  console.info(
+    '\x1b[34m%s\x1b[0m',
+    '\nInfo: Consider using custom hook functions.'
+  );
+  console.info(
+    'Please use the getItem, setItem, and removeItem functions provided by the custom hook useLocalStorage.\n'
+  );
+
+  process.exit(1);
+}
