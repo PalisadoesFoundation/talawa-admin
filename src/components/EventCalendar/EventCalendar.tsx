d507f6a0a0b12369fbd0398f0e974a2875dee844
Amit Sharma <amit.sharma.mec21@itbhu.ac.in>
2024-02-07 23:57:44 +0530

    Updated the Scroll Bar of the Event Calendar and the Side NavBar.

--- src/components/EventCalendar/EventCalendar.tsx
+++ src/components/EventCalendar/EventCalendar.tsx
@@ -571,7 +571,7 @@ const Calendar: React.FC<InterfaceCalendarProps> = ({
           </Dropdown>
         </div>
       </div>
-      <div style={{ height: '80vh', overflowY: 'scroll', padding: '10px' }}>
+      <div className={styles.calendar__scroll}>
         {viewType == ViewType.MONTH ? (
           <div>
             <div className={styles.calendar__weekdays}>
