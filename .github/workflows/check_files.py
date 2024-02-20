ff6f1dbaa7a8f828257a12ef268ca627bdca4b17
Shekhar Patel <90516956+duplixx@users.noreply.github.com>
2024-02-21 01:10:35 +0530

    Update check_files.py

--- .github/workflows/check_files.py
+++ .github/workflows/check_files.py
@@ -26,8 +26,7 @@ def _get_changed_files(base_branch, pr_branch):
             command,
             shell=True,
             stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
+            stderr=subprocess.PIPE
         )
         output, error = process.communicate()
     except Exception as e:
