7828c40038b1eb3cea610a23531d3d5cf39584f5
Akhilender Bongirwar <112749383+akhilender-bongirwar@users.noreply.github.com>
2024-02-28 17:59:21 +0530

    feat: Integrated Dicebear Library To Enhance Security (#1585)
    * feat: Integrated Dicebear Library To Enhance Security
    
    - Added the Dicebear library as a dependency to the project.
    - Imported the necessary functions from the library.
    - Created a new `Avatar` component that utilizes the Dicebear library locally.
    - Updated the component to accept `name`, `alt`, and `size` as props.
    - Utilized the Dicebear library to generate avatars with initials.
    - Removed direct HTTP API calls from the codebase.
    - Replaced instances of direct HTTP API calls with the newly created `Avatar` component.
    - Updated the exisiting tests and ensured the tests are still valid.
    - Mitigated security risks associated with SSRF and XSS attacks by eliminating direct HTTP API calls.
    - By using the Dicebear library locally, we reduce potential vulnerabilities.
    - Ensured no other functionality is affected.
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>
    
    * fix: Reverted changes in the tests
    
    - Reverted the Avatar component in all the tests as they are redundant in tests except where it is needed.
    - Ensured all the remaning tests are succesfully passing.
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>
    
    * fix: Added dynamic style
    
    - Handled case where no alt tag is provided by adding default alt value.
    - dynamic rendering for className for img tag in avatar component
    - Dynamic rendering for test-id
    - Replaced suitable changes for the above properties added
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>
    
    * fix: modified test
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>
    
    * fix: resolved conflict
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>
    
    ---------
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>

--- src/components/LeftDrawer/LeftDrawer.tsx
+++ src/components/LeftDrawer/LeftDrawer.tsx
@@ -11,6 +11,7 @@ import styles from './LeftDrawer.module.css';
 import { useMutation } from '@apollo/client';
 import { REVOKE_REFRESH_TOKEN } from 'GraphQl/Mutations/mutations';
 import useLocalStorage from 'utils/useLocalstorage';
+import Avatar from 'components/Avatar/Avatar';
 
 export interface InterfaceLeftDrawerProps {
   hideDrawer: boolean | null;
@@ -115,8 +116,8 @@ const leftDrawer = ({
               {userImage && userImage !== 'null' ? (
                 <img src={userImage} alt={`profile picture`} />
               ) : (
-                <img
-                  src={`https://api.dicebear.com/5.x/initials/svg?seed=${firstName}%20${lastName}`}
+                <Avatar
+                  name={`${firstName} ${lastName}`}
                   alt={`dummy picture`}
                 />
               )}
