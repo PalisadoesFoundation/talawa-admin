4b94cc61ed850d2029844d6be8c4b92fd919c280
Aarish Shah Mohsin <aarishshah1@gmail.com>
2024-03-02 01:25:38 +0530

    Refactored setup.ts and Added Tests (#1645)
    * Removed all eslint-disable lines and added workflow for chechecking
    
    * fixed typo
    
    * fixed typo
    
    * Added tests for AddOnStore
    
    * Fixed all tests
    
    * fixed test
    
    * Refactored setup.ts and added tests
    
    ---------
    
    Co-authored-by: Aarish Shah Mohsin <aarish@localhost.localdomain>

---
+++ src/setup/askForTalawaApiUrl/askForTalawaApiUrl.ts
@@ -0,0 +1,13 @@
+import inquirer from 'inquirer';
+
+export async function askForTalawaApiUrl(): Promise<string> {
+  const { endpoint } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'endpoint',
+      message: 'Enter your talawa-api endpoint:',
+      default: 'http://localhost:4000/graphql/',
+    },
+  ]);
+  return endpoint;
+}
