035b0e35399e824b21bded21c7eceaaa3d793896
Akhilender Bongirwar <112749383+akhilender-bongirwar@users.noreply.github.com>
2024-03-02 20:52:19 +0530

    refactor: Created Unified Login/Register Component for User and Admin Portals (#1616)
    * refactor: Created Unified Login/Register Component for User and Admin Portals
    
    - Resolves the problem of having separate Login and Register Components for User and Admin portals, causing UI inconsistencies.
    - Created a shared Login/Register Component that can be used seamlessly across both User and Admin portals.
    - Ensured consistency in UI elements to eliminate the need for synchronization between the portals.
    - Maintained existing authentication logic and redirection functionality.
    - Thoroughly tested the unified component to validate its integration and verify the intended behavior for both portals.
    - Also increased margin and padding in the Login and register page to match the figma file
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>
    
    * refactor: replaced all '/user' route to '/' route
    
    - Removed '/user' route as we have unified login and register for admin and user.
    - Made all necessary changes which addressed the using only single `/` endpoint.
    - Ensured it is functioning as expected.
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>
    
    ---------
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>

--- src/components/LoginPortalToggle/LoginPortalToggle.tsx
+++ src/components/LoginPortalToggle/LoginPortalToggle.tsx
@@ -1,31 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './LoginPortalToggle.module.css';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import { NavLink } from 'react-router-dom';
 
-function loginPortalToggle(): JSX.Element {
+interface InterfaceLoginPortalToggleProps {
+  onToggle: (role: 'admin' | 'user') => void;
+}
+
+function loginPortalToggle({
+  onToggle,
+}: InterfaceLoginPortalToggleProps): JSX.Element {
   const { t } = useTranslation('translation', { keyPrefix: 'loginPage' });
+  const [activeRole, setActiveRole] = useState<'admin' | 'user'>('admin');
+
+  const handleNavLinkClick = (role: 'admin' | 'user'): void => {
+    onToggle(role);
+    setActiveRole(role);
+  };
 
   return (
     <Row className="mb-4">
       <Col>
         <NavLink
-          className={styles.navLinkClass}
-          activeClassName={styles.activeLink}
+          className={`${styles.navLinkClass} ${activeRole === 'admin' && styles.activeLink}`}
           exact
           to="/"
+          onClick={() => handleNavLinkClick('admin')}
         >
           {t('admin')}
         </NavLink>
       </Col>
       <Col>
         <NavLink
-          className={styles.navLinkClass}
-          activeClassName={styles.activeLink}
+          className={`${styles.navLinkClass} ${activeRole === 'user' && styles.activeLink}`}
           exact
-          to="/user"
+          to="/"
+          onClick={() => handleNavLinkClick('user')}
         >
           {t('user')}
         </NavLink>
