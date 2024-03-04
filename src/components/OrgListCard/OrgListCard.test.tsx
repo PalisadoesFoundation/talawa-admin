c94bcb5f68eaa591d48d9b60657d3d912234a62d
Amit Sharma <amit.sharma.mec21@itbhu.ac.in>
2024-03-04 23:56:27 +0530

    Redesigned the organization card.

--- src/components/OrgListCard/OrgListCard.test.tsx
+++ src/components/OrgListCard/OrgListCard.test.tsx
@@ -78,9 +78,11 @@ describe('Testing the Super Dash List', () => {
       </MockedProvider>,
     );
     expect(screen.getByAltText(/Dogs Care image/i)).toBeInTheDocument();
-    expect(screen.getByText('Admins:')).toBeInTheDocument();
-    expect(screen.getByText('Members:')).toBeInTheDocument();
+    expect(screen.getByText(/Admins:/i)).toBeInTheDocument();
+    expect(screen.getByText(/Members:/i)).toBeInTheDocument();
     expect(screen.getByText('Dogs Care')).toBeInTheDocument();
+    expect(screen.getByText(/Sample City/i)).toBeInTheDocument();
+    expect(screen.getByText(/123 Sample Street/i)).toBeInTheDocument();
     userEvent.click(screen.getByTestId(/manageBtn/i));
     removeItem('id');
   });
