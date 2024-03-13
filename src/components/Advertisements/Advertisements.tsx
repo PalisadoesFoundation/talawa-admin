b6410b21b99b8c815e452292bb104bceddf7b772
aditya <adiraiar022@gmail.com>
2024-03-14 00:16:08 +0530

    new commit

--- src/components/Advertisements/Advertisements.tsx
+++ src/components/Advertisements/Advertisements.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import PropTypes from 'react';
 import styles from './Advertisements.module.css';
 import { useQuery } from '@apollo/client';
 import { ADVERTISEMENTS_GET } from 'GraphQl/Queries/Queries';
@@ -8,6 +7,7 @@ import { useTranslation } from 'react-i18next';
 import AdvertisementEntry from './core/AdvertisementEntry/AdvertisementEntry';
 import AdvertisementRegister from './core/AdvertisementRegister/AdvertisementRegister';
 import { useParams } from 'react-router-dom';
+
 export default function advertisements(): JSX.Element {
   const { data: advertisementsData, loading: loadingAdvertisements } =
     useQuery(ADVERTISEMENTS_GET);
@@ -17,104 +17,72 @@ export default function advertisements(): JSX.Element {
   document.title = t('title');
 
   if (loadingAdvertisements) {
-    return (
-      <>
-        <div data-testid="AdEntryStore" className={styles.loader}></div>
-      </>
-    );
+    return <div data-testid="AdEntryStore" className={styles.loader}></div>;
   }
 
   return (
-    <>
-      <Row>
-        <Col col={8}>
-          <div className={styles.justifysp}>
-            <p className={styles.logintitle}>{t('pHeading')}</p>
-            <AdvertisementRegister />
-            <Tabs
-              defaultActiveKey="archievedAds"
-              id="uncontrolled-tab-example"
-              className="mb-3"
-            >
-              <Tab eventKey="activeAds" title={t('activeAds')}>
-                {advertisementsData?.advertisementsConnection
-                  .filter((ad: any) => ad.organization._id == currentOrgId)
+    <Row>
+      <Col col={8}>
+        <div className={styles.justifysp}>
+          <p className={styles.logintitle}>{t('pHeading')}</p>
+          <AdvertisementRegister />
+          <Tabs
+            defaultActiveKey="archievedAds"
+            id="uncontrolled-tab-example"
+            className="mb-3"
+          >
+            <Tab eventKey="activeAds" title={t('activeAds')}>
+              {advertisementsData?.advertisementsConnection
+                .filter((ad: any) => ad.organization._id === currentOrgId)
+                .filter((ad: any) => new Date(ad.endDate) > new Date())
+                .length === 0 ? (
+                <h4>{t('pMessage')} </h4>
+              ) : (
+                advertisementsData?.advertisementsConnection
+                  .filter((ad: any) => ad.organization._id === currentOrgId)
                   .filter((ad: any) => new Date(ad.endDate) > new Date())
-                  .length == 0 ? (
-                  <h4>{t('pMessage')} </h4>
-                ) : (
-                  advertisementsData?.advertisementsConnection
-                    .filter((ad: any) => ad.organization._id == currentOrgId)
-                    .filter((ad: any) => new Date(ad.endDate) > new Date())
-                    .map(
-                      (
-                        ad: {
-                          _id: string;
-                          name: string | undefined;
-                          type: string | undefined;
-                          organization: any;
-                          mediaUrl: string;
-                          endDate: Date;
-                          startDate: Date;
-                        },
-                        i: React.Key | null | undefined,
-                      ): JSX.Element => (
-                        <AdvertisementEntry
-                          id={ad._id}
-                          key={i}
-                          name={ad.name}
-                          type={ad.type}
-                          organizationId={ad.organization._id}
-                          startDate={new Date(ad.startDate)}
-                          endDate={new Date(ad.endDate)}
-                          mediaUrl={ad.mediaUrl}
-                        />
-                      ),
-                    )
-                )}
-              </Tab>
-              <Tab eventKey="archievedAds" title={t('archievedAds')}>
-                {advertisementsData?.advertisementsConnection
-                  .filter((ad: any) => ad.organization._id == currentOrgId)
+                  .map((ad: any, i: number) => (
+                    <AdvertisementEntry
+                      id={ad._id}
+                      key={i}
+                      name={ad.name}
+                      type={ad.type}
+                      organizationId={ad.organization._id}
+                      startDate={new Date(ad.startDate)}
+                      endDate={new Date(ad.endDate)}
+                      mediaUrl={ad.mediaUrl}
+                    />
+                  ))
+              )}
+            </Tab>
+            <Tab eventKey="archievedAds" title={t('archievedAds')}>
+              {advertisementsData?.advertisementsConnection
+                .filter((ad: any) => ad.organization._id === currentOrgId)
+                .filter((ad: any) => new Date(ad.endDate) < new Date())
+                .length === 0 ? (
+                <h4>{t('pMessage')} </h4>
+              ) : (
+                advertisementsData?.advertisementsConnection
+                  .filter((ad: any) => ad.organization._id === currentOrgId)
                   .filter((ad: any) => new Date(ad.endDate) < new Date())
-                  .length == 0 ? (
-                  <h4>{t('pMessage')} </h4>
-                ) : (
-                  advertisementsData?.advertisementsConnection
-                    .filter((ad: any) => ad.organization._id == currentOrgId)
-                    .filter((ad: any) => new Date(ad.endDate) < new Date())
-                    .map(
-                      (
-                        ad: {
-                          _id: string;
-                          name: string | undefined;
-                          type: string | undefined;
-                          organization: any;
-                          mediaUrl: string;
-                          endDate: Date;
-                          startDate: Date;
-                        },
-                        i: React.Key | null | undefined,
-                      ): JSX.Element => (
-                        <AdvertisementEntry
-                          id={ad._id}
-                          key={i}
-                          name={ad.name}
-                          type={ad.type}
-                          organizationId={ad.organization._id}
-                          startDate={new Date(ad.startDate)}
-                          endDate={new Date(ad.endDate)}
-                          mediaUrl={ad.mediaUrl}
-                        />
-                      ),
-                    )
-                )}
-              </Tab>
-            </Tabs>
-          </div>
-        </Col>
-      </Row>
-    </>
+                  .map((ad: any, i: number) => (
+                    <AdvertisementEntry
+                      id={ad._id}
+                      key={i}
+                      name={ad.name}
+                      type={ad.type}
+                      organizationId={ad.organization._id}
+                      startDate={new Date(ad.startDate)}
+                      endDate={new Date(ad.endDate)}
+                      mediaUrl={ad.mediaUrl}
+                    />
+                  ))
+              )}
+            </Tab>
+          </Tabs>
+        </div>
+      </Col>
+    </Row>
   );
 }
 
