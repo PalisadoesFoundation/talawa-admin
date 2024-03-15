c9260938a3d73d29ad4c9f95f45522308085a7e0
duplixx <90516956+duplixx@users.noreply.github.com>
2024-03-15 19:01:49 +0530

    minor

--- src/components/LeftDrawerOrg/LeftDrawerOrg.test.tsx
+++ src/components/LeftDrawerOrg/LeftDrawerOrg.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import 'jest-localstorage-mock';
 import { I18nextProvider } from 'react-i18next';
