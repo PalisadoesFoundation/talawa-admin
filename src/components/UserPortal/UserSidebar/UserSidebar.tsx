a0a966994eb47d9bea18e6d14cc402a6fc661662
Glen <dsouzaglen30@gmail.com>
2024-03-24 15:43:24 +0530

    Add Interface to remove any

--- src/components/UserPortal/UserSidebar/UserSidebar.tsx
+++ src/components/UserPortal/UserSidebar/UserSidebar.tsx
@@ -13,6 +13,14 @@ import { useTranslation } from 'react-i18next';
 import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
 import useLocalStorage from 'utils/useLocalstorage';
 
+interface InterfaceOrganization {
+  _id: string;
+  name: string;
+  description: string;
+  image: string | null;
+  __typename: string;
+}
+
 function userSidebar(): JSX.Element {
   const { t } = useTranslation('translation', {
     keyPrefix: 'userSidebar',
@@ -73,34 +81,37 @@ function userSidebar(): JSX.Element {
             </div>
             <ListGroup variant="flush">
               {organizations.length ? (
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                organizations.map((organization: any, index: number) => {
-                  const organizationUrl = `/user/organization/id=${organization._id}`;
+                organizations.map(
+                  (organization: InterfaceOrganization, index: number) => {
+                    const organizationUrl = `/user/organization/id=${organization._id}`;
 
-                  return (
-                    <ListGroup.Item
-                      key={index}
-                      action
-                      className={`${styles.rounded} ${styles.colorLight}`}
-                    >
-                      <Link to={organizationUrl} className={styles.link}>
-                        <div className="d-flex flex-row justify-content-center">
-                          <img
-                            src={
-                              organization.image ? organization.image : AboutImg
-                            }
-                            className={styles.personImage}
-                            width="auto"
-                            height="30px"
-                          />
-                          <div className={styles.orgName}>
-                            {organization.name}
+                    return (
+                      <ListGroup.Item
+                        key={index}
+                        action
+                        className={`${styles.rounded} ${styles.colorLight}`}
+                      >
+                        <Link to={organizationUrl} className={styles.link}>
+                          <div className="d-flex flex-row justify-content-center">
+                            <img
+                              src={
+                                organization.image
+                                  ? organization.image
+                                  : AboutImg
+                              }
+                              className={styles.personImage}
+                              width="auto"
+                              height="30px"
+                            />
+                            <div className={styles.orgName}>
+                              {organization.name}
+                            </div>
                           </div>
-                        </div>
-                      </Link>
-                    </ListGroup.Item>
-                  );
-                })
+                        </Link>
+                      </ListGroup.Item>
+                    );
+                  },
+                )
               ) : (
                 <div className="w-100 text-center">{t('noOrganizations')}</div>
               )}
