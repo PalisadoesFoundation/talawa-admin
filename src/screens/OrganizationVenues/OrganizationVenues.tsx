aaea787373050b9153792ef11b02afe8020660cc
Glen <dsouzaglen30@gmail.com>
2024-04-09 17:07:05 +0530

    Migrate to card layout

--- src/screens/OrganizationVenues/OrganizationVenues.tsx
+++ src/screens/OrganizationVenues/OrganizationVenues.tsx
@@ -6,39 +6,15 @@ import { errorHandler } from 'utils/errorHandler';
 import { useMutation, useQuery } from '@apollo/client';
 import Col from 'react-bootstrap/Col';
 import { VENUE_LIST } from 'GraphQl/Queries/OrganizationQueries';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
 import Loader from 'components/Loader/Loader';
 import { Navigate, useParams } from 'react-router-dom';
 import VenueModal from 'components/Venues/VenueModal';
-import { Dropdown, Form } from 'react-bootstrap';
-import { Search, Sort, Edit } from '@mui/icons-material';
+import { Card, Dropdown, Form } from 'react-bootstrap';
+import { Search, Sort } from '@mui/icons-material';
+import { ReactComponent as PeopleIcon } from 'assets/svgs/people.svg';
 import { DELETE_VENUE_MUTATION } from 'GraphQl/Mutations/VenueMutations';
 import type { InterfaceQueryVenueListItem } from 'utils/interfaces';
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: ['#31bb6b', '!important'],
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-    maxWidth: '500px',
-    overflow: 'auto',
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(() => ({
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
+import defaultImg from 'assets/images/defaultImg.png';
 
 function organizationVenues(): JSX.Element {
   const { t } = useTranslation('translation', {
@@ -51,6 +27,7 @@ function organizationVenues(): JSX.Element {
     'create',
   );
   const [searchTerm, setSearchTerm] = useState('');
+  const [searchBy, setSearchBy] = useState<'name' | 'desc'>('name');
   const [sortOrder, setSortOrder] = useState<'highest' | 'lowest'>('highest');
   const [editVenueData, setEditVenueData] =
     useState<InterfaceQueryVenueListItem | null>(null);
@@ -126,7 +103,16 @@ function organizationVenues(): JSX.Element {
   }, [venueData]);
 
   const filteredVenues = venues
-    .filter((venue) => venue.name.includes(searchTerm))
+    .filter((venue) => {
+      if (searchBy === 'name') {
+        return venue.name.toLowerCase().includes(searchTerm.toLowerCase());
+      } else {
+        return (
+          venue.description &&
+          venue.description.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+      }
+    })
     .sort((a, b) => {
       if (sortOrder === 'highest') {
         return parseInt(b.capacity) - parseInt(a.capacity);
@@ -137,14 +123,14 @@ function organizationVenues(): JSX.Element {
 
   return (
     <>
-      <div className={styles.btnsContainer}>
-        <div className={styles.input}>
+      <div className={`${styles.btnsContainer} gap-3 flex-wrap`}>
+        <div className={`${styles.input}`}>
           <Form.Control
             type="name"
             id="searchByName"
             className="bg-white"
-            placeholder={t('searchByName')}
-            data-testid="searchByName"
+            placeholder={t('searchBy') + ' ' + t(searchBy)}
+            data-testid="searchBy"
             autoComplete="off"
             required
             value={searchTerm}
@@ -158,14 +144,51 @@ function organizationVenues(): JSX.Element {
             <Search />
           </Button>
         </div>
-        <div className={styles.btnsBlock}>
-          <div className="d-flex">
+        <div className="d-flex gap-3 flex-wrap ">
+          <div className="d-flex gap-3 justify-content-between flex-fill">
+            <Dropdown
+              aria-expanded="false"
+              title="SearchBy"
+              data-tesid="searchByToggle"
+              className="flex-fill"
+            >
+              <Dropdown.Toggle data-testid="searchBy" variant="outline-success">
+                <Sort className={'me-1'} />
+                {t('searchBy')}
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  id="searchName"
+                  onClick={(e): void => {
+                    setSearchBy('name');
+                    e.preventDefault();
+                  }}
+                  data-testid="name"
+                >
+                  {t('name')}
+                </Dropdown.Item>
+                <Dropdown.Item
+                  id="searchDesc"
+                  onClick={(e): void => {
+                    setSearchBy('desc');
+                    e.preventDefault();
+                  }}
+                  data-testid="desc"
+                >
+                  {t('desc')}
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
             <Dropdown
               aria-expanded="false"
               title="Sort Venues"
               data-testid="sort"
+              className="flex-fill"
             >
-              <Dropdown.Toggle variant={'success'} data-testid="sortVenues">
+              <Dropdown.Toggle
+                variant="outline-success"
+                data-testid="sortVenues"
+              >
                 <Sort className={'me-1'} />
                 {t('sort')}
               </Dropdown.Toggle>
@@ -185,10 +208,9 @@ function organizationVenues(): JSX.Element {
               </Dropdown.Menu>
             </Dropdown>
           </div>
-
           <Button
             variant="success"
-            className="mx-2"
+            className="ml-3 flex-fill"
             onClick={showCreateVenueModal}
             data-testid="createVenueBtn"
           >
@@ -204,8 +226,82 @@ function organizationVenues(): JSX.Element {
               <Loader />
             </>
           ) : (
-            <div className={styles.list_box} data-testid="orgvenueslist">
-              <Col>
+            <div
+              className={`${styles.list_box} row `}
+              data-testid="orgvenueslist"
+            >
+              {filteredVenues.length ? (
+                filteredVenues.map(
+                  (venueItem: InterfaceQueryVenueListItem, index: number) => (
+                    <div
+                      className="col-xl-4 col-lg-4 col-md-6"
+                      data-testid="post-item"
+                      key={venueItem._id}
+                    >
+                      <div className={styles.cards} data-testid="cardStructure">
+                        <Card className={styles.card}>
+                          <Card.Img
+                            variant="top"
+                            src={venueItem.image || defaultImg}
+                            alt="image not found"
+                            className={styles.novenueimage}
+                          />
+                          <Card.Body className="pb-0">
+                            <Card.Title className="d-flex justify-content-between">
+                              <div className={styles.title}>
+                                {venueItem.name.length > 25
+                                  ? venueItem.name.slice(0, 25) + '...'
+                                  : venueItem.name}
+                              </div>
+
+                              <div className={styles.capacityLabel}>
+                                Capacity: {venueItem.capacity}
+                                <PeopleIcon
+                                  className="ms-1"
+                                  width={16}
+                                  height={16}
+                                />
+                              </div>
+                            </Card.Title>
+                            <Card.Text className={styles.text}>
+                              {venueItem.description &&
+                              venueItem.description.length > 75
+                                ? venueItem.description.slice(0, 75) + '...'
+                                : venueItem.description}
+                            </Card.Text>
+                          </Card.Body>
+                          <div className="d-flex justify-content-end gap-2 mb-2 me-3">
+                            <Button
+                              variant="outline-secondary"
+                              size="sm"
+                              onClick={() => {
+                                showEditVenueModal(venueItem);
+                              }}
+                              data-testid={`updateVenueBtn${index + 1}`}
+                            >
+                              <i className="fa fa-pen me-1"></i>
+                              <span>{t('edit')}</span>
+                            </Button>
+                            <Button
+                              variant="outline-danger"
+                              size="sm"
+                              data-testid={`deleteVenueBtn${index + 1}`}
+                              onClick={() => handleDelete(venueItem._id)}
+                            >
+                              <i className="fa fa-trash me-2"></i>
+                              <span>{t('delete')}</span>
+                            </Button>
+                          </div>
+                        </Card>
+                      </div>
+                    </div>
+                  ),
+                )
+              ) : (
+                <h6>{t('noVenues')}</h6>
+              )}
+
+              {/* <Col>
                 <TableContainer component={Paper} sx={{ minWidth: '540px' }}>
                   <Table aria-label="customized table">
                     <TableHead>
@@ -215,8 +311,7 @@ function organizationVenues(): JSX.Element {
                         <StyledTableCell align="center">
                           Description
                         </StyledTableCell>
-                        <StyledTableCell align="center">
-                          Capacity
+                        <StyledTableCell align="center">div                         Capacity
                         </StyledTableCell>
                         <StyledTableCell align="center">
                           Actions
@@ -281,7 +376,7 @@ function organizationVenues(): JSX.Element {
                     </TableBody>
                   </Table>
                 </TableContainer>
-              </Col>
+              </Col> */}
             </div>
           )}
         </div>
