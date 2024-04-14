7b9294d26343b899d2969ed1f8024d06d649cf8f
Akhilender <akhilenderb9@gmail.com>
2024-04-14 14:57:31 +0530

    fix: Fixed inconsistent left drawer expansion/collapse behavior
    - Adjusted the left drawer component to function properly on screens with a width of 820px or less.
    - Ensured smooth expansion and collapse of the left drawer when clicked.
    - Ensured the overflow-y of content is handled in smaller screens.
    - Modified the changes for organization dashboard screens to match the superadmin screens when the left drawer is collapsed.
    - Covered the changes with tests.
    - Ensured no other functionality got affected.
    
    Signed-off-by: Akhilender <akhilenderb9@gmail.com>

--- src/components/LeftDrawerOrg/LeftDrawerOrg.test.tsx
+++ src/components/LeftDrawerOrg/LeftDrawerOrg.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import 'jest-localstorage-mock';
 import { I18nextProvider } from 'react-i18next';
@@ -240,6 +240,12 @@ async function wait(ms = 100): Promise<void> {
     });
   });
 }
+
+const resizeWindow = (width: number): void => {
+  window.innerWidth = width;
+  fireEvent(window, new Event('resize'));
+};
+
 beforeEach(() => {
   setItem('FirstName', 'John');
   setItem('LastName', 'Doe');
@@ -320,6 +326,30 @@ describe('Testing LeftDrawerOrg component for SUPERADMIN', () => {
     await wait();
     userEvent.click(screen.getByText('Dashboard'));
     expect(global.window.location.pathname).toContain('/orgdash/id=123');
+  });
+
+  test('Testing when screen size is less than 820px', async () => {
+    setItem('SuperAdmin', true);
+    render(
+      <MockedProvider addTypename={false} link={link}>
+        <BrowserRouter>
+          <Provider store={store}>
+            <I18nextProvider i18n={i18nForTest}>
+              <LeftDrawerOrg {...props} />
+            </I18nextProvider>
+          </Provider>
+        </BrowserRouter>
+      </MockedProvider>,
+    );
+    await wait();
+    resizeWindow(800);
+    expect(screen.getByText(/Dashboard/i)).toBeInTheDocument();
+    expect(screen.getByText(/People/i)).toBeInTheDocument();
+
+    const peopelBtn = screen.getByTestId(/People/i);
+    userEvent.click(peopelBtn);
+    await wait();
+    expect(window.location.pathname).toContain('/orgpeople/id=123');
   });
 
   test('Testing when image is present for Organization', async () => {
