120ac57fc9c03060db572cfce088e2a886219667
kanhaiya04 <kanhaiyayadav9999@gmail.com>
2024-04-24 00:51:17 +0530

    fixed the OrgAdminListCard test

--- src/components/OrgAdminListCard/OrgAdminListCard.test.tsx
+++ src/components/OrgAdminListCard/OrgAdminListCard.test.tsx
@@ -1,28 +1,26 @@
 import React from 'react';
-import { act, render, screen } from '@testing-library/react';
+import { act, render, screen, waitFor } from '@testing-library/react';
+import type { RenderResult } from '@testing-library/react';
 import { MockedProvider } from '@apollo/react-testing';
 import userEvent from '@testing-library/user-event';
 import { I18nextProvider } from 'react-i18next';
-
 import { REMOVE_ADMIN_MUTATION } from 'GraphQl/Mutations/mutations';
 import OrgAdminListCard from './OrgAdminListCard';
 import i18nForTest from 'utils/i18nForTest';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { StaticMockLink } from 'utils/StaticMockLink';
 
 const MOCKS = [
   {
     request: {
       query: REMOVE_ADMIN_MUTATION,
-      variable: { userid: '564', orgid: '987' },
+      variables: { userid: '456', orgid: '987' },
     },
     result: {
       data: {
-        organizations: [
-          {
-            _id: '1',
-          },
-        ],
+        removeAdmin: {
+          _id: '456',
+        },
       },
     },
   },
@@ -36,8 +34,43 @@ async function wait(ms = 100): Promise<void> {
   });
 }
 
+const renderOrgAdminListCard = (props: {
+  toggleRemoveModal: () => boolean;
+  id: string | undefined;
+}): RenderResult => {
+  return render(
+    <MockedProvider addTypename={false} link={link}>
+      <MemoryRouter initialEntries={['/orgpeople/987']}>
+        <I18nextProvider i18n={i18nForTest}>
+          <Routes>
+            <Route
+              path="/orgpeople/:orgId"
+              element={<OrgAdminListCard {...props} />}
+            />
+            <Route
+              path="/orgList"
+              element={<div data-testid="orgListScreen">orgListScreen</div>}
+            />
+          </Routes>
+        </I18nextProvider>
+      </MemoryRouter>
+    </MockedProvider>,
+  );
+};
+
 describe('Testing Organization Admin List Card', () => {
   global.alert = jest.fn();
+
+  beforeEach(() => {
+    Object.defineProperty(window, 'location', {
+      writable: true,
+      value: { reload: jest.fn() },
+    });
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
 
   test('should render props and text elements test for the page component', async () => {
     const props = {
@@ -45,19 +78,13 @@ describe('Testing Organization Admin List Card', () => {
       id: '456',
     };
 
-    render(
-      <MockedProvider addTypename={false} link={link}>
-        <BrowserRouter>
-          <I18nextProvider i18n={i18nForTest}>
-            <OrgAdminListCard {...props} />
-          </I18nextProvider>
-        </BrowserRouter>
-      </MockedProvider>,
-    );
+    renderOrgAdminListCard(props);
 
     await wait();
 
     userEvent.click(screen.getByTestId(/removeAdminBtn/i));
+
+    await wait(2000);
   });
 
   test('Should not render text elements when props value is not passed', async () => {
@@ -66,18 +93,11 @@ describe('Testing Organization Admin List Card', () => {
       id: undefined,
     };
 
-    render(
-      <MockedProvider addTypename={false} link={link}>
-        <BrowserRouter>
-          <I18nextProvider i18n={i18nForTest}>
-            <OrgAdminListCard {...props} />
-          </I18nextProvider>
-        </BrowserRouter>
-      </MockedProvider>,
-    );
+    renderOrgAdminListCard(props);
 
-    await wait();
-
-    expect(window.location.pathname).toEqual('/orglist');
+    await waitFor(() => {
+      const orgListScreen = screen.getByTestId('orgListScreen');
+      expect(orgListScreen).toBeInTheDocument();
+    });
   });
 });
