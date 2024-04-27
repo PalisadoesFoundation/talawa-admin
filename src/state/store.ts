45cc77795a0123c64526f424a26ad79f9fe12c34
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2024-04-27 10:37:21 -0700

    Deleted all main files

--- src/state/store.ts
+++
@@ -1,5 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import { reducers } from './reducers/index';
-
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
