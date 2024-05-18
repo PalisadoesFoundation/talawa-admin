76d8ace90dd225a8458b48231642a826996da59f
Pranshu Gupta <pranshgupta54@gmail.com>
2024-05-18 16:15:55 +0530

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -9,7 +9,7 @@ process.env.PUBLIC_URL = '';
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
 process.on('unhandledRejection', (err) => {
-  throw err;
+  throw error;
 });
 
 // Ensure environment variables are read.
