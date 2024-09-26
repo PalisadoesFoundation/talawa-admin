33120266c9a6b007354767d4a36eda12889598e1
adithya <adithyav102@gmail.com>
2024-09-26 10:07:30 +0530

    Updated react-redux and peer-dependencies

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,6 @@
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import { reducers } from './reducers/index';
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
+export const store = configureStore({
+  reducer: reducers,
+});
