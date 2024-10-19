789aa19b1b79c0091569c9d57654ead6ffa97f83
Disha <dishatalreja1202@gmail.com>
2024-10-19 20:15:25 +0530

    feat: added option for creating a dedicated group chat for an event

--- src/screens/UserPortal/Events/Events.tsx
+++ src/screens/UserPortal/Events/Events.tsx
@@ -48,6 +48,7 @@ export default function events(): JSX.Element {
   const [endTime, setEndTime] = React.useState('10:00:00');
   const [viewType, setViewType] = React.useState<ViewType>(ViewType.MONTH);
   const [createEventModal, setCreateEventmodalisOpen] = React.useState(false);
+  const [createChatCheck, setCreateChatCheck] = React.useState(false);
   const { orgId: organizationId } = useParams();
 
   const { data, refetch } = useQuery(ORGANIZATION_EVENTS_CONNECTION, {
@@ -92,6 +93,7 @@ export default function events(): JSX.Element {
           location: eventLocation,
           startTime: !isAllDay ? startTime + 'Z' : null,
           endTime: !isAllDay ? endTime + 'Z' : null,
+          createChat: createChatCheck,
         },
       });
 
@@ -327,6 +329,21 @@ export default function events(): JSX.Element {
                       checked={isRegisterable}
                       data-testid="registerableEventCheck"
                       onChange={(): void => setIsRegisterable(!isRegisterable)}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className={styles.dispflex}>
+                    <label htmlFor="createChat">{t('createChat')}?</label>
+                    <Form.Switch
+                      className="me-4"
+                      id="chat"
+                      type="checkbox"
+                      data-testid="createChat"
+                      checked={createChatCheck}
+                      onChange={(): void =>
+                        setCreateChatCheck(!createChatCheck)
+                      }
                     />
                   </div>
                 </div>
