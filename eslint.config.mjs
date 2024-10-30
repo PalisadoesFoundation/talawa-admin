f13dfadb014eadf9b85f29cf7e7a979ac4e5f1fe
Ankit varshney <lyfflame45@gmail.com>
2024-10-30 08:29:11 +0530

    fix ts eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,6 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
-import * as graphqlEslint from "@graphql-eslint/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
