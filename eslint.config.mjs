8bbff3dfde4bb1d5e4fc1249b991ca9a423f05ee
Ankit varshney <lyfflame45@gmail.com>
2024-10-30 08:14:19 +0530

    fix ts eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,155 +1,163 @@
-import react from "eslint-plugin-react";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import jest from "eslint-plugin-jest";
-import _import from "eslint-plugin-import";
 import tsdoc from "eslint-plugin-tsdoc";
-import prettier from "eslint-plugin-prettier";
+import _import from "eslint-plugin-import";
 import { fixupPluginRules } from "@eslint/compat";
 import globals from "globals";
 import tsParser from "@typescript-eslint/parser";
+import * as parser from "@graphql-eslint/eslint-plugin";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import * as graphqlEslint from "@graphql-eslint/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
 });
 
-export default [{
-    ignores: ["src/components/CheckIn/tagTemplate.ts"],
-}, ...compat.extends(
-    "plugin:react/recommended",
-    "eslint:recommended",
-    "plugin:jest/recommended",
-    "plugin:prettier/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "eslint-config-prettier",
-    "prettier",
-), {
-    plugins: {
-        react,
-        "@typescript-eslint": typescriptEslint,
-        jest,
-        import: fixupPluginRules(_import),
-        tsdoc,
-        prettier,
+export default [
+    {
+        ignores: [
+            "**/.github",
+            "**/.vscode",
+            "**/build",
+            "**/coverage",
+            "**/node_modules",
+            "src/types",
+            "docs/Schema.md",
+        ],
     },
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-            Atomics: "readonly",
-            SharedArrayBuffer: "readonly",
+    ...compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+    ),
+    {
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+            tsdoc,
+            import: fixupPluginRules(_import),
         },
 
-        parser: tsParser,
-        ecmaVersion: 2018,
-        sourceType: "module",
+        languageOptions: {
+            parser: tsParser,
+            globals: globals.node,
+        },
+        rules: {
+            "no-restricted-imports": [
+                "error",
+                {
+                    patterns: ["**/src/**"],
+                },
+            ],
+            "import/no-duplicates": "error",
+            "tsdoc/syntax": "error",
+            "@typescript-eslint/ban-ts-comment": "error",
+            "@typescript-eslint/no-empty-object-type": "error",
+            "@typescript-eslint/no-unsafe-function-type": "error",
+            "@typescript-eslint/no-wrapper-object-types": "error",
+            "@typescript-eslint/no-duplicate-enum-values": "error",
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-non-null-asserted-optional-chain": "error",
+            "@typescript-eslint/no-non-null-assertion": "error",
+            "@typescript-eslint/no-var-requires": "error",
+        },
+    },
+    {
+        files: ["**/*.ts"],
 
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: "latest",
+            sourceType: "module",
+
+            parserOptions: {
+                project: "./tsconfig.json",
+                tsconfigRootDir: ".",
             },
         },
-    },
 
-    settings: {
-        react: {
-            version: "detect",
+        rules: {
+            "@typescript-eslint/array-type": "error",
+            "@typescript-eslint/consistent-type-assertions": "error",
+            "@typescript-eslint/consistent-type-imports": "error",
+            "@typescript-eslint/explicit-function-return-type": "error",
+
+            "@typescript-eslint/naming-convention": [
+                "error",
+                {
+                    selector: "interface",
+                    format: ["PascalCase"],
+                    prefix: ["Interface", "TestInterface"],
+                },
+                {
+                    selector: ["typeAlias", "typeLike", "enum"],
+                    format: ["PascalCase"],
+                },
+                {
+                    selector: "typeParameter",
+                    format: ["PascalCase"],
+                    prefix: ["T"],
+                },
+                {
+                    selector: "variable",
+                    format: ["camelCase", "UPPER_CASE"],
+                    leadingUnderscore: "allow",
+                },
+                {
+                    selector: "parameter",
+                    format: ["camelCase"],
+                    leadingUnderscore: "allow",
+                },
+                {
+                    selector: "function",
+                    format: ["camelCase"],
+                },
+                {
+                    selector: "memberLike",
+                    modifiers: ["private"],
+                    format: ["camelCase"],
+                    leadingUnderscore: "require",
+                },
+                {
+                    selector: "variable",
+                    modifiers: ["exported"],
+                    format: null,
+                },
+            ],
         },
     },
-
-    rules: {
-        "react/destructuring-assignment": "error",
-        "@typescript-eslint/explicit-module-boundary-types": "error",
-
-        "react/no-multi-comp": ["error", {
-            ignoreStateless: false,
-        }],
-
-        "react/jsx-filename-extension": ["error", {
-            extensions: [".tsx"],
-        }],
-
-        "import/no-duplicates": "error",
-        "tsdoc/syntax": "error",
-        "@typescript-eslint/ban-ts-comment": "error",
-        "@typescript-eslint/no-explicit-any": "error",
-        "@typescript-eslint/no-inferrable-types": "error",
-        "@typescript-eslint/no-non-null-asserted-optional-chain": "error",
-        "@typescript-eslint/no-non-null-assertion": "error",
-        "@typescript-eslint/no-var-requires": "error",
-        "@typescript-eslint/no-unsafe-function-type": "error",
-        "@typescript-eslint/no-wrapper-object-types": "error",
-        "@typescript-eslint/no-empty-object-type": "error",
-        "@typescript-eslint/no-duplicate-enum-values": "error",
-        "@typescript-eslint/array-type": "error",
-        "@typescript-eslint/consistent-type-assertions": "error",
-        "@typescript-eslint/consistent-type-imports": "error",
-
-        "@typescript-eslint/explicit-function-return-type": [2, {
-            allowExpressions: true,
-            allowTypedFunctionExpressions: true,
-        }],
-
-        camelcase: "off",
-
-        "@typescript-eslint/naming-convention": ["error", {
-            selector: "interface",
-            format: ["PascalCase"],
-            prefix: ["Interface", "TestInterface"],
-        }, {
-            selector: ["typeAlias", "typeLike", "enum"],
-            format: ["PascalCase"],
-        }, {
-            selector: "typeParameter",
-            format: ["PascalCase"],
-            prefix: ["T"],
-        }, {
-            selector: "variable",
-            format: ["camelCase", "UPPER_CASE", "PascalCase"],
-            leadingUnderscore: "allow",
-        }, {
-            selector: "parameter",
-            format: ["camelCase"],
-            leadingUnderscore: "allow",
-        }, {
-            selector: "function",
-            format: ["camelCase", "PascalCase"],
-        }, {
-            selector: "memberLike",
-            modifiers: ["private"],
-            format: ["camelCase"],
-            leadingUnderscore: "require",
-        }, {
-            selector: "variable",
-            modifiers: ["exported"],
-            format: null,
-        }],
-
-        "react/jsx-pascal-case": ["error", {
-            allowAllCaps: false,
-            allowNamespace: false,
-        }],
-
-        "react/jsx-equals-spacing": ["warn", "never"],
-        "react/no-this-in-sfc": "error",
-        "jest/expect-expect": 0,
-
-        "react/no-unstable-nested-components": ["error", {
-            allowAsProps: true,
-        }],
-
-        "react/function-component-definition": [0, {
-            namedComponents: "function-declaration",
-        }],
-
-        "prettier/prettier": "error",
+    {
+        files: ["./src/typeDefs/**/*.ts"],
+        processor: "@graphql-eslint/graphql",
     },
-}];
+    {
+        files: ["./src/typeDefs/**/*.graphql"],
+
+        plugins: {
+            "@graphql-eslint": graphqlEslint,
+        },
+
+        languageOptions: {
+            parser: parser,
+        },
+    },
+    {
+        files: ["tests/**/*"],
+
+        rules: {
+            "no-restricted-imports": "off",
+        },
+    },
+    {
+        files: ["./src/index.ts", "./src/utilities/copyToClipboard.ts"],
+
+        rules: {
+            "@typescript-eslint/explicit-function-return-type": "off",
+            "@typescript-eslint/no-empty-function": "off",
+        },
+    },
+];
