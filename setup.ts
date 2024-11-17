c0884d10739b6425486f6193178cb954858a3717
VanshikaSabharwal <vanshikasabharwal06@gmail.com>
2024-11-18 01:32:24 +0530

    updated setup.ts

--- setup.ts
+++ setup.ts
@@ -7,9 +7,18 @@ import { checkEnvFile } from './src/setup/checkEnvFile/checkEnvFile';
 import { validateRecaptcha } from './src/setup/validateRecaptcha/validateRecaptcha';
 import { askForCustomPort } from './src/setup/askForCustomPort/askForCustomPort';
 
-export async function main(): Promise<void> {
-  console.log('Welcome to the Talawa Admin setup! 🚀');
+// Update the .env file with new values
+const updateEnvFile = (key: string, value: string): void => {
+  const currentEnvContent = fs.readFileSync('.env', 'utf8');
+  const updatedEnvContent = currentEnvContent.replace(
+    new RegExp(`^${key}=.*$`, 'm'),
+    `${key}=${value}`,
+  );
+  fs.writeFileSync('.env', updatedEnvContent, 'utf8');
+};
 
+// Handle .env file creation or validation
+const handleEnvFile = (): void => {
   if (!fs.existsSync('.env')) {
     fs.openSync('.env', 'w');
     const config = dotenv.parse(fs.readFileSync('.env.example'));
@@ -19,147 +28,78 @@ export async function main(): Promise<void> {
   } else {
     checkEnvFile();
   }
+};
 
-  let shouldSetCustomPort: boolean;
+// Ask and update the custom port
+const askAndUpdatePort = async (): Promise<void> => {
+  const { shouldSetCustomPortResponse } = await inquirer.prompt({
+    type: 'confirm',
+    name: 'shouldSetCustomPortResponse',
+    message: 'Would you like to set up a custom port?',
+    default: true,
+  });
 
-  if (process.env.PORT) {
-    console.log(
-      `\nCustom port for development server already exists with the value:\n${process.env.PORT}`,
-    );
-    shouldSetCustomPort = true;
-  } else {
-    const { shouldSetCustomPortResponse } = await inquirer.prompt({
-      type: 'confirm',
-      name: 'shouldSetCustomPortResponse',
-      message: 'Would you like to set up a custom port?',
-      default: true,
-    });
-    shouldSetCustomPort = shouldSetCustomPortResponse;
-  }
-
-  if (shouldSetCustomPort) {
+  if (shouldSetCustomPortResponse) {
     const customPort = await askForCustomPort();
-
-    const port = dotenv.parse(fs.readFileSync('.env')).PORT;
-
-    fs.readFile('.env', 'utf8', (err, data) => {
-      const result = data.replace(`PORT=${port}`, `PORT=${customPort}`);
-      fs.writeFileSync('.env', result, 'utf8');
-    });
+    updateEnvFile('PORT', String(customPort));
   }
+};
 
-  let shouldSetTalawaApiUrl: boolean;
+// Ask and update the Talawa API URL
+const askAndUpdateTalawaApiUrl = async (): Promise<void> => {
+  const { shouldSetTalawaApiUrlResponse } = await inquirer.prompt({
+    type: 'confirm',
+    name: 'shouldSetTalawaApiUrlResponse',
+    message: 'Would you like to set up Talawa API endpoint?',
+    default: true,
+  });
 
-  if (process.env.REACT_APP_TALAWA_URL) {
-    console.log(
-      `\nEndpoint for accessing talawa-api graphql service already exists with the value:\n${process.env.REACT_APP_TALAWA_URL}`,
-    );
-    shouldSetTalawaApiUrl = true;
-  } else {
-    const { shouldSetTalawaApiUrlResponse } = await inquirer.prompt({
-      type: 'confirm',
-      name: 'shouldSetTalawaApiUrlResponse',
-      message: 'Would you like to set up talawa-api endpoint?',
-      default: true,
-    });
-    shouldSetTalawaApiUrl = shouldSetTalawaApiUrlResponse;
-  }
-
-  if (shouldSetTalawaApiUrl) {
-    let isConnected = false,
-      endpoint = '';
+  if (shouldSetTalawaApiUrlResponse) {
+    let endpoint = ' ';
+    let isConnected = false;
 
     while (!isConnected) {
       endpoint = await askForTalawaApiUrl();
       const url = new URL(endpoint);
       isConnected = await checkConnection(url.origin);
     }
-    const envPath = '.env';
-    const currentEnvContent = fs.readFileSync(envPath, 'utf8');
-    const talawaApiUrl = dotenv.parse(currentEnvContent).REACT_APP_TALAWA_URL;
 
-    const updatedEnvContent = currentEnvContent.replace(
-      `REACT_APP_TALAWA_URL=${talawaApiUrl}`,
-      `REACT_APP_TALAWA_URL=${endpoint}`,
-    );
-
-    fs.writeFileSync(envPath, updatedEnvContent, 'utf8');
+    updateEnvFile('REACT_APP_TALAWA_URL', endpoint);
     const websocketUrl = endpoint.replace(/^http(s)?:\/\//, 'ws$1://');
-    const currentWebSocketUrl =
-      dotenv.parse(updatedEnvContent).REACT_APP_BACKEND_WEBSOCKET_URL;
-
-    const finalEnvContent = updatedEnvContent.replace(
-      `REACT_APP_BACKEND_WEBSOCKET_URL=${currentWebSocketUrl}`,
-      `REACT_APP_BACKEND_WEBSOCKET_URL=${websocketUrl}`,
-    );
-
-    fs.writeFileSync(envPath, finalEnvContent, 'utf8');
+    updateEnvFile('REACT_APP_BACKEND_WEBSOCKET_URL', websocketUrl);
   }
+};
 
+// Ask and set up reCAPTCHA
+const askAndSetRecaptcha = async (): Promise<void> => {
   const { shouldUseRecaptcha } = await inquirer.prompt({
     type: 'confirm',
     name: 'shouldUseRecaptcha',
-    message: 'Would you like to set up ReCAPTCHA?',
+    message: 'Would you like to set up reCAPTCHA?',
     default: true,
   });
 
   if (shouldUseRecaptcha) {
-    const useRecaptcha = dotenv.parse(
-      fs.readFileSync('.env'),
-    ).REACT_APP_USE_RECAPTCHA;
-
-    fs.readFile('.env', 'utf8', (err, data) => {
-      const result = data.replace(
-        `REACT_APP_USE_RECAPTCHA=${useRecaptcha}`,
-        `REACT_APP_USE_RECAPTCHA=yes`,
-      );
-      fs.writeFileSync('.env', result, 'utf8');
-    });
-    let shouldSetRecaptchaSiteKey: boolean;
-    if (process.env.REACT_APP_RECAPTCHA_SITE_KEY) {
-      console.log(
-        `\nreCAPTCHA site key already exists with the value ${process.env.REACT_APP_RECAPTCHA_SITE_KEY}`,
-      );
-      shouldSetRecaptchaSiteKey = true;
-    } else {
-      const { shouldSetRecaptchaSiteKeyResponse } = await inquirer.prompt({
-        type: 'confirm',
-        name: 'shouldSetRecaptchaSiteKeyResponse',
-        message: 'Would you like to set up a reCAPTCHA site key?',
-        default: true,
-      });
-      shouldSetRecaptchaSiteKey = shouldSetRecaptchaSiteKeyResponse;
-    }
-
-    if (shouldSetRecaptchaSiteKey) {
-      const { recaptchaSiteKeyInput } = await inquirer.prompt([
-        {
-          type: 'input',
-          name: 'recaptchaSiteKeyInput',
-          message: 'Enter your reCAPTCHA site key:',
-          validate: async (input: string): Promise<boolean | string> => {
-            if (validateRecaptcha(input)) {
-              return true;
-            }
-            return 'Invalid reCAPTCHA site key. Please try again.';
-          },
+    const { recaptchaSiteKeyInput } = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'recaptchaSiteKeyInput',
+        message: 'Enter your reCAPTCHA site key:',
+        validate: (input: string): boolean | string => {
+          return (
+            validateRecaptcha(input) ||
+            'Invalid reCAPTCHA site key. Please try again.'
+          );
         },
-      ]);
+      },
+    ]);
 
-      const recaptchaSiteKey = dotenv.parse(
-        fs.readFileSync('.env'),
-      ).REACT_APP_RECAPTCHA_SITE_KEY;
-
-      fs.readFile('.env', 'utf8', (err, data) => {
-        const result = data.replace(
-          `REACT_APP_RECAPTCHA_SITE_KEY=${recaptchaSiteKey}`,
-          `REACT_APP_RECAPTCHA_SITE_KEY=${recaptchaSiteKeyInput}`,
-        );
-        fs.writeFileSync('.env', result, 'utf8');
-      });
-    }
+    updateEnvFile('REACT_APP_RECAPTCHA_SITE_KEY', recaptchaSiteKeyInput);
   }
+};
 
+// Ask and set up logging errors in the console
+const askAndSetLogErrors = async (): Promise<void> => {
   const { shouldLogErrors } = await inquirer.prompt({
     type: 'confirm',
     name: 'shouldLogErrors',
@@ -169,16 +109,23 @@ export async function main(): Promise<void> {
   });
 
   if (shouldLogErrors) {
-    const logErrors = dotenv.parse(fs.readFileSync('.env')).ALLOW_LOGS;
-
-    fs.readFile('.env', 'utf8', (err, data) => {
-      const result = data.replace(`ALLOW_LOGS=${logErrors}`, 'ALLOW_LOGS=YES');
-      fs.writeFileSync('.env', result, 'utf8');
-    });
+    updateEnvFile('ALLOW_LOGS', 'YES');
   }
+};
+
+// Main function to run the setup process
+export async function main(): Promise<void> {
+  console.log('Welcome to the Talawa Admin setup! 🚀');
+
+  handleEnvFile();
+
+  await askAndUpdatePort();
+  await askAndUpdateTalawaApiUrl();
+  await askAndSetRecaptcha();
+  await askAndSetLogErrors();
 
   console.log(
-    '\nCongratulations! Talawa Admin has been successfully setup! 🥂🎉',
+    '\nCongratulations! Talawa Admin has been successfully set up! 🥂🎉',
   );
 }
 
