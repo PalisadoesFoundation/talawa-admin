eac28f7a31ba3ba7e09add90288f804ec7e4052b
VanshikaSabharwal <vanshikasabharwal06@gmail.com>
2024-11-18 02:09:22 +0530

    did recommended changes to setup.ts

--- setup.ts
+++ setup.ts
@@ -10,11 +10,16 @@ import { askForCustomPort } from './src/setup/askForCustomPort/askForCustomPort'
 // Update the .env file with new values
 const updateEnvFile = (key: string, value: string): void => {
   const currentEnvContent = fs.readFileSync('.env', 'utf8');
-  const updatedEnvContent = currentEnvContent.replace(
-    new RegExp(`^${key}=.*$`, 'm'),
-    `${key}=${value}`,
-  );
-  fs.writeFileSync('.env', updatedEnvContent, 'utf8');
+  const keyRegex = new RegExp(`^${key}=.*$`, 'm');
+  if (keyRegex.test(currentEnvContent)) {
+    const updatedEnvContent = currentEnvContent.replace(
+      keyRegex,
+      `${key}=${value}`,
+    );
+    fs.writeFileSync('.env', updatedEnvContent, 'utf8');
+  } else {
+    fs.appendFileSync('.env', `\n${key}=${value}`, 'utf8');
+  }
 };
 
 // Handle .env file creation or validation
