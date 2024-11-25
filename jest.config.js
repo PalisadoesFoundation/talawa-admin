c1ef506bec362624d33f2c94f3929d1b9412e170
prayansh_chhablani <prayanshchhablani@gmail.com>
2024-11-25 15:50:36 +0530

    resolving jest, vitest workflow

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ export default {
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.test.{js,jsx,ts,tsx}',
   ],
   testEnvironment: 'jsdom',
   transform: {
