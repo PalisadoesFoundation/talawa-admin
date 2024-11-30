fec6c585ee02b2e947c14c0b4acf55dd46b5a34d
adithya <adithyav102@gmail.com>
2024-11-30 10:39:48 +0530

    Added ts-doc comment

--- src/components/AddOn/core/AddOnEntry/AddOnEntry.spec.tsx
+++ src/components/AddOn/core/AddOnEntry/AddOnEntry.spec.tsx
@@ -1,3 +1,9 @@
+/**
+ * Unit tests for the AddOnEntry component.
+ *
+ * This file contains tests for the AddOnEntry component to ensure it behaves as expected
+ * under various scenarios.
+ */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
