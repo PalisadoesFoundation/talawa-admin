5d867540a596243a5b2b6cdf42060a24307ea314
MayankJha014 <mayankjha0330@gmail.com>
2024-12-06 14:03:41 +0530

    bug fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './vitest.setup.ts',
     include: [
       'src/screens/UserPortal/Volunteer/Actions/*.{spec,test}.{js,jsx,ts,tsx}',
     ],
