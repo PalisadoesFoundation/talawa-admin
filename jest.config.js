8613cd09a94c2efc67b64b45ac9ecd9a72ea40bc
Mayankpulse333 <mayank@pulsegen.io>
2024-12-15 00:32:29 +0530

    test fail fix trying

--- jest.config.js
+++ jest.config.js
@@ -36,8 +36,7 @@ export default {
     '<rootDir>/src',
   ],
   moduleNameMapper: {
-    '\\.module\\.(css|scss|sass)$': 'identity-obj-proxy',
-    '\\.(css|scss|sass)$': '<rootDir>/scripts/__mocks__/styleMock.js',
+    '\\.(css|scss|sass|less)$': 'identity-obj-proxy',
     '^react-native$': 'react-native-web',
     '^@dicebear/core$': '<rootDir>/scripts/__mocks__/@dicebear/core.ts',
     '^@dicebear/collection$':
