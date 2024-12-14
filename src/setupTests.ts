dfe7b0b29e6bd7c761ed3bb886ded6df46e24a44
nadeem nagarji <nadeemnagarji@gmail.com>
2024-12-14 13:52:38 +0530

    migration of People.test.tsx from jest to vitest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,8 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { vi, beforeAll } from 'vitest';
 
-global.fetch = jest.fn();
+global.fetch = vi.fn();
 
 import { format } from 'util';
 
@@ -19,17 +20,18 @@ Object.defineProperty(HTMLMediaElement.prototype, 'muted', {
   set: () => ({}),
 });
 
-import { jestPreviewConfigure } from 'jest-preview';
-
 // Global CSS here
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'react-datepicker/dist/react-datepicker.css';
 import 'flag-icons/css/flag-icons.min.css';
 
-jestPreviewConfigure({
-  // Opt-in to automatic mode to preview failed test case automatically.
-  autoPreview: true,
-});
+// jestPreviewConfigure({
+//   // Opt-in to automatic mode to preview failed test case automatically.
+//   autoPreview: true,
+// });
 
-jest.setTimeout(15000);
+// Use the global setTimeout function
+beforeAll(() => {
+  vi.setConfig({ testTimeout: 15000 });
+});
