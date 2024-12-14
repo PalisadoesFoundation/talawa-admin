a42c919ea76241e7b8ab9c4d73397f16d5d31afe
nadeem nagarji <nadeemnagarji@gmail.com>
2024-12-14 14:46:08 +0530

    updated setupTests.ts to old jest setup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,9 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import { vi, beforeAll } from 'vitest';
 
-global.fetch = vi.fn();
+global.fetch = jest.fn();
 
 import { format } from 'util';
 
@@ -20,18 +19,17 @@ Object.defineProperty(HTMLMediaElement.prototype, 'muted', {
   set: () => ({}),
 });
 
+import { jestPreviewConfigure } from 'jest-preview';
+
 // Global CSS here
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'react-datepicker/dist/react-datepicker.css';
 import 'flag-icons/css/flag-icons.min.css';
 
-// jestPreviewConfigure({
-//   // Opt-in to automatic mode to preview failed test case automatically.
-//   autoPreview: true,
-// });
-
-// Use the global setTimeout function
-beforeAll(() => {
-  vi.setConfig({ testTimeout: 15000 });
+jestPreviewConfigure({
+  // Opt-in to automatic mode to preview failed test case automatically.
+  autoPreview: true,
 });
+
+jest.setTimeout(15000);
