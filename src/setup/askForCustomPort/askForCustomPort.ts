4da59bf596b361eb1ebff137af478710d873f547
Aad1tya27 <agarwal.aaditya2765@gmail.com>
2024-12-22 17:32:13 +0530

    upgraded inqurier version

--- src/setup/askForCustomPort/askForCustomPort.ts
+++ src/setup/askForCustomPort/askForCustomPort.ts
@@ -1,13 +1,13 @@
 import inquirer from 'inquirer';
 
-export async function askForCustomPort(): Promise<number> {
-  const { customPort } = await inquirer.prompt([
+export async function askForCustomPort(): Promise<string> {
+  const { customPort } = await inquirer.prompt<{ customPort: string }>([
     {
       type: 'input',
       name: 'customPort',
       message:
         'Enter custom port for development server (leave blank for default 4321):',
-      default: 4321,
+      default: '4321',
     },
   ]);
   return customPort;
