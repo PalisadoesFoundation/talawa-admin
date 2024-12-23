7cb6a69546f07fb85d8cb66b7023d95a48ef923b
VanshikaSabharwal <vanshikasabharwal06@gmail.com>
2024-12-23 23:54:10 +0530

    updated jest to vitest in askForDocker file

--- src/setup/askForDocker/askForDocker.spec.ts
+++ src/setup/askForDocker/askForDocker.spec.ts
@@ -1,22 +1,24 @@
 import inquirer from 'inquirer';
 import { askForDocker } from './askForDocker';
 
-jest.mock('inquirer');
+import { describe, test, expect, vi } from 'vitest';
+
+vi.mock('inquirer');
 
 describe('askForCustomPort', () => {
   test('should return default docker port if user provides no input', async () => {
-    jest
-      .spyOn(inquirer, 'prompt')
-      .mockResolvedValueOnce({ dockerAppPort: '4321' });
+    vi.spyOn(inquirer, 'prompt').mockResolvedValueOnce({
+      dockerAppPort: '4321',
+    });
 
     const result = await askForDocker();
     expect(result).toBe('4321');
   });
 
   test('should return user-provided port', async () => {
-    jest
-      .spyOn(inquirer, 'prompt')
-      .mockResolvedValueOnce({ dockerAppPort: '4321' });
+    vi.spyOn(inquirer, 'prompt').mockResolvedValueOnce({
+      dockerAppPort: '8080',
+    });
 
     const result = await askForDocker();
     expect(result).toBe('8080');
