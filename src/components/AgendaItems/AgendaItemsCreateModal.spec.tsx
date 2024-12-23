027936d88197c73057aef63ef67eab3ec9c876c2
Ramneet04 <ramneetsinghrtd@gmail.com>
2024-12-23 22:25:32 +0530

    formated the code

--- src/components/AgendaItems/AgendaItemsCreateModal.spec.tsx
+++ src/components/AgendaItems/AgendaItemsCreateModal.spec.tsx
@@ -74,9 +74,8 @@ vi.mock('react-toastify', () => ({
   },
 }));
 vi.mock('utils/convertToBase64');
-const mockedConvertToBase64 = convertToBase64 as import('vitest').MockedFunction<
-  typeof convertToBase64
->;
+const mockedConvertToBase64 =
+  convertToBase64 as import('vitest').MockedFunction<typeof convertToBase64>;
 
 describe('AgendaItemsCreateModal', () => {
   test('renders modal correctly', () => {
