b9693d56741babba5a729d57277338f471b6c2c3
Pranav Nathe <93403830+pranavnathe@users.noreply.github.com>
2024-12-26 01:10:14 +0530

    Refactor tests to use Vitest (#2869)

--- src/components/UserProfileSettings/DeleteUser.spec.tsx
+++ src/components/UserProfileSettings/DeleteUser.spec.tsx
@@ -4,21 +4,26 @@ import { MockedProvider } from '@apollo/react-testing';
 import { BrowserRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import i18nForTest from 'utils/i18nForTest';
-import OtherSettings from './OtherSettings';
+import DeleteUser from './DeleteUser';
+import { describe, it, expect } from 'vitest';
 
 describe('Delete User component', () => {
-  test('renders delete user correctly', () => {
-    const { getByText } = render(
+  it('renders delete user correctly', () => {
+    const { getByText, getAllByText } = render(
       <MockedProvider addTypename={false}>
         <BrowserRouter>
           <I18nextProvider i18n={i18nForTest}>
-            <OtherSettings />
+            <DeleteUser />
           </I18nextProvider>
         </BrowserRouter>
       </MockedProvider>,
     );
 
-    expect(getByText('Other Settings')).toBeInTheDocument();
-    expect(getByText('Change Language')).toBeInTheDocument();
+    expect(
+      getByText(
+        'By clicking on Delete User button your user will be permanently deleted along with its events, tags and all related data.',
+      ),
+    ).toBeInTheDocument();
+    expect(getAllByText('Delete User')[0]).toBeInTheDocument();
   });
 });
--- src/components/UserProfileSettings/OtherSettings.spec.tsx
+++ src/components/UserProfileSettings/OtherSettings.spec.tsx
@@ -4,25 +4,22 @@ import { MockedProvider } from '@apollo/react-testing';
 import { BrowserRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import i18nForTest from 'utils/i18nForTest';
-import DeleteUser from './DeleteUser';
+import OtherSettings from './OtherSettings';
+import { describe, it, expect } from 'vitest';
 
 describe('Delete User component', () => {
-  test('renders delete user correctly', () => {
-    const { getByText, getAllByText } = render(
+  it('renders delete user correctly', () => {
+    const { getByText } = render(
       <MockedProvider addTypename={false}>
         <BrowserRouter>
           <I18nextProvider i18n={i18nForTest}>
-            <DeleteUser />
+            <OtherSettings />
           </I18nextProvider>
         </BrowserRouter>
       </MockedProvider>,
     );
 
-    expect(
-      getByText(
-        'By clicking on Delete User button your user will be permanently deleted along with its events, tags and all related data.',
-      ),
-    ).toBeInTheDocument();
-    expect(getAllByText('Delete User')[0]).toBeInTheDocument();
+    expect(getByText('Other Settings')).toBeInTheDocument();
+    expect(getByText('Change Language')).toBeInTheDocument();
   });
 });
