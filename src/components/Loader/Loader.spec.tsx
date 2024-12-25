6eb509952f2a67de0b1a9d3ed8bd5237233b924b
khushi patil <khushipatil1523@gmail.com>
2024-12-25 15:20:26 +0530

    convert test from jest to vitest

--- src/components/Loader/Loader.spec.tsx
+++ src/components/Loader/Loader.spec.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Loader from './Loader';
-import { describe, it } from 'vitest';
+import { describe, it, expect } from 'vitest';
 
 describe('Testing Loader component', () => {
-  it('Component should be rendered properly', () => {
+  it('should render the component properly', () => {
     render(<Loader />);
-
     expect(screen.getByTestId('spinner-wrapper')).toBeInTheDocument();
     expect(screen.getByTestId('spinner')).toBeInTheDocument();
   });
 
-  it('Component should render on custom sizes', () => {
+  it('should render the component with custom sizes', () => {
     render(<Loader size="sm" />);
-
     expect(screen.getByTestId('spinner-wrapper')).toBeInTheDocument();
     expect(screen.getByTestId('spinner')).toBeInTheDocument();
   });
 
-  it('Component should render with large size', () => {
+  it('should render the component with large size', () => {
     render(<Loader size="lg" />);
-
     expect(screen.getByTestId('spinner-wrapper')).toBeInTheDocument();
     expect(screen.getByTestId('spinner')).toBeInTheDocument();
   });
