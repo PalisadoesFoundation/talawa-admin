0ba3839729b4151c2f8fc05d01c485c73d322c87
khushi patil <khushipatil1523@gmail.com>
2024-12-26 04:55:18 +0530

    Fix ESLint warnings and update configuration

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+export default {
+    ignores: [
+      "src/components/CheckIn/tagTemplate.ts", 
+      "package.json",                          
+      "package-lock.json",                     
+      "tsconfig.json",                        
+    ]
+  };
