876b0327152c5b13f9536280670cdd6ae63fd36a
VanshikaSabharwal <vanshikasabharwal06@gmail.com>
2024-12-25 12:31:11 +0530

    removed the unnecessary code and added vitest to askForDocker

--- src/setup/askForDocker/askForDocker.ts
+++ src/setup/askForDocker/askForDocker.ts
@@ -1,13 +1,12 @@
-import type { QuestionCollection } from 'inquirer';
 import inquirer from 'inquirer';
 
 export const askForDocker = async (): Promise<string> => {
-  const questions: QuestionCollection = [
+  const answers = await inquirer.prompt<{ dockerAppPort: string }>([
     {
       type: 'input',
       name: 'dockerAppPort',
       message: 'Enter the port to expose Docker (default: 4321):',
-      default: '4321', // Default should be a string since `input` returns string values
+      default: '4321',
       validate: (input: string) => {
         const port = Number(input);
         if (isNaN(port) || port < 1024 || port > 65535) {
@@ -16,8 +15,7 @@ export const askForDocker = async (): Promise<string> => {
         return true;
       },
     },
-  ];
+  ]);
 
-  const answers = await inquirer.prompt(questions);
   return answers.dockerAppPort;
 };
