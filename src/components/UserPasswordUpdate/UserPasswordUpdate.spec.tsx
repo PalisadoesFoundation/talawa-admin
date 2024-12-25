e2177047a5fed03588a2a53a6dc7b39153b153b5
Rahul Chougule <87270395+rahulch07@users.noreply.github.com>
2024-12-26 03:46:47 +0530

    Refactored jest to vitest #2825 (#2877)
    * refactored Jest to Vitest
    
    * removed jest file

--- src/components/UserPasswordUpdate/UserPasswordUpdate.spec.tsx
+++ src/components/UserPasswordUpdate/UserPasswordUpdate.spec.tsx
@@ -1,42 +1,21 @@
 import React, { act } from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { MockedProvider } from '@apollo/react-testing';
 import userEvent from '@testing-library/user-event';
 import { I18nextProvider } from 'react-i18next';
-import { UPDATE_USER_PASSWORD_MUTATION } from 'GraphQl/Mutations/mutations';
 import i18nForTest from 'utils/i18nForTest';
 import UserPasswordUpdate from './UserPasswordUpdate';
 import { StaticMockLink } from 'utils/StaticMockLink';
 import { toast as mockToast } from 'react-toastify';
+import { MOCKS } from './UserPasswordUpdateMocks';
+import { vi } from 'vitest';
 
-jest.mock('react-toastify', () => ({
+vi.mock('react-toastify', () => ({
   toast: {
-    error: jest.fn(),
-    success: jest.fn(),
+    error: vi.fn(),
+    success: vi.fn(),
   },
 }));
-
-const MOCKS = [
-  {
-    request: {
-      query: UPDATE_USER_PASSWORD_MUTATION,
-      variables: {
-        previousPassword: 'anshgoyal',
-        newPassword: 'anshgoyalansh',
-        confirmNewPassword: 'anshgoyalansh',
-      },
-    },
-    result: {
-      data: {
-        users: [
-          {
-            _id: '1',
-          },
-        ],
-      },
-    },
-  },
-];
 
 const link = new StaticMockLink(MOCKS, true);
 
@@ -56,9 +35,9 @@ describe('Testing User Password Update', () => {
     confirmNewPassword: 'ThePalisadoesFoundation',
   };
 
-  global.alert = jest.fn();
+  global.alert = vi.fn();
 
-  test('should render props and text elements test for the page component', async () => {
+  it('should render props and text elements it for the page component', async () => {
     render(
       <MockedProvider addTypename={false} link={link}>
         <I18nextProvider i18n={i18nForTest}>
@@ -93,7 +72,7 @@ describe('Testing User Password Update', () => {
     ).toBeInTheDocument();
   });
 
-  test('displays an error when the password field is empty', async () => {
+  it('displays an error when the password field is empty', async () => {
     render(
       <MockedProvider addTypename={false} link={link}>
         <I18nextProvider i18n={i18nForTest}>
@@ -108,7 +87,7 @@ describe('Testing User Password Update', () => {
     expect(mockToast.error).toHaveBeenCalledWith(`Password can't be empty`);
   });
 
-  test('displays an error when new and confirm password field does not match', async () => {
+  it('displays an error when new and confirm password field does not match', async () => {
     render(
       <MockedProvider addTypename={false} link={link}>
         <I18nextProvider i18n={i18nForTest}>
@@ -138,6 +117,69 @@ describe('Testing User Password Update', () => {
     await wait();
     expect(mockToast.error).toHaveBeenCalledWith(
       'New and Confirm password do not match.',
+    );
+  });
+
+  it('Successfully update old password', async () => {
+    render(
+      <MockedProvider addTypename={false} link={link}>
+        <I18nextProvider i18n={i18nForTest}>
+          <UserPasswordUpdate id="1" key="123" />
+        </I18nextProvider>
+      </MockedProvider>,
+    );
+
+    await wait();
+
+    userEvent.type(
+      screen.getByPlaceholderText(/Previous Password/i),
+      formData.previousPassword,
+    );
+    userEvent.type(
+      screen.getAllByPlaceholderText(/New Password/i)[0],
+      formData.newPassword,
+    );
+    userEvent.type(
+      screen.getByPlaceholderText(/Confirm New Password/i),
+      formData.confirmNewPassword,
+    );
+
+    userEvent.click(screen.getByText(/Save Changes/i));
+    expect(mockToast.success).toHaveBeenCalledWith(
+      'Password updated Successfully',
+    );
+  });
+
+  it('wrong old password', async () => {
+    render(
+      <MockedProvider addTypename={false} link={link}>
+        <I18nextProvider i18n={i18nForTest}>
+          <UserPasswordUpdate id="1" key="123" />
+        </I18nextProvider>
+      </MockedProvider>,
+    );
+
+    // await wait();
+
+    userEvent.type(
+      screen.getByPlaceholderText(/Previous Password/i),
+      formData.wrongPassword,
+    );
+    userEvent.type(
+      screen.getAllByPlaceholderText(/New Password/i)[0],
+      formData.newPassword,
+    );
+    userEvent.type(
+      screen.getByPlaceholderText(/Confirm New Password/i),
+      formData.confirmNewPassword,
+    );
+
+    userEvent.click(screen.getByText(/Save Changes/i));
+
+    await waitFor(() =>
+      expect(mockToast.error).toHaveBeenCalledWith(
+        'ApolloError: Invalid previous password',
+      ),
     );
   });
 });
