ed8147c927e607e0077473496b28ab2a7840f38b
AQIB-NAWAB <aqibnawab1100@gmail.com>
2024-12-26 18:49:29 +0500

    refactor the setupTests.ts from  jest into vitest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,35 +1,42 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// vitest-dom adds custom matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
-global.fetch = jest.fn();
-
+import { vi } from 'vitest';
 import { format } from 'util';
 
-global.console.error = function (...args): void {
+// Mock global fetch
+global.fetch = vi.fn();
+
+// Override console methods to throw errors
+global.console.error = (...args): void => {
   throw new Error(format(...args));
 };
 
-global.console.warn = function (...args): void {
+global.console.warn = (...args): void => {
   throw new Error(format(...args));
 };
+
+// Mock HTMLMediaElement.prototype.muted setter
 Object.defineProperty(HTMLMediaElement.prototype, 'muted', {
   set: () => ({}),
 });
 
-import { jestPreviewConfigure } from 'jest-preview';
-
-// Global CSS here
+// Import global CSS
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'react-datepicker/dist/react-datepicker.css';
 import 'flag-icons/css/flag-icons.min.css';
 
+import { jestPreviewConfigure } from 'jest-preview';
+
+// Configure Jest Preview for Vitest
 jestPreviewConfigure({
   // Opt-in to automatic mode to preview failed test case automatically.
   autoPreview: true,
 });
 
-jest.setTimeout(18000);
+// Set Vitest timeout
+vi.setConfig({ testTimeout: 400000 });
