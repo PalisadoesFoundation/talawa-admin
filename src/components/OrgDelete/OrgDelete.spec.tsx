7a4eac87c8bfcbd540ceb580311d114f2c77b8d0
Pranav Nathe <93403830+pranavnathe@users.noreply.github.com>
2024-12-26 17:56:57 +0530

    Refactor OrgDelete.test.tsx from Jest to Vitest #2812 (#2921)

--- src/components/OrgDelete/OrgDelete.spec.tsx
+++ src/components/OrgDelete/OrgDelete.spec.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import type { NormalizedCacheObject } from '@apollo/client';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { I18nextProvider } from 'react-i18next';
-
+import { describe, it, expect } from 'vitest';
 import OrgDelete from './OrgDelete';
 import i18nForTest from 'utils/i18nForTest';
 import { BACKEND_URL } from 'Constant/constant';
@@ -14,7 +14,7 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
 });
 
 describe('Testing Organization People List Card', () => {
-  test('should render props and text elements test for the page component', () => {
+  it('should render props and text elements test for the page component', () => {
     render(
       <ApolloProvider client={client}>
         <I18nextProvider i18n={i18nForTest}>
