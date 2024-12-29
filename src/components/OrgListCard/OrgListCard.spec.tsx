4e2a1c266bb96c7290f28c441aeea5196887962a
vivekbisen04 <vivekbisen204@gmail.com>
2024-12-29 11:49:21 +0530

    linting resolved

--- src/components/OrgListCard/OrgListCard.spec.tsx
+++ src/components/OrgListCard/OrgListCard.spec.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { act } from 'react-dom/test-utils';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
