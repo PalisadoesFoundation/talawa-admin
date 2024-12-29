78699b300c05c794b0b9590873baf080d6cce279
Mehul Aggarwal <88583647+AceHunterr@users.noreply.github.com>
2024-12-29 21:21:03 +0530

    Using Global CSS root variables across all files (#3014)
    * Using Global CSS root variables across all files
    
    * Removed green color

--- src/components/EventManagement/EventAttendance/EventAttendance.tsx
+++ src/components/EventManagement/EventAttendance/EventAttendance.tsx
@@ -312,12 +312,12 @@ function EventAttendance(): JSX.Element {
                       componentsProps={{
                         tooltip: {
                           sx: {
-                            backgroundColor: 'white',
+                            backgroundColor: 'var(--bs-white)',
                             fontSize: '2em',
                             maxHeight: '170px',
                             overflowY: 'scroll',
                             scrollbarColor: 'white',
-                            border: '1px solid green',
+                            border: 'var(--primary-border-solid)',
                             borderRadius: '6px',
                             boxShadow: '0 0 5px rgba(0,0,0,0.1)',
                           },
