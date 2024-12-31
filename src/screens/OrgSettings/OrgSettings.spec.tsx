ba3b9c54dc1a69ef81fe6ee20ecce742eaa9a235
vivekbisen04 <vivekbisen204@gmail.com>
2024-12-31 14:58:50 +0530

    Format:fix

--- src/screens/OrgSettings/OrgSettings.spec.tsx
+++ src/screens/OrgSettings/OrgSettings.spec.tsx
@@ -1,7 +1,7 @@
 import type { ReactElement } from 'react';
 import React from 'react';
 import { describe, it, expect, vi } from 'vitest';
-import { render, screen, waitFor, within } from '@testing-library/react';
+import { render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
