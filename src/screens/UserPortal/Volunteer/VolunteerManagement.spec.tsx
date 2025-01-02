4892773eaf22fc58cc328465061a92fe8326017a
Ramneet04 <ramneetsinghrtd@gmail.com>
2025-01-03 01:09:05 +0530

    code coverage success

--- src/screens/UserPortal/Volunteer/VolunteerManagement.spec.tsx
+++ src/screens/UserPortal/Volunteer/VolunteerManagement.spec.tsx
@@ -13,6 +13,7 @@ import userEvent from '@testing-library/user-event';
 import { MOCKS } from './UpcomingEvents/UpcomingEvents.mocks';
 import { StaticMockLink } from 'utils/StaticMockLink';
 import useLocalStorage from 'utils/useLocalstorage';
+import { vi } from 'vitest';
 const { setItem } = useLocalStorage();
 
 const link1 = new StaticMockLink(MOCKS);
@@ -43,10 +44,13 @@ const renderVolunteerManagement = (): RenderResult => {
 
 describe('Volunteer Management', () => {
   beforeAll(() => {
-    jest.mock('react-router-dom', () => ({
-      ...jest.requireActual('react-router-dom'),
-      useParams: () => ({ orgId: 'orgId' }),
-    }));
+    vi.mock('react-router-dom', async () => {
+      const actual = await vi.importActual('react-router-dom'); // Import the actual implementation
+      return {
+        ...actual,
+        useParams: () => ({ orgId: 'orgId' }),
+      };
+    });
   });
 
   beforeEach(() => {
@@ -54,10 +58,11 @@ describe('Volunteer Management', () => {
   });
 
   afterAll(() => {
-    jest.clearAllMocks();
+    vi.clearAllMocks();
   });
 
   it('should redirect to fallback URL if URL params are undefined', async () => {
+    setItem('userId', null);
     render(
       <MockedProvider addTypename={false}>
         <MemoryRouter initialEntries={['/user/volunteer/']}>
@@ -125,5 +130,25 @@ describe('Volunteer Management', () => {
 
     const groupsTab = screen.getByTestId('groupsTab');
     expect(groupsTab).toBeInTheDocument();
+  });
+  test('Component should highlight the selected tab', async () => {
+    renderVolunteerManagement();
+
+    const upcomingEventsBtn = screen.getByTestId('upcomingEventsBtn');
+    const invitationsBtn = screen.getByTestId('invitationsBtn');
+    // Click the invitations tab
+    userEvent.click(invitationsBtn);
+    await waitFor(() => {
+      expect(invitationsBtn).toHaveClass('btn-success');
+      expect(upcomingEventsBtn).not.toHaveClass('btn-success');
+    });
+  });
+  test('should update the component state on tab switch', async () => {
+    renderVolunteerManagement();
+
+    const actionsBtn = screen.getByTestId('actionsBtn');
+    userEvent.click(actionsBtn);
+    const actionsTab = screen.getByTestId('actionsTab');
+    expect(actionsTab).toBeInTheDocument();
   });
 });
