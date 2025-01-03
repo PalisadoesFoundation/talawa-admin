9c14ee6acba064fea19e56e21c248fbea38aad14
Swastik Ranjan Panigrahi <textswastik.alt@gmail.com>
2025-01-03 08:20:37 +0530

    Update TableLoader.spec.tsx

--- src/components/TableLoader/TableLoader.spec.tsx
+++ src/components/TableLoader/TableLoader.spec.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
-import { vi } from 'vitest';
-import { describe, it, beforeAll, expect } from 'vitest';
+import { vi, describe, it, beforeAll, expect } from 'vitest';
 
 import type { InterfaceTableLoader } from './TableLoader';
 import TableLoader from './TableLoader';
