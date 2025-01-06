425bcfecc861e72673644b257ce9f5224d7a80af
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2025-01-05 21:31:48 -0800

    20250105213148 Deleted all files in the develop branch in anticipation of merging develop-postgres into develop cleanly

--- config/babel.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: [
-    '@babel/preset-env', // Transforms modern JavaScript
-    '@babel/preset-typescript', // Transforms TypeScript
-    '@babel/preset-react', // Transforms JSX
-  ],
-  plugins: ['babel-plugin-transform-import-meta'],
-};
