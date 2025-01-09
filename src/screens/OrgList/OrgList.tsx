38ef3efbe5943162f82a18dd3a87cda2a822f3f4
gautam-divyanshu <gautamdivyanshu034@gmail.com>
2025-01-09 23:26:13 +0530

    errors solved

--- src/screens/OrgList/OrgList.tsx
+++ src/screens/OrgList/OrgList.tsx
@@ -360,6 +360,7 @@ function orgList(): JSX.Element {
             selectedOption={sortingState.selectedOption}
             onSortChange={handleSortChange}
             dataTestIdPrefix="sortOrgs"
+            dropdownTestId="sort"
           />
           {superAdmin && (
             <Button
