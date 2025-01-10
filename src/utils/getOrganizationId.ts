62b533de9045db7ce5a1d415b21269b84fd7dd36
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2025-01-10 08:25:58 -0800

    20250110082558 Deleted all files in the main branch in anticipation of merging develop into main cleanly

--- src/utils/getOrganizationId.ts
+++
@@ -1,8 +0,0 @@
-/* istanbul ignore next */
-const getOrganizationId = (url: string): string => {
-  const id = url?.split('=')[1];
-
-  return id?.split('#')[0];
-};
-
-export default getOrganizationId;
