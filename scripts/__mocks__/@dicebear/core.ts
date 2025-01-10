c591a00c826c5a4c3f31ee45924a641c4eb86c3a
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2025-01-09 19:04:18 -0800

    20250109190418 Deleted all files in the develop branch in anticipation of merging develop-postgres into develop cleanly

--- scripts/__mocks__/@dicebear/core.ts
+++
@@ -1,5 +0,0 @@
-export const createAvatar = jest.fn(() => {
-  return {
-    toDataUri: jest.fn(() => 'mocked-data-uri'),
-  };
-});
--- src/state/hooks.ts
+++
@@ -1,5 +0,0 @@
-import { useDispatch } from 'react-redux';
-import type { AppDispatch } from './store';
-
-// Use throughout your app instead of plain `useDispatch`
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
