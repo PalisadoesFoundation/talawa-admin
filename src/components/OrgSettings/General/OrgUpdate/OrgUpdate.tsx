44daf3aa6723a12e46f9d8b3ca21759372523825
AceHunterr <aggarwalmehul26@gmail.com>
2025-01-14 03:37:43 +0530

    Fixed bugs

--- src/components/OrgSettings/General/OrgUpdate/OrgUpdate.tsx
+++ src/components/OrgSettings/General/OrgUpdate/OrgUpdate.tsx
@@ -142,7 +142,6 @@ function orgUpdate(props: InterfaceOrgUpdateProps): JSX.Element {
           file: formState.orgImage,
         },
       });
-      // istanbul ignore next
       if (data) {
         refetch({ id: orgId });
         toast.success(t('successfulUpdated') as string);
@@ -358,7 +357,6 @@ function orgUpdate(props: InterfaceOrgUpdateProps): JSX.Element {
             onChange={async (e: React.ChangeEvent): Promise<void> => {
               const target = e.target as HTMLInputElement;
               const file = target.files && target.files[0];
-              /* istanbul ignore else */
               if (file)
                 setFormState({
                   ...formState,
