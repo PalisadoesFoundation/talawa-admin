e8d9e52cd10b2ac45ada3a7d049d4357f179ba1d
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2025-01-23 08:21:19 -0800

    Revert "Revert "refactor: homepage/doc-sidebar (#3389)" (#3399)"
    This reverts commit ece035fa4f20abed173a0165865bfec269687049.

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,37 +1,17 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
-
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={styles.heroBanner}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
+import Head from '@docusaurus/Head';
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
+  const pageTitle = 'Talawa-Docs: Powered by The Palisadoes';
+
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={pageTitle}
       description="Description will go into a meta tag in <head />"
     >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      <Head>
+        <meta name="robots" content="noindex" />
+      </Head>
     </Layout>
   );
 }
