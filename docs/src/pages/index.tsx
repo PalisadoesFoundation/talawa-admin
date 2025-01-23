5ff3ce808f3327b48dd31f9b3b322b8ea51fdfb2
Rukh-Khan <khanrukhsar1890@gmail.com>
2025-01-23 13:08:52 +0530

    title-tab-match

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,12 +1,12 @@
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import Head from '@docusaurus/Head';
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
+  const pageTitle = 'Talawa-Docs: Powered by The Palisadoes';
+
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={pageTitle}
       description="Description will go into a meta tag in <head />"
     >
       <Head>
