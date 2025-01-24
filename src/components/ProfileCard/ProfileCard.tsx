d2ed676f7cf4d6ebabe96eb55fa8e6abb4ffccc5
hustlernik <hustlernikxd@gmail.com>
2025-01-24 23:33:04 +0530

    UI fix UserSidebarOrg

---
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -0,0 +1,96 @@
+import Avatar from 'components/Avatar/Avatar';
+import React from 'react';
+import { ButtonGroup, Dropdown } from 'react-bootstrap';
+import { useNavigate, useParams } from 'react-router-dom';
+import useLocalStorage from 'utils/useLocalstorage';
+import styles from './../../style/app.module.css';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
+/**
+ * Renders a profile card for the user.
+ *
+ * This component displays the user's profile picture or an avatar, their name (truncated if necessary),
+ * and their role (SuperAdmin, Admin, or User). It provides options to view the profile.
+ *
+ * - If a user image is available, it displays that; otherwise, it shows an avatar.
+ * - The displayed name is truncated if it exceeds a specified length.
+
+ *
+ * @returns JSX.Element - The profile card .
+ */
+const profileCard = (): JSX.Element => {
+  const { getItem } = useLocalStorage();
+  const superAdmin = getItem('SuperAdmin');
+  const adminFor = getItem('AdminFor');
+  const userRole = superAdmin
+    ? 'SuperAdmin'
+    : adminFor?.length > 0
+      ? 'Admin'
+      : 'User';
+  const firstName = getItem('FirstName');
+  const lastName = getItem('LastName');
+  const userImage = getItem('UserImage');
+  const navigate = useNavigate();
+  const { orgId } = useParams();
+
+  const MAX_NAME_LENGTH = 20;
+  const fullName = `${firstName} ${lastName}`;
+  const displayedName =
+    fullName.length > MAX_NAME_LENGTH
+      ? /*istanbul ignore next*/
+        fullName.substring(0, MAX_NAME_LENGTH - 3) + '...'
+      : fullName;
+
+  return (
+    <Dropdown as={ButtonGroup} variant="none">
+      <div className={styles.profileContainer}>
+        <div className={styles.imageContainer}>
+          {userImage && userImage !== 'null' ? (
+            <img
+              src={userImage}
+              alt={`profile picture`}
+              data-testid="display-img"
+            />
+          ) : (
+            <Avatar
+              data-testid="display-img"
+              size={45}
+              avatarStyle={styles.avatarStyle}
+              name={`${firstName} ${lastName}`}
+              alt={`dummy picture`}
+            />
+          )}
+        </div>
+        <div className={styles.profileTextUserSidebarOrg}>
+          <span className={styles.primaryText} data-testid="display-name">
+            {displayedName}
+          </span>
+          <span className={styles.secondaryText} data-testid="display-type">
+            {`${userRole}`}
+          </span>
+        </div>
+        <button
+          className="border-0 bg-white"
+          data-testid="profileBtn"
+          onClick={() =>
+            userRole === 'User'
+              ? navigate(`/user/settings`)
+              : navigate(`/member/${orgId || ''}`)
+          }
+        >
+          <ChevronRightIcon
+            sx={{
+              color: 'gray',
+              background: 'white',
+              fontSize: 40,
+              strokeWidth: 0.5,
+              marginLeft: '50px',
+            }}
+          />
+        </button>
+      </div>
+    </Dropdown>
+  );
+};
+
+export default profileCard;
---
+++ src/components/UserPortal/OrganizationCard/SignOut/SignOut.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styles from './../style/app.module.css';
+import LogoutIcon from '@mui/icons-material/Logout';
+import useSession from 'utils/useSession';
+import { REVOKE_REFRESH_TOKEN } from 'GraphQl/Mutations/mutations';
+import { useMutation } from '@apollo/client';
+import { useNavigate } from 'react-router-dom';
+
+/**
+ * Renders a sign out button.
+ *
+ * This component helps to logout.
+ * The logout function revokes the refresh token and clears local storage before redirecting to the home page.
+
+ *
+ * @returns JSX.Element - The profile card .
+ */
+
+const SignOut = (): JSX.Element => {
+  const { endSession } = useSession();
+  const [revokeRefreshToken] = useMutation(REVOKE_REFRESH_TOKEN);
+  const navigate = useNavigate();
+
+  const logout = async (): Promise<void> => {
+    try {
+      await revokeRefreshToken();
+    } catch (error) {
+      console.error('Error revoking refresh token:', error);
+    }
+    localStorage.clear();
+    endSession();
+    navigate('/');
+  };
+  return (
+    <div className={styles.signOutContainer}>
+      <LogoutIcon />
+      <button className={styles.signOutButton} onClick={logout}>
+        Sign Out
+      </button>
+    </div>
+  );
+};
+
+export default SignOut;
