bdab69f1b4fa74d4d09a72f0572cef146235d257
rahulch07 <chouguler310@gmail.com>
2025-02-01 13:56:49 +0530

    fixed orgListCard.spec.tsx

--- src/components/OrgListCard/OrgListCard.spec.tsx
+++ src/components/OrgListCard/OrgListCard.spec.tsx
@@ -88,7 +88,6 @@ describe('Testing the Super Dash List', () => {
     expect(screen.getByText('Dogs Care')).toBeDefined();
     expect(screen.getByText(/Texas/i)).toBeDefined();
     expect(screen.getByTestId(/manageBtn/i)).toBeDefined();
-    expect(screen.getByTestId(/flaskIcon/i)).toBeDefined();
 
     await userEvent.click(screen.getByTestId(/manageBtn/i));
     removeItem('id');
