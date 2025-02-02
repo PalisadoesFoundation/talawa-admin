bf5a8c34d1570a8be0631634ad4069e33bd51fbd
Syed Ali Ul Hasan <syedaliulhasan19@gmail.com>
2025-02-02 20:00:06 +0530

    resolved coderabbit conversation

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,6 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { vi } from 'vitest';
 global.fetch = vi.fn();
 
 import { format } from 'util';
@@ -25,7 +26,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'react-datepicker/dist/react-datepicker.css';
 import 'flag-icons/css/flag-icons.min.css';
-import { vi } from 'vitest';
 
 jestPreviewConfigure({
   // Opt-in to automatic mode to preview failed test case automatically.
