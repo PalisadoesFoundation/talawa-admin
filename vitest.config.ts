cbaea36f1ced4f3700db8c283e624cae1d0eaa30
JaiPannu-IITI <pannujai06iiti@gmail.com>
2025-02-05 10:03:14 +0530

    vite-plugin-node-polyfills upgraded

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgrPlugin()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ['events'],
+    }),
+    tsconfigPaths(),
+    svgrPlugin(),
+  ],
   test: {
     include: ['src/**/*.spec.{js,jsx,ts,tsx}'],
     globals: true,
