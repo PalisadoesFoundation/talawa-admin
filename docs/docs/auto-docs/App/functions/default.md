bb4c61395bf7e003bb419a959ff9735b5ed2b6ee
adithya <adithyav102@gmail.com>
2025-02-15 21:43:09 +0530

    Fix failing test

--- docs/docs/auto-docs/App/functions/default.md
+++ docs/docs/auto-docs/App/functions/default.md
@@ -6,7 +6,7 @@
 
 > **default**(): `JSX.Element`
 
-Defined in: [src/App.tsx:72](https://github.com/PalisadoesFoundation/talawa-admin/blob/main/src/App.tsx#L72)
+Defined in: [src/App.tsx:114](https://github.com/PalisadoesFoundation/talawa-admin/blob/main/src/App.tsx#L114)
 
 This is the main function for our application. It sets up all the routes and components,
 defining how the user can navigate through the app. The function uses React Router's `Routes`
--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import SecuredRoute from 'components/SecuredRoute/SecuredRoute';
 import SecuredRouteForUser from 'components/UserPortal/SecuredRouteForUser/SecuredRouteForUser';
 import * as installedPlugins from 'components/plugins/index';
 import OrganizaitionFundCampiagn from 'screens/OrganizationFundCampaign/OrganizationFundCampagins';
+import LoginPage from 'screens/LoginPage/LoginPage';
 
 const AddOnStore = lazy(
   () => import('components/AddOn/core/AddOnStore/AddOnStore'),
@@ -24,7 +25,6 @@ const EventManagement = lazy(
 const ForgotPassword = lazy(
   () => import('screens/ForgotPassword/ForgotPassword'),
 );
-const LoginPage = lazy(() => import('screens/LoginPage/LoginPage'));
 const MemberDetail = lazy(() => import('screens/MemberDetail/MemberDetail'));
 const OrgContribution = lazy(
   () => import('screens/OrgContribution/OrgContribution'),
