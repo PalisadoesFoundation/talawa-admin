c56725a0d649e8b613a0edfca5ec376e1835efe9
hustlernik <hustlernikxd@gmail.com>
2025-02-23 21:57:54 +0530

    css refactor

--- src/screens/FundCampaignPledge/FundCampaignPledge.tsx
+++ src/screens/FundCampaignPledge/FundCampaignPledge.tsx
@@ -9,7 +9,7 @@ import { Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { Navigate, useParams } from 'react-router-dom';
 import { currencySymbols } from 'utils/currency';
-import styles from '../../style/app.module.css';
+import styles from '../../style/app-fixed.module.css';
 import PledgeDeleteModal from './PledgeDeleteModal';
 import PledgeModal from './PledgeModal';
 import { Breadcrumbs, Link, Stack, Typography } from '@mui/material';
