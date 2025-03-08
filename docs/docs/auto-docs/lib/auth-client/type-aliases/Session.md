26e6bd88ef148ef216f943ab1b3933ea2d8fa32a
Purnendu Mishra <purnendumishra129th@gmail.com>
2025-03-08 18:36:39 +0000

    added docs for better auth

--- docs/docs/auto-docs/lib/auth-client/type-aliases/Session.md
+++
@@ -1,9 +0,0 @@
-[Admin Docs](/)
-
-***
-
-# Type Alias: Session
-
-> **Session**: *typeof* `authClient.$Infer.Session`
-
-Defined in: [src/lib/auth-client.ts:5](https://github.com/PalisadoesFoundation/talawa-admin/blob/main/src/lib/auth-client.ts#L5)
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,4 @@
 import { createAuthClient } from 'better-auth/react';
 export const authClient = createAuthClient({
-  baseURL: 'http://localhost:4000', // the base url of your auth server
+  baseURL: process.env.BASE_URL_SERVER, // the base url of your auth server
 });
-export type Session = typeof authClient.$Infer.Session;
