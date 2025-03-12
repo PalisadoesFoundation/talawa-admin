599fbcc0f30a53ac8c593a0f5cf8070531872e42
hustlernik <hustlernikxd@gmail.com>
2025-03-12 18:08:10 +0530

    lint fix

--- src/GraphQl/Queries/Queries.ts
+++ src/GraphQl/Queries/Queries.ts
@@ -1078,29 +1078,29 @@ export { ACTION_ITEM_CATEGORY_LIST } from './ActionItemCategoryQueries';
 export { ACTION_ITEM_LIST } from './ActionItemQueries';
 
 export {
-  AgendaItemByEvent,
-  AgendaItemByOrganization,
+	AgendaItemByEvent,
+	AgendaItemByOrganization,
 } from './AgendaItemQueries';
 
 export { AGENDA_ITEM_CATEGORY_LIST } from './AgendaCategoryQueries';
 // to take the list of the blocked users
 export {
-  ADVERTISEMENTS_GET,
-  IS_SAMPLE_ORGANIZATION_QUERY,
-  ORGANIZATION_EVENTS_CONNECTION,
+	ADVERTISEMENTS_GET,
+	IS_SAMPLE_ORGANIZATION_QUERY,
+	ORGANIZATION_EVENTS_CONNECTION,
 } from './PlugInQueries';
 
 // display posts
 export {
-  ORGANIZATION_POST_LIST,
-  ORGANIZATION_ADVERTISEMENT_LIST,
+	ORGANIZATION_POST_LIST,
+	ORGANIZATION_ADVERTISEMENT_LIST,
 } from './OrganizationQueries';
 
 export {
-  ORGANIZATION_ADMINS_LIST,
-  USER_CREATED_ORGANIZATIONS,
-  USER_JOINED_ORGANIZATIONS,
-  USER_ORGANIZATION_CONNECTION,
+	ORGANIZATION_ADMINS_LIST,
+	USER_CREATED_ORGANIZATIONS,
+	USER_JOINED_ORGANIZATIONS,
+	USER_ORGANIZATION_CONNECTION,
 } from './OrganizationQueries';
 
 export const GET_ORGANIZATION_EVENTS = gql`
