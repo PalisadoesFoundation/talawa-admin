a8c04c717dbebac36b8b564b5374f23bb1cba563
adithya <adithyav102@gmail.com>
2025-03-16 11:09:08 +0530

    Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,7 @@ const { setItem } = useLocalStorage();
 /**
  * This is the main function for our application. It sets up all the routes and components,
  * defining how the user can navigate through the app. The function uses React Router's `Routes`
- * and `Route`   components to map different URL paths to corresponding screens and components.
+ * and `Route`  components to map different URL paths to corresponding screens and components.
  *
  * ## Important Details
  * - **UseEffect Hook**: This hook checks user authentication status using the `CHECK_AUTH` GraphQL query.
