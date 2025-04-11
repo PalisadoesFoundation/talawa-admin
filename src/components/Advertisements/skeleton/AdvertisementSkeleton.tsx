aae931829a84e0b4cfcb5162096c083c9b8edde5
bandhan-majumder <bandhanmajumder16@gmail.com>
2025-04-11 10:27:28 +0530

    add: tsdocs added

--- src/components/Advertisements/skeleton/AdvertisementSkeleton.tsx
+++ src/components/Advertisements/skeleton/AdvertisementSkeleton.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import styles from 'style/app-fixed.module.css';
 
+/**
+ * AdvertisementSkeleton Component
+ *
+ * This component renders a skeleton loader for advertisements, typically used
+ * as a placeholder while the actual advertisement data is being fetched or loaded.
+ * It creates a list of 6 skeleton items, each styled to resemble the layout of an
+ * advertisement card.
+ *
+ * Each skeleton item includes:
+ * - A shimmering image container to represent the advertisement image.
+ * - A shimmering title placeholder to represent the advertisement name.
+ * - A shimmering button placeholder.
+ *
+ * The skeleton items are styled using CSS classes provided by the `styles` object,
+ * and each item is uniquely identified with a `data-testid` attribute for testing purposes.
+ *
+ * @returns {JSX.Element[]} An array of JSX elements representing the skeleton loaders.
+ */
 export function AdvertisementSkeleton() {
   return [...Array(6)].map((_, index) => (
     <div
