040e91dd247931fce696debf4e6d2c0b4ace5a41
BharathC0 <bharatha52452@gmail.com>
2025-08-30 09:24:17 +0530

    fix: ensure automatic JSX runtime and resolve type errors

--- config/babel.config.cjs
+++ config/babel.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   presets: [
     '@babel/preset-env', // Transforms modern JavaScript
     '@babel/preset-typescript', // Transforms TypeScript
-    '@babel/preset-react', // Transforms JSX
+    ['@babel/preset-react', { runtime: 'classic' }], // Transforms JSX
   ],
   plugins: ['babel-plugin-transform-import-meta', 'istanbul'],
 };
