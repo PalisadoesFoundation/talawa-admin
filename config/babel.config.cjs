c50ef0188572a80553e11d8e1933ba9b26b12a3f
BharathC0 <bharatha52452@gmail.com>
2025-08-30 09:39:01 +0530

    Update config/babel.config.cjs
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- config/babel.config.cjs
+++ config/babel.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   presets: [
     '@babel/preset-env', // Transforms modern JavaScript
     '@babel/preset-typescript', // Transforms TypeScript
-    ['@babel/preset-react', { runtime: 'classic' }], // Transforms JSX
+    ['@babel/preset-react', { runtime: 'automatic' }], // Transforms JSX
   ],
   plugins: ['babel-plugin-transform-import-meta', 'istanbul'],
 };
