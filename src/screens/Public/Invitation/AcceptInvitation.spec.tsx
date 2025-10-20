c6ec5681ec7798fbdf7323331102907517792753
iamanishx <manishbiswal754@gmail.com>
2025-10-21 00:20:13 +0530

    fix: tests
    Signed-off-by: iamanishx <manishbiswal754@gmail.com>

--- src/screens/Public/Invitation/AcceptInvitation.spec.tsx
+++ src/screens/Public/Invitation/AcceptInvitation.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { MemoryRouter, Route, Routes } from 'react-router';
 import { MockedProvider } from '@apollo/client/testing';
 import type { MockedResponse } from '@apollo/client/testing';
 import {
