56263d1072684899826ca40e9bf427e8c5d5d256
Harsh <harshk1729@gmail.com>
2025-11-01 05:51:56 +0530

    fix spelling

--- src/screens/Users/Users.tsx
+++ src/screens/Users/Users.tsx
@@ -307,7 +307,7 @@ const Users = (): JSX.Element => {
         <div className={styles.message} data-testid="errorMsg">
           <WarningAmberRounded className={styles.errorIcon} fontSize="large" />
           <h6 className="fw-bold text-danger text-center">
-            Error occured while loading Users
+            Error occurred while loading Users
             <br />
             {UsersError.message}
           </h6>
