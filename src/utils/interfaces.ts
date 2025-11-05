7fe2009692e34943d16237526fa1fbbff2123c92
Harsh <harshk1729@gmail.com>
2025-11-06 03:22:30 +0530

    fixed org creator name in modal table

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -2032,24 +2032,27 @@ export interface InterfaceQueryUserListItem {
         city?: string;
         state?: string;
         countryCode?: string;
-        blockedUsers?: {
-          edges: {
-            node: {
-              id: string;
-              name: string;
-              emailAddress: string;
-              avatarURL?: string;
-              city: string;
-              state: string;
-              createdAt: string;
-            };
-          }[];
-        };
         creator: {
           id: string;
           name: string;
           emailAddress: string;
           avatarURL?: string;
+        };
+      };
+    }[];
+  };
+
+  orgsWhereUserIsBlocked?: {
+    edges: {
+      node: {
+        id: string;
+        name: string;
+        avatarURL?: string;
+        city: string;
+        state: string;
+        createdAt: string;
+        creator: {
+          name: string;
         };
       };
     }[];
