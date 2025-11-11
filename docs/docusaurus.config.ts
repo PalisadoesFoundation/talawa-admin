e8f806a55b297c0bd9d05e787a85d3c2f56774ac
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2025-11-10 23:55:22 -0500

    Fixed dark mode preference

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -186,7 +186,7 @@ const config: Config = {
       colorMode: {
         defaultMode: 'light',
         disableSwitch: false,
-        respectPrefersColorScheme: false,
+        respectPrefersColorScheme: true,
       },
       prism: {
         theme: prismThemes.github,
