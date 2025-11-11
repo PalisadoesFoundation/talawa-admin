9961d384ffeeba56b81a908ebad21fcc842686ea
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2025-11-10 23:48:39 -0500

    Fixed file formatting

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -19,10 +19,10 @@ const config: Config = {
   onBrokenLinks: 'throw',
   markdown: {
     hooks: {
-      onBrokenMarkdownLinks: "warn", // Or 'throw', 'ignore'
+      onBrokenMarkdownLinks: 'warn', // Or 'throw', 'ignore'
     },
   },
-  
+
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
