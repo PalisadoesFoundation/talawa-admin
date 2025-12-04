10d2bc03c5e36cb6ce86851cc4ad2a165cdb76c6
priyanshu bharti <bhartipriyanshustm@gmail.com>
2025-12-04 18:41:31 +0530

    fix: correct Texas text match in OrgListCard test

--- src/components/OrgListCard/OrgListCard.spec.tsx
+++ src/components/OrgListCard/OrgListCard.spec.tsx
@@ -91,6 +91,7 @@ describe('Testing the Super Dash List', () => {
     expect(screen.getByAltText(/Dogs Care image/i)).toBeDefined();
     expect(screen.getByText(/Members:/i)).toBeDefined();
     expect(screen.getByText('Dogs Care')).toBeDefined();
+
     expect(screen.getByText(/Texas, USA/i)).toBeInTheDocument();
     expect(screen.getByTestId(/manageBtn/i)).toBeDefined();
 
