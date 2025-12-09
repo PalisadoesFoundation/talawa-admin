a5d788096c508734fec7d1a16a47bb81803d678d
unknown <rithvikbansal0501@gmail.com>
2025-12-09 07:14:01 +0530

    Fix: 100% coverage

--- src/screens/OrgContribution/OrgContribution.spec.tsx
+++ src/screens/OrgContribution/OrgContribution.spec.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { MockedProvider } from '@apollo/react-testing';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router';
 import { vi, describe, test, expect } from 'vitest';
 import { I18nextProvider } from 'react-i18next';
-
 import OrgContribution from './OrgContribution';
 import { store } from 'state/store';
 import i18nForTest from 'utils/i18nForTest';
@@ -16,6 +15,20 @@ async function wait(ms = 100): Promise<void> {
     setTimeout(resolve, ms);
   });
 }
+
+const renderComponent = () => {
+  return render(
+    <MockedProvider link={link}>
+      <BrowserRouter>
+        <Provider store={store}>
+          <I18nextProvider i18n={i18nForTest}>
+            <OrgContribution />
+          </I18nextProvider>
+        </Provider>
+      </BrowserRouter>
+    </MockedProvider>,
+  );
+};
 
 describe('Organisation Contribution Page', () => {
   beforeEach(() => {
@@ -36,17 +49,7 @@ describe('Organisation Contribution Page', () => {
 
     window.location.assign('/orglist');
 
-    const { container } = render(
-      <MockedProvider link={link}>
-        <BrowserRouter>
-          <Provider store={store}>
-            <I18nextProvider i18n={i18nForTest}>
-              <OrgContribution />
-            </I18nextProvider>
-          </Provider>
-        </BrowserRouter>
-      </MockedProvider>,
-    );
+    const { container } = renderComponent();
 
     expect(container.textContent).not.toBe('Loading data...');
     await wait();
@@ -55,5 +58,17 @@ describe('Organisation Contribution Page', () => {
     expect(container.textContent).toMatch('Recent Stats');
     expect(container.textContent).toMatch('Contribution');
     expect(window.location.assign).toHaveBeenCalledWith('/orglist');
+  });
+
+  test('onsearch called in initial render', () => {
+    renderComponent();
+
+    const orgInput = screen.getByTestId('filterOrgName');
+    fireEvent.keyDown(orgInput, { key: 'Enter' });
+    expect(orgInput).toBeInTheDocument();
+
+    const txnInput = screen.getByTestId('filterTransaction');
+    fireEvent.keyDown(txnInput, { key: 'Enter' });
+    expect(txnInput).toBeInTheDocument();
   });
 });
