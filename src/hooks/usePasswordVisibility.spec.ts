bc672103426f9ab3ca928c4e165ac401101ab249
kushu30 <kushu123456789@gmail.com>
2025-12-26 21:29:34 +0530

    chore: move hook test and revert sensitive dependency files

--- src/hooks/usePasswordVisibility.spec.ts
+++ src/hooks/usePasswordVisibility.spec.ts
@@ -1,5 +1,5 @@
 import { renderHook, act } from '@testing-library/react';
-import { usePasswordVisibility } from '../../hooks/usePasswordVisibility';
+import { usePasswordVisibility } from './usePasswordVisibility';
 
 describe('usePasswordVisibility', () => {
   it('defaults showPassword to false', () => {
