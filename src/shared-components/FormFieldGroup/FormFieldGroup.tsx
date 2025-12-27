7c722f086d20a064a0f2c13eb6d569a133ec96d1
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-12-27 16:04:56 +0530

    FormFieldGroup (7 of 7)

---
+++ src/shared-components/FormFieldGroup/FormFieldGroup.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import type { FormFieldGroupProps } from './types';
+
+export const FormFieldGroup: React.FC<
+  FormFieldGroupProps & { children: React.ReactNode }
+> = ({
+  name,
+  label,
+  children,
+  groupClass,
+  labelClass,
+  error,
+  touched,
+  required,
+  helpText,
+}) => {
+  return (
+    <Form.Group controlId={name} className={groupClass}>
+      {label && (
+        <Form.Label className={labelClass}>
+          {label} {required && <span aria-label="required">*</span>}
+        </Form.Label>
+      )}
+      {children}
+      {touched && error && (
+        <Form.Control.Feedback type="invalid" className="d-block">
+          {error}
+        </Form.Control.Feedback>
+      )}
+      {helpText && !error && (
+        <Form.Text className="text-muted">{helpText}</Form.Text>
+      )}
+    </Form.Group>
+  );
+};
