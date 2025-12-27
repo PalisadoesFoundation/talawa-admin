0ae29d242b2839257965ae525f37250092d7953f
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-12-27 16:04:05 +0530

    FormDateField (6 of 7)

---
+++ src/shared-components/FormFieldGroup/FormDateField.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { FormDateFieldProps } from './types';
+
+export const FormDateField: React.FC<FormDateFieldProps> = ({
+  value,
+  onChange,
+  format = 'YYYY-MM-DD',
+  maxDate,
+  minDate,
+  className,
+  label,
+  slotProps,
+  ...datePickerProps
+}) => {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker
+        format={format}
+        value={value}
+        onChange={onChange}
+        className={className}
+        label={label}
+        maxDate={maxDate}
+        minDate={minDate}
+        slotProps={slotProps}
+        {...datePickerProps}
+      />
+    </LocalizationProvider>
+  );
+};
