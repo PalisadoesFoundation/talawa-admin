731087455f22c236e98938051d26f9a132326195
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-12-27 16:08:29 +0530

    Index barrel file

---
+++ src/shared-components/FormFieldGroup/index.ts
@@ -0,0 +1,16 @@
+export { FormFieldGroup } from './FormFieldGroup';
+export { FormTextField } from './FormTextField';
+export { FormTextArea } from './FormTextArea';
+export { FormSelect } from './FormSelect';
+export { FormCheckbox } from './FormCheckbox';
+export { FormRadioGroup } from './FormRadioGroup';
+export { FormDateField } from './FormDateField';
+export type {
+  FormFieldGroupProps,
+  FormTextFieldProps,
+  FormTextAreaProps,
+  FormSelectProps,
+  FormCheckBoxProps,
+  FormRadioGroupProps,
+  FormDateFieldProps,
+} from './types';
