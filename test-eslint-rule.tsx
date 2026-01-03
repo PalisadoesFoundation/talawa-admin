3a368695a86773a80963044d89340ee3b3a36ba1
SujalTripathi <SujalTripathi@users.noreply.github.com>
2026-01-03 20:22:09 +0530

    test: Add test file with @mui/x-data-grid import to demonstrate ESLint rule
    This commit intentionally violates the rule to show CI failure.
    Will be removed in next commit.

---
+++ test-eslint-rule.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+
+// This is a test file to demonstrate the ESLint rule
+export const TestComponent = (): JSX.Element => {
+  return <div>Test</div>;
+};
