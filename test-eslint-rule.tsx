e465402034dcfa5e13b0809e0a59adaae01d3fe6
SujalTripathi <SujalTripathi@users.noreply.github.com>
2026-01-03 20:46:10 +0530

    test: Remove test file - demonstration complete
    The test file test-eslint-rule.tsx successfully demonstrated:
    -  CI FAILED when file imported @mui/x-data-grid (rule caught it)
    -  CI will PASS after removal (rule working correctly)
    
    Only eslint.config.js remains in this PR.

--- test-eslint-rule.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
-
-// This is a test file to demonstrate the ESLint rule
-export const TestComponent = (): JSX.Element => {
-  return <div>Test</div>;
-};
