f7d09e3339aae0e7a62a5680112101f49a0a38af
Jashandeep Singh <jashandeep1849@gmail.com>
2026-01-06 23:31:36 +0530

    Feat: Created reusable ProfileFormCard component (#6199)
    * Created reusable ProfileFormCard Component
    
    * implemented coderabbit suggestions
    
    * implemented coderabbit suggestion
    
    * implemented coderabbit suggestions
    
    * fixed mock
    
    ---------
    
    Co-authored-by: Peter Harrison <16875803+palisadoes@users.noreply.github.com>

--- src/screens/AdminPortal/OrganizationPeople/OrganizationPeople.tsx
+++ src/screens/AdminPortal/OrganizationPeople/OrganizationPeople.tsx
@@ -437,7 +437,7 @@ function OrganizationPeople(): JSX.Element {
       renderCell: (params: GridCellParams) => {
         return (
           <Link
-            to={`/member/${currentUrl}`}
+            to={`/member/${currentUrl}/${params.row.id}`}
             state={{ id: params.row.id }}
             className={`${styles.membername} ${styles.subtleBlueGrey} ${styles.memberNameFontSize}`}
           >
