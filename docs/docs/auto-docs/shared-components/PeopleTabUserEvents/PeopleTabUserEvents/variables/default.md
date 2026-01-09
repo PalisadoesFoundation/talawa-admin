dab3e50066bcf15d4aeec60cc309ed1e4a6ff504
Vanshika Sabharwal <143436704+VanshikaSabharwal@users.noreply.github.com>
2026-01-09 21:23:43 +0530

    fix: Added people tab components as shared components (#6291)
    * fix: added people tab components as shared components
    
    * fix: added docs
    
    * fix: added the test

---
+++ docs/docs/auto-docs/shared-components/PeopleTabUserEvents/PeopleTabUserEvents/variables/default.md
@@ -0,0 +1,9 @@
+[Admin Docs](/)
+
+***
+
+# Variable: default
+
+> `const` **default**: `React.FC`\<[`InterfacePeopletabUserEventsProps`](../../../../types/PeopleTab/interface/interfaces/InterfacePeopletabUserEventsProps.md)\>
+
+Defined in: [src/shared-components/PeopleTabUserEvents/PeopleTabUserEvents.tsx:46](https://github.com/PalisadoesFoundation/talawa-admin/blob/main/src/shared-components/PeopleTabUserEvents/PeopleTabUserEvents.tsx#L46)
--- src/subComponents/SortingButton.tsx
+++ src/subComponents/SortingButton.tsx
@@ -4,6 +4,7 @@ import SortIcon from '@mui/icons-material/Sort';
 import FilterAltOutlined from '@mui/icons-material/FilterAltOutlined';
 import PropTypes from 'prop-types';
 import styles from '../style/app-fixed.module.css';
+import { useTranslation } from 'react-i18next';
 
 interface InterfaceSortingOption {
   /** The label to display for the sorting option */
@@ -33,6 +34,7 @@ interface InterfaceSortingButtonProps {
   type?: 'sort' | 'filter';
   /** Accessible label for the dropdown button (screen readers) */
   ariaLabel?: string;
+  icon?: string | null;
 }
 
 /**
@@ -54,9 +56,11 @@ const SortingButton: React.FC<InterfaceSortingButtonProps> = ({
   buttonLabel,
   type = 'sort',
   ariaLabel,
+  icon,
 }) => {
   // Determine the icon based on the type
   const IconComponent = type === 'filter' ? FilterAltOutlined : SortIcon;
+  const { t: tCommon } = useTranslation('common');
 
   return (
     <Dropdown aria-expanded="false" title={title} data-testid={dropdownTestId}>
@@ -66,12 +70,21 @@ const SortingButton: React.FC<InterfaceSortingButtonProps> = ({
         className={className}
         aria-label={ariaLabel}
       >
-        <IconComponent
-          className={'me-1'}
-          data-testid="sorting-icon"
-          data-icon-type={type}
-          aria-hidden="true"
-        />{' '}
+        <span className={styles.iconRightMargin}>
+          {icon ? (
+            <img
+              src={String(icon)}
+              alt={tCommon('sortingIcon')}
+              aria-hidden="true"
+            />
+          ) : (
+            <IconComponent
+              data-testid="sorting-icon"
+              data-icon-type={type}
+              aria-hidden="true"
+            />
+          )}
+        </span>
         {/* Use the appropriate icon */}
         {buttonLabel || selectedOption}
         {/* Use buttonLabel if provided, otherwise use selectedOption */}
