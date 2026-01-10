74fccdbc0b9fbfe0ca0233a0a815b8b94578ba5a
Aditya Rathi <162597136+AdityaRathi-10@users.noreply.github.com>
2026-01-10 21:56:46 +0530

    Refactor OrgListCard/TruncatedText component (#6252)
    * refactor truncatedtext component
    
    * tsdoc updates in related files
    
    * tsdoc fix in useDebounce
    
    * generated docs
    
    * tsdox fix in organizationcard
    
    * updated organizationcard file
    
    * minor fixes in organizationcard
    
    * added tests in truncatedtext spec file
    
    * minor fixes in truncatedtext spec file
    
    * added interface for truncatedtext in types
    
    * generated docs
    
    * minor changes and docs
    
    ---------
    
    Co-authored-by: Peter Harrison <16875803+palisadoes@users.noreply.github.com>

--- src/shared-components/useDebounce/useDebounce.tsx
+++ src/shared-components/useDebounce/useDebounce.tsx
@@ -3,24 +3,14 @@
  * The debounced function delays the execution of the callback until after a specified
  * delay has elapsed since the last time it was invoked.
  *
- * @template T - The type of the callback function.
+ * @typeParam T - The type of the callback function.
  * @param callback - The function to debounce. It will be executed after the delay
- * if no new calls are made during that time.
+ *   if no new calls are made during that time.
  * @param delay - The debounce delay in milliseconds.
  *
  * @returns An object containing:
  * - `debouncedCallback`: The debounced version of the provided callback function.
  * - `cancel`: A function to cancel any pending execution of the debounced callback.
- *
- * @example
- * ```tsx
- * const { debouncedCallback, cancel } = useDebounce((value) => {
- *   console.log(value);
- * }, 300);
- *
- * debouncedCallback('Hello');
- * // If called again within 300ms, the previous call is canceled.
- * ```
  *
  * @remarks
  * This hook is useful for scenarios like search input handling, where you want to
