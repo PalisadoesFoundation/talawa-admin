9cd487312a6b33c63f9d164600790f6bc11cf95c
Ritesh-251 <hareradheharekrishna23@gmail.com>
2026-01-20 13:23:38 +0530

    fix(css): import react-toastify css in shared component
    This ensures toasts are visible in e2e tests where css injection is strict.
    Fixes Cypress visibility failures.

--- src/shared-components/NotificationToast/NotificationToast.tsx
+++ src/shared-components/NotificationToast/NotificationToast.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { Id, ToastContainerProps, ToastOptions } from 'react-toastify';
 import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import i18n from 'utils/i18n';
 
 import type {
