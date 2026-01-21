e92c99e1c3942e75ee16c43755ac134f3edd8014
Aditya Singh <adityasinghrajput755@gmail.com>
2026-01-22 03:53:29 +0530

    fix(tests): update EventAgendaItems to use selectOptions for native select
    The selectAgendaCategory helper was using getByRole('combobox') which
    was valid for MUI Autocomplete but not for native HTML select element.
    Updated to use userEvent.selectOptions() which is the correct approach
    for native select elements.

--- src/components/AdminPortal/EventManagement/EventAgendaItems/EventAgendaItems.spec.tsx
+++ src/components/AdminPortal/EventManagement/EventAgendaItems/EventAgendaItems.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, waitFor, act, within } from '@testing-library/react';
+import { render, screen, waitFor, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import * as apolloClient from '@apollo/client';
 import { MockedProvider } from '@apollo/client/testing';
@@ -343,11 +343,8 @@ describe('Testing Agenda Items Components', () => {
 
   const selectAgendaCategory = async (categoryName = 'Category 1') => {
     const categorySelect = screen.getByTestId('categorySelect');
-    await userEvent.click(within(categorySelect).getByRole('combobox'));
-    const categoryOption = await screen.findByRole('option', {
-      name: categoryName,
-    });
-    await userEvent.click(categoryOption);
+    // Use selectOptions for native HTML select element (migrated from MUI Autocomplete)
+    await userEvent.selectOptions(categorySelect, categoryName);
   };
 
   const renderEventAgendaItems = ({
