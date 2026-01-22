54e634dc25be2e13b9ec217d8965e9c4bc492259
Sarthak Kharka <kharkasarthak@gmail.com>
2026-01-23 00:32:07 +0530

    removed Form import

--- src/screens/LoginPage/LoginPage.tsx
+++ src/screens/LoginPage/LoginPage.tsx
@@ -17,7 +17,6 @@ import Check from '@mui/icons-material/Check';
 import Clear from '@mui/icons-material/Clear';
 import type { ChangeEvent } from 'react';
 import React, { useEffect, useRef, useState } from 'react';
-import Form from 'react-bootstrap/Form';
 import { Button } from 'shared-components/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
