19863a99ed85134bb9763d9d2ebf1064c8386dce
Monish Puttu <monishputtu1780@gmail.com>
2026-01-26 09:27:44 +0530

    refactor: code quality improvements

--- src/utils/performance.ts
+++ src/utils/performance.ts
@@ -7,16 +7,17 @@ export { default as useDebounce } from './useDebounce';
 /**
  * Creates a debounced version of a function (for non-hook usage)
  */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export function debounce<TFn extends (...args: any[]) => void>(
+export function debounce<TFn extends (...args: unknown[]) => void>(
   fn: TFn,
   delay: number,
-): (...args: Parameters<TFn>) => void {
-  /* eslint-enable @typescript-eslint/no-explicit-any */
+): (...args: unknown[]) => void {
   let timeoutId: number | null = null;
 
-  return function (this: unknown, ...args: Parameters<TFn>) {
+  return function (this: unknown, ...args: unknown[]) {
     if (timeoutId !== null) clearTimeout(timeoutId);
-    timeoutId = window.setTimeout(() => fn.apply(this, args), delay);
+    timeoutId = window.setTimeout(() => {
+      fn.apply(this, args);
+      timeoutId = null;
+    }, delay);
   };
 }
