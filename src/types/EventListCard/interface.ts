0eb660adde82b2bcbffc626cc4c12da61dbe4cce
rohanshrma222 <hackerviruse04@gmail.com>
2026-01-27 01:07:57 +0530

    did cr changes

--- src/types/EventListCard/interface.ts
+++ src/types/EventListCard/interface.ts
@@ -1,9 +1,16 @@
+/**
+ * Event list card props extending InterfaceEvent.
+ * `@remarks` refetchEvents is optional and triggers a refresh when provided.
+ */
 import type { InterfaceEvent } from 'types/Event/interface';
 import type { TFunction } from 'i18next';
 
 export interface InterfaceEventListCard extends InterfaceEvent {
   refetchEvents?: () => void;
 }
+/**
+ * Props for EventListCardModals component.
+ */
 
 export interface InterfaceEventListCardModalsProps {
   eventListCardProps: InterfaceEventListCard;
