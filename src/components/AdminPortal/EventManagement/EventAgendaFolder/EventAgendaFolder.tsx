c6114af3d3946cb7e342ca345e731fe71cde2e44
Harsh <harshk1729@gmail.com>
2026-01-27 21:32:18 +0530

    fix translation failing test

--- src/components/AdminPortal/EventManagement/EventAgendaFolder/EventAgendaFolder.tsx
+++ src/components/AdminPortal/EventManagement/EventAgendaFolder/EventAgendaFolder.tsx
@@ -64,7 +64,7 @@ function EventAgendaFolder(props: { eventId: string }): JSX.Element {
   const { eventId } = props;
   const { orgId } = useParams();
 
-  const { t } = useTranslation('translation', { keyPrefix: 'agendaSection' });
+  const { t } = useTranslation('translation', { keyPrefix: 'agendaItems' });
 
   // State to manage the create agenda item modal visibility
   const [agendaFolderCreateModalIsOpen, setAgendaFolderCreateModalIsOpen] =
