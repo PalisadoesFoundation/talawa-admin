1a1790da18de69c1dd6960ca43e380a2ef013c57
Rohan <rohansharmarohansharma22@gmail.com>
2026-01-30 16:33:50 +0530

    PART-10i18n translations (#6726)
    * migratin to i18n
    
    * added missing keys
    
    * Used plain string keys,fixed formatting
    
    * added test common
    
    * changed type from t to tcommon
    
    * added comments,missing arguments
    
    * fixed formatting
    
    * fixed type error
    
    * added test in common,removed backtick
    
    * added new css style
    
    * addressed non-internationalized text
    
    * removed unused keys
    
    * added docs
    
    * did some syntax changes
    
    * fixed tests
    
    * did cr changes
    
    * removed all non-internationalized
    
    * reverted changes
    
    * added docs
    
    * made some cleanup
    
    * added docs
    
    * added docs,moved props into separate interface
    
    * did cr changes
    
    * added docs
    
    * edited keys
    
    * added comments,did cleanups
    
    * minor changes
    
    * added string type
    
    * imported aftereach
    
    * trigger coderabbit review
    
    * wrote a new test to match the coverage
    
    * deleted unauth. test ,wrote a new test to match the coverage
    
    * fixed formatting
    
    * fixed formatting
    
    * RC changes
    
    * added doc
    
    * added doc,added tests
    
    * did CR changes
    
    * added doc
    
    * added comments,added tests
    
    * added needed space
    
    * added docs
    
    * fixed tests
    
    * trigger coderabbit review
    
    * fixed the test
    
    * recovering the coverage
    
    * trigger PR refresh
    
    * removed unnecessary comment
    
    * fixed testing
    
    * fixed testing
    
    * removed keyboard false prop from basemodal,did minor changes
    
    * added docs
    
    * added comments
    
    * added docs
    
    * TsDOc changes
    
    * added doc
    
    * fixed failing tests
    
    * CR change
    
    * addressed cr changes
    
    * added docs
    
    * added comments
    
    * added docs,did CR changes
    
    * standardizing to  Camelcasing
    
    * corrected key,fixed flakyness
    
    * fixed formatting
    
    * fixed remaining issuesz
    
    * added doc
    
    * minor change
    
    * fixed test
    
    * removed the use dummydatetime
    
    * fixed formatting
    
    * added docs
    
    * fixed testing
    
    * replaced hardcoded strings in EventListCardPreviewModal
    
    * added docs
    
    * fixing tests
    
    * fixing tests
    
    * removed unnecessary type,completed mock
    
    * added beforeEach
    
    * fixed test
    
    * CR changes
    
    * added docs
    
    * did minor changes

--- src/screens/AdminPortal/EventVolunteers/VolunteerGroups/modal/VolunteerGroupModal.spec.tsx
+++ src/screens/AdminPortal/EventVolunteers/VolunteerGroups/modal/VolunteerGroupModal.spec.tsx
@@ -953,7 +953,6 @@ describe('Testing VolunteerGroupModal', () => {
       const volunteersRequiredInput = screen.getByTestId(
         'volunteersRequiredInput',
       );
-      await user.clear(volunteersRequiredInput);
       await user.type(volunteersRequiredInput, '5');
       await waitFor(() => {
         expect(volunteersRequiredInput).toHaveValue(5);
