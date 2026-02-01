dfc55158af2e274e6b8b1bc230b97d4c1f73087a
rohanshrma222 <hackerviruse04@gmail.com>
2026-02-02 02:06:41 +0530

    cleanup

--- scripts/eslint/rules/i18n-camelcase.ts
+++ scripts/eslint/rules/i18n-camelcase.ts
@@ -1,28 +1,27 @@
 /**
  * Restrictions for i18n translation keys to enforce camelCase
  */
+export const tCallSelector =
+  ":matches(CallExpression[callee.name='t'], CallExpression[callee.type='MemberExpression'][callee.property.name='t'])";
+
 export const i18nCamelCaseRestrictions = [
   {
-    selector:
-      "CallExpression[callee.name='t'] > Literal[value=/\\s/], CallExpression[callee.type='MemberExpression'][callee.object.name='i18n'][callee.property.name='t'] > Literal[value=/\\s/]",
+    selector: `${tCallSelector} > Literal[value=/\\s/]`,
     message:
       'i18n Convention Violation: Translation keys should not contain spaces. Use camelCase (e.g., "myOrganizations").',
   },
   {
-    selector:
-      "CallExpression[callee.name='t'] > Literal[value=/^(?![a-z])/], CallExpression[callee.type='MemberExpression'][callee.object.name='i18n'][callee.property.name='t'] > Literal[value=/^(?![a-z])/]",
+    selector: `${tCallSelector} > Literal[value=/^(?![a-z])/]`,
     message:
       'i18n Convention Violation: Translation keys should start with a lowercase letter (camelCase).',
   },
   {
-    selector:
-      "CallExpression[callee.name='t'] > Literal[value=/[-_]/], CallExpression[callee.type='MemberExpression'][callee.object.name='i18n'][callee.property.name='t'] > Literal[value=/[-_]/]",
+    selector: `${tCallSelector} > Literal[value=/[-_]/]`,
     message:
       'i18n Convention Violation: Translation keys should not contain dashes or underscores. Use camelCase.',
   },
   {
-    selector:
-      "CallExpression[callee.name='t'] > Literal[value=/[^a-zA-Z0-9.:]/], CallExpression[callee.type='MemberExpression'][callee.object.name='i18n'][callee.property.name='t'] > Literal[value=/[^a-zA-Z0-9.:]/]",
+    selector: `${tCallSelector} > Literal[value=/[^a-zA-Z0-9.:]/]`,
     message:
       'i18n Convention Violation: Translation keys should only contain alphanumeric characters, dots, or colons.',
   },
