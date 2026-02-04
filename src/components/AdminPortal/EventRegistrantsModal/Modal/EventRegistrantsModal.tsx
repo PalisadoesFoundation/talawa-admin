f08b4eb40d702ff43588d889a032cac1bafb9499
= <kanikayadav061@gmail.com>
2026-02-04 09:15:34 +0530

    fix: modal changes

--- src/components/AdminPortal/EventRegistrantsModal/Modal/EventRegistrantsModal.tsx
+++ src/components/AdminPortal/EventRegistrantsModal/Modal/EventRegistrantsModal.tsx
@@ -1,63 +1,31 @@
-/**
- * EventRegistrantsModal Component
- *
- * This component renders a modal to manage event registrants. It allows users to:
- * - View the list of registered attendees for a specific event.
- * - Add new attendees from the organization's member list or through on-spot registration.
- * - Remove existing attendees from the event.
- *
- * @param props - The properties passed to the component.
- * @param show - Determines whether the modal is visible.
- * @param eventId - The ID of the event for which registrants are being managed.
- * @param orgId - The ID of the organization associated with the event.
- * @param handleClose - Callback function to close the modal.
- *
- * @returns The rendered EventRegistrantsModal component.
- *
- * @remarks
- * - Uses Apollo Client's `useQuery` and `useMutation` hooks to fetch and modify data.
- * - Displays a loading spinner while data is being fetched.
- * - Integrates with `NotificationToast` for user notifications..
- * - Supports translations using `react-i18next`.
- *
- * @example
- * ```tsx
- * <EventRegistrantsModal
- *   show={true}
- *   eventId="event123"
- *   orgId="org456"
- *   handleClose={() => setShowModal(false)}
- * />
- * ```
- *
- * Dependencies:
- * - shared-components/Button for button components.
- * - shared-components/BaseModal for modal components.
- * - `@apollo/client` for GraphQL queries and mutations.
- * - `@mui/material` for UI components like Avatar, Chip, and Autocomplete.
- * - `NotificationToast` for toast notifications.
- * - `react-i18next` for translations.
- */
-import React, { useState, useEffect } from 'react';
-import Button from 'shared-components/Button';
+import React, { useEffect, useState } from 'react';
 import { useMutation, useQuery } from '@apollo/client';
+import Autocomplete from '@mui/material/Autocomplete';
+
+import Button from 'shared-components/Button';
+import { BaseModal } from 'shared-components/BaseModal';
+import { ErrorBoundaryWrapper } from 'shared-components/ErrorBoundaryWrapper/ErrorBoundaryWrapper';
+import { NotificationToast } from 'components/NotificationToast/NotificationToast';
+import { useTranslation } from 'react-i18next';
+
+import { FormFieldGroup } from 'shared-components/FormFieldGroup/FormFieldGroup';
+/* eslint-disable no-restricted-imports */
+import TextField from '@mui/material/TextField';
+
 import {
   EVENT_ATTENDEES,
   MEMBERS_LIST,
   EVENT_DETAILS,
 } from 'GraphQl/Queries/Queries';
 import { ADD_EVENT_ATTENDEE } from 'GraphQl/Mutations/mutations';
-import { FormTextField } from 'shared-components/FormFieldGroup/FormFieldGroup';
-import Autocomplete from '@mui/material/Autocomplete';
-import { useTranslation } from 'react-i18next';
+
 import AddOnSpotAttendee from './AddOnSpot/AddOnSpotAttendee';
 import InviteByEmailModal from './InviteByEmail/InviteByEmailModal';
+
 import type { InterfaceUser } from 'types/shared-components/User/interface';
-import styles from './EventRegistrantsModal.module.css';
-import { NotificationToast } from 'components/NotificationToast/NotificationToast';
-import { BaseModal } from 'shared-components/BaseModal';
-import { ErrorBoundaryWrapper } from 'shared-components/ErrorBoundaryWrapper/ErrorBoundaryWrapper';
 import { InterfaceEventRegistrantsModalProps } from 'types/AdminPortal/EventRegistrantsModal/interface';
+
+import styles from './EventRegistrantsModal.module.css';
 
 export const EventRegistrantsModal = ({
   eventId,
@@ -65,16 +33,13 @@ export const EventRegistrantsModal = ({
   handleClose,
   show,
 }: InterfaceEventRegistrantsModalProps): JSX.Element => {
-  const [member, setMember] = useState<InterfaceUser | null>(null);
-  const [open, setOpen] = useState(false);
-  const [inviteOpen, setInviteOpen] = useState(false);
-  const [isRecurring, setIsRecurring] = useState<boolean>(false);
-  const [inputValue, setInputValue] = useState<string>('');
+  const [selectedMember, setSelectedMember] = useState<InterfaceUser | null>(
+    null,
+  );
+  const [openOnSpot, setOpenOnSpot] = useState(false);
+  const [openInviteByEmail, setOpenInviteByEmail] = useState(false);
+  const [isRecurring, setIsRecurring] = useState(false);
 
-  // Hooks for mutation operations
-  const [addRegistrantMutation] = useMutation(ADD_EVENT_ATTENDEE);
-
-  // Translation hooks
   const { t } = useTranslation('translation', {
     keyPrefix: 'eventRegistrantsModal',
   });
@@ -82,50 +47,56 @@ export const EventRegistrantsModal = ({
   const { t: tErrors } = useTranslation('errors');
 
   const { data: eventData } = useQuery(EVENT_DETAILS, {
-    variables: { eventId: eventId },
-    fetchPolicy: 'cache-first',
+    variables: { eventId },
   });
 
-  // Determine event type
   useEffect(() => {
     if (eventData?.event) {
-      setIsRecurring(!!eventData.event.recurrenceRule);
+      setIsRecurring(Boolean(eventData.event.recurrenceRule));
     }
   }, [eventData]);
 
-  // Query hooks to fetch event attendees and organization members
-  const { refetch: attendeesRefetch } = useQuery(EVENT_ATTENDEES, {
-    variables: { eventId: eventId },
+  const { refetch: refetchAttendees } = useQuery(EVENT_ATTENDEES, {
+    variables: { eventId },
   });
 
   const { data: memberData } = useQuery(MEMBERS_LIST, {
     variables: { organizationId: orgId },
   });
 
-  // Function to add a new registrant to the event
-  const addRegistrant = (): void => {
-    if (member == null) {
+  const [addRegistrantMutation] = useMutation(ADD_EVENT_ATTENDEE);
+
+  const addRegistrant = async (): Promise<void> => {
+    if (!selectedMember) {
       NotificationToast.warning(t('selectUserFirst'));
       return;
     }
-    NotificationToast.warning(t('addingAttendee'));
-    const addVariables = isRecurring
-      ? { userId: member.id, recurringEventInstanceId: eventId }
-      : { userId: member.id, eventId: eventId };
 
-    addRegistrantMutation({
-      variables: addVariables,
-    })
-      .then(() => {
-        NotificationToast.success(
-          tCommon('addedSuccessfully', { item: 'Attendee' }) as string,
-        );
-        attendeesRefetch(); // Refresh the list of attendees
-      })
-      .catch((err) => {
-        NotificationToast.error(t('errorAddingAttendee') as string);
-        NotificationToast.error(err.message);
-      });
+    NotificationToast.warning(t('addingAttendee'));
+
+    const variables = isRecurring
+      ? {
+          userId: selectedMember.id,
+          recurringEventInstanceId: eventId,
+        }
+      : {
+          userId: selectedMember.id,
+          eventId,
+        };
+
+    try {
+      await addRegistrantMutation({ variables });
+
+      NotificationToast.success(
+        tCommon('addedSuccessfully', { item: 'Attendee' }) as string,
+      );
+
+      setSelectedMember(null);
+      refetchAttendees();
+    } catch (error) {
+      NotificationToast.error(t('errorAddingAttendee') as string);
+      NotificationToast.error((error as Error).message);
+    }
   };
 
   return (
@@ -136,35 +107,34 @@ export const EventRegistrantsModal = ({
       resetButtonText={tErrors('resetButton')}
     >
       <section>
+        {/* On-Spot Attendee Modal */}
         <AddOnSpotAttendee
-          show={open}
-          handleClose={() => setOpen(false)}
-          reloadMembers={() => {
-            attendeesRefetch();
-          }}
+          show={openOnSpot}
+          handleClose={() => setOpenOnSpot(false)}
+          reloadMembers={refetchAttendees}
         />
+
+        {/* Invite by Email Modal */}
         <InviteByEmailModal
-          show={inviteOpen}
-          handleClose={() => setInviteOpen(false)}
+          show={openInviteByEmail}
+          handleClose={() => setOpenInviteByEmail(false)}
           eventId={eventId}
           isRecurring={isRecurring}
-          onInvitesSent={() => {
-            attendeesRefetch();
-          }}
+          onInvitesSent={refetchAttendees}
         />
+
         <BaseModal
           show={show}
           onHide={handleClose}
           title={t('eventRegistrantsTitle')}
           headerClassName={styles.modalHeader}
-          dataTestId="invite-modal"
           showCloseButton
           footer={
             <div>
               <Button
                 className={styles.inviteButton}
                 data-testid="invite-by-email-btn"
-                onClick={() => setInviteOpen(true)}
+                onClick={() => setOpenInviteByEmail(true)}
               >
                 {t('inviteByEmailButton')}
               </Button>
@@ -179,59 +149,40 @@ export const EventRegistrantsModal = ({
             </div>
           }
         >
-          <Autocomplete
-            disablePortal
-            inputValue={inputValue}
-            onInputChange={(_, value) => setInputValue(value)}
-            id="addRegistrant"
-            onChange={(_, newMember): void => {
-              setMember(newMember);
-            }}
-            noOptionsText={
-              <div className="d-flex ">
-                <p className="me-2">{t('noRegistrationsFound')}</p>
-                <button
-                  type="button"
-                  data-testid="add-onspot-link"
-                  className={`underline ${styles.underlineText}`}
-                  onClick={() => setOpen(true)}
-                  onMouseDown={(e) => e.preventDefault()}
-                  onKeyDown={(e) => {
-                    if (e.key === 'Enter' || e.key === ' ') {
-                      e.preventDefault();
-                      setOpen(true);
-                    }
-                  }}
-                >
-                  {t('addOnspotRegistrationLink')}
-                </button>
-              </div>
-            }
-            options={memberData?.usersByOrganizationId || []}
-            getOptionLabel={(member: InterfaceUser): string =>
-              member.name || t('unknownUser')
-            }
-            renderInput={(params): React.ReactNode => (
-              <FormTextField
-                name="addRegistrant"
-                label={t('addRegistrantLabel') as string}
-                ref={params.InputProps.ref}
-                value={inputValue}
-                placeholder={t('addRegistrantPlaceholder') as string}
-                data-testid="autocomplete"
-                id={params.id}
-                disabled={params.disabled}
-                fullWidth
-                onChange={(v: string) => {
-                  if (params.inputProps?.onChange) {
-                    params.inputProps.onChange({
-                      target: { value: v },
-                    } as React.ChangeEvent<HTMLInputElement>);
-                  }
-                }}
-              />
-            )}
-          />
+          <FormFieldGroup name="addRegistrant" label={t('addRegistrantLabel')}>
+            <Autocomplete
+              options={memberData?.usersByOrganizationId || []}
+              getOptionLabel={(user: InterfaceUser) =>
+                user?.name || t('unknownUser')
+              }
+              onChange={(_, newMember) => setSelectedMember(newMember)}
+              noOptionsText={
+                <div className="d-flex">
+                  <p className="me-2">{t('noRegistrationsFound')}</p>
+                  <button
+                    type="button"
+                    className={`underline ${styles.underlineText}`}
+                    onMouseDown={(e) => e.preventDefault()}
+                    onClick={() => setOpenOnSpot(true)}
+                  >
+                    {t('addOnspotRegistrationLink')}
+                  </button>
+                </div>
+              }
+              renderInput={(params) => (
+                /* 
+                  MUI Autocomplete requires a MUI-compatible input.
+                  FormTextField is not compatible with Autocomplete’s internal input handling.
+                */
+                <TextField
+                  {...params}
+                  placeholder={t('addRegistrantPlaceholder')}
+                  data-testid="autocomplete"
+                />
+              )}
+            />
+          </FormFieldGroup>
+
           <br />
         </BaseModal>
       </section>
