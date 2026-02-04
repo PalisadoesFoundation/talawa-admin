da3ffbbf789aa21c63196aeb1b604f4a8219161d
Sarthak Kharka <143311247+s3arthak@users.noreply.github.com>
2026-02-04 07:50:22 +0530

    refactor: migrate UserContactDetails dropdowns to shared DropDownButton (#7000)
    * refactor DynamicDropDown
    
    * added coderabbitai suggestions
    
    * deleted unused files
    
    * css modify
    
    * dateBox alignment fix

--- docs/docs/auto-docs/types/DropDown/interface/interfaces/InterfaceDropDownProps.md
+++ docs/docs/auto-docs/types/DropDown/interface/interfaces/InterfaceDropDownProps.md
@@ -6,10 +6,6 @@
 
 Defined in: [src/types/DropDown/interface.ts:2](https://github.com/PalisadoesFoundation/talawa-admin/blob/main/src/types/DropDown/interface.ts#L2)
 
-## Extended by
-
-- [`InterfaceChangeDropDownProps`](InterfaceChangeDropDownProps.md)
-
 ## Properties
 
 ### btnStyle?
--- src/types/DropDown/interface.ts
+++ src/types/DropDown/interface.ts
@@ -5,25 +5,6 @@ export interface InterfaceDropDownProps {
   btnTextStyle?: string;
 }
 
-/**
- * Props for DynamicDropDown change-handling variant.
- * @typeParam T - Form state shape.
- */
-export interface InterfaceChangeDropDownProps<
-  T,
-> extends InterfaceDropDownProps {
-  /** State setter for the form. */
-  setFormState: React.Dispatch<React.SetStateAction<T>>;
-  /** Current form state. */
-  formState: T;
-  /** Dropdown options. */
-  fieldOptions: { value: string; label: string }[];
-  /** Form field name bound to this dropdown. */
-  fieldName: string;
-  /** Optional custom change handler. */
-  handleChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
-}
-
 export interface InterfaceCollapsibleDropdown {
   showDropdown: boolean;
   target: TargetsType;
