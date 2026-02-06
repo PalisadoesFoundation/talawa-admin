2d9af12e96651994a62b351363fbd5096e4506ef
rohanshrma222 <hackerviruse04@gmail.com>
2026-02-06 12:06:42 +0530

    fixed linting

--- scripts/eslint/config/exemptions.js
+++ scripts/eslint/config/exemptions.js
@@ -98,7 +98,7 @@ export const wrapperExemptions = [
     componentName: 'Button',
     allowedIds: ['rb-button', 'rb-button-path'],
   }),
-   {
+  {
     files: [
       'src/components/AdminPortal/EventRegistrantsModal/Modal/EventRegistrantsModal.tsx',
     ],
