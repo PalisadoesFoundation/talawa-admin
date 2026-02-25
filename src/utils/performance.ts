4964353b8bcf0f98c85a97d62ff37ba01952bec6
rohanshrma222 <hackerviruse04@gmail.com>
2026-02-25 10:20:44 +0530

    fixed linting error

--- src/utils/performance.ts
+++ src/utils/performance.ts
@@ -1,13 +1,9 @@
 import debounce from 'lodash-es/debounce';
 import throttle from 'lodash-es/throttle';
-import type { DebouncedFunc } from 'lodash';
 
 export { debounce, throttle };
 
-type UnknownFunction = (...args: unknown[]) => unknown;
-
-export const debounceInput = (
-  fn: UnknownFunction,
-  wait = 300,
-): DebouncedFunc<UnknownFunction> =>
-  debounce(fn, wait, { leading: false, trailing: true });
+export const debounceInput = (fn: (...args: never[]) => void, wait = 300) =>
+  debounce(fn, wait, { leading: false, trailing: true }) as ReturnType<
+    typeof debounce
+  >;
