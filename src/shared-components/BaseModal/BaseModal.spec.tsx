6ffcb09b1afc5e18f43d3778d6b1ab29c8ebd5c2
Shivam <shivam108ku@gmail.com>
2026-03-03 18:11:09 +0530

    fix: formatting in BaseModal.spec.tsx

--- src/shared-components/BaseModal/BaseModal.spec.tsx
+++ src/shared-components/BaseModal/BaseModal.spec.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { describe, it, expect, vi, afterEach } from 'vitest';
-import { render, screen, waitFor, fireEvent, cleanup } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitFor,
+  fireEvent,
+  cleanup,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Button } from 'react-bootstrap';
 import BaseModal from './BaseModal';
