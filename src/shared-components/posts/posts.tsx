f5ef2ab2822ddb6050bd8e6b746c69b7e9513398
jashan <jashandeep1849@gmail.com>
2026-06-19 13:19:36 +0530

    fixed posts attachements not rendering

--- src/shared-components/posts/posts.tsx
+++ src/shared-components/posts/posts.tsx
@@ -477,7 +477,14 @@ export default function PostsPage() {
                   </div>
                   <div className="post-content">{post.caption ?? ''}</div>
                   {hasImage && (
-                    <div className="post-image-placeholder">{'\uD83D\uDCF7'} Photo</div>
+                    <div className="post-image-container">
+                      <img
+                        src={post.attachmentURL}
+                        alt="Post attachment"
+                        className="post-image"
+                        crossOrigin="anonymous"
+                      />
+                    </div>
                   )}
                   <div className="post-footer">
                     <div className="post-action">
@@ -528,7 +535,14 @@ export default function PostsPage() {
                     </div>
                     <div className="post-content">{post.caption ?? ''}</div>
                     {hasImage && (
-                      <div className="post-image-placeholder">{'\uD83D\uDCF7'} Photo</div>
+                      <div className="post-image-container">
+                        <img
+                          src={post.attachmentURL}
+                          alt="Post attachment"
+                          className="post-image"
+                          crossOrigin="anonymous"
+                        />
+                      </div>
                     )}
                     <div className="post-footer">
                       <div className="post-action">
